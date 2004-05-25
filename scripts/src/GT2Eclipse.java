c0dfd3f00a1b6247c1c8390db6d8d0517e4dcc81
Jody Garnett <jody.garnett@gmail.com>
2004-05-25 07:06:39 +0000

    updated to reflect new latout
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@6115 e5c1c795-43da-0310-a71f-fac65c449510

--- scripts/src/GT2Eclipse.java
+++ scripts/src/GT2Eclipse.java
@@ -41,7 +41,9 @@ import java.util.TreeSet;
  * For each <i>project </i> in <code>buildReport.txt</code>:
  * </p>
  * <ul>
- * <li>geotools-src/ <i>project </i>/project.xml will be processed:
+ * <li>module/ <i>project </i>/project.xml will be processed:
+ * <li>ext/ <i>project </i>/project.xml will be processed:
+ * <li>plugin/ <i>project </i>/project.xml will be processed:
  * <ul>
  * <li>dependencies will be added to MAVEN_REPORT int .classpath</li>
  * <li><code>src</code> will be added as a folder compiling to
@@ -54,61 +56,91 @@ import java.util.TreeSet;
  * Example: <code><pre>
  * 
  *  maven build
- *  java org.geotools2.build.GT2Eclipse
+ *  gt2eclipse .
  *  
  * </pre></code>
  * <p>
  * If you are running this from eclipse remember to hit refresh afterwords.
  * </p>
  * 
- * @see org.geotools2.build
  * @author jgarnett, Refractions Reasearch Inc.
  * @version CVS Version
  */
 public class GT2Eclipse {
 	static String buildlist;
 	/**
-	 * List of demos based on geotools-demos
+	 * List of tagets based on ext
 	 */
-	public static Set demos(String path) {
+	public static Set ext(String path) {
 		Set set = new TreeSet();
-		File geotools2demos = new File(path, "geotools-demos");
-		String dir[] = geotools2demos.list();
+		File ext = new File(path, "ext");
+		String dir[] = ext.list();
 		String src;
 		for (int i = 0; i < dir.length; i++) {
-			src = dir[i];
-			if (src.equals("CVS"))
-				continue;
-			if (!set.contains(src)) {
-				System.out.println("target " + src + " was not built by maven");
-			}
-			System.out.println("demo " + src + " not built by maven");
-			set.add(src);
+		    src = dir[i];
+		    if (src.equals("CVS"))
+			continue;
+		    if (!set.contains(src)) {
+			System.out.println("target " + src + " was not built by maven");
+		    }
+		    System.out.println("ext " + src + " not built by maven");
+		    set.add(src);
 		}
 		return set;
 	}
-	public static void generatebuildlist(String path) {
-		try {
-			File srcdir = new File(path, "geotools-src");
-			buildlist="buildlist.txt";
-			BufferedWriter out = new BufferedWriter(new FileWriter(new File(path, buildlist)));
-			out.write("This file is used for building an eclipse .classpath file\n");
-			File[] list = srcdir.listFiles(new FileFilter() {
-	
-				public boolean accept(File pathname) {
-					if (pathname.isDirectory())
-						return true;
-					return false;
-				}
-			});
-			for (int i = 0; i < list.length; i++) {
-				out.write("gt2:" + list[i].getName() + " \n");
-			}
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
+	/**
+	 * List of targets based on plugin
+	 */
+	public static Set plugin(String path) {
+		Set set = new TreeSet();
+		File plugin = new File(path, "plugin");
+		String dir[] = plugin.list();
+		String src;
+		for (int i = 0; i < dir.length; i++) {
+		    src = dir[i];
+		    if (src.equals("CVS"))
+			continue;
+		    if (!set.contains(src)) {
+			System.out.println("target " + src + " was not built by maven");
+		    }
+		    System.out.println("plugin " + src + " not built by maven");
+		    set.add(src);
 		}
+		return set;
+	}
+	public static void generatebuildlist(String path ) {
+	    try {
+
+		buildlist="buildlist.txt";
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File(path, buildlist)));
+		out.write("This file is used for building an eclipse .classpath file\n");
+		FileFilter filter = new FileFilter() {			
+			public boolean accept(File pathname) {
+			    if (!pathname.isDirectory()) return false;
+			    return new File( pathname, "project.xml" ).exists() &&
+				new File( pathname, "src" ).exists();
+			}
+		    };		
+		File module = new File(path, "module");
+		File plugin = new File(path, "plugin");
+		File ext = new File(path, "ext");
+		File[] list = module.listFiles( filter );
+		for (int i = 0; i < list.length; i++) {
+		    out.write("gt2:" + list[i].getName() + " \n");
+		}
+		list = plugin.listFiles( filter );
+		for (int i = 0; i < list.length; i++) {
+		    out.write("gt2:" + list[i].getName() + " \n");
+		}
+		list = ext.listFiles( filter );
+		for (int i = 0; i < list.length; i++) {
+		    out.write("gt2:" + list[i].getName() + " \n");
+		}
+		out.close();
+	    } catch (IOException e) {
+		e.printStackTrace();
+		System.exit(1);
+	    }
 	}
 	/**
 	 * List of targets build by the last <code>maven build</code> command
@@ -118,63 +150,61 @@ public class GT2Eclipse {
 	 * </p>
 	 */
 	public static Set targets(String path) {
-		try {
-			BufferedReader buildReport;
-			if (buildlist == null) {
-				buildReport = new BufferedReader(new FileReader(new File(path,
-						"buildReport.txt")));
-			} else {
-				buildReport = new BufferedReader(new FileReader(new File(path,
-						buildlist)));
-			}
-			Set set = new TreeSet();
-			buildReport.readLine(); // skip first line
-			String line;
-			while ((line = buildReport.readLine()) != null) {
-				line = line.substring(line.indexOf(':') + 1);
-				line = line.substring(0, line.indexOf(' '));
-				// a few corrections
-				if (line.equals("oracle-spatial"))
-					line = "oraclespatial";
-				if (line.equals("java2drendering"))
-					line = "Java2DRendering";
-				if (line.equals("wmsserver"))
-					continue;
-				System.out.println("target " + line);
-				set.add(line);
-			}
-			File geotools2src = new File(path, "geotools-src");
-			String dir[] = geotools2src.list();
-			String src;
-			for (int i = 0; i < dir.length; i++) {
-				src = dir[i];
-				if (src.equals("CVS"))
-					continue;
-				if (!set.contains(src)) {
-					System.out.println("target " + src
-							+ " was not built by maven");
-					continue;
-				}
-				System.out.println("target arc not build by maven");
-				set.add(src);
-			}
-			// a few corrections
-			//
-			set.add("postgis");
-			set.add("j2se-demos");
-			return set;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.err
-					.println("Please run maven.build from your geotools2 directory."
-							+ "This will generate the file buildReports.txt required");
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.err
-					.println("Problem understanding the file buildReports.txt");
-			return null;
+	    try {
+		BufferedReader buildReport;
+		if (buildlist == null) {
+		    buildReport = new BufferedReader(new FileReader(new File(path, "buildReport.txt")));
+		} else {
+		    buildReport = new BufferedReader(new FileReader(new File(path, buildlist)));
 		}
+		Set set = new TreeSet();
+		buildReport.readLine(); // skip first line
+		String line;
+		while ((line = buildReport.readLine()) != null) {
+		    line = line.substring(line.indexOf(':') + 1);
+		    line = line.substring(0, line.indexOf(' '));
+		    // a few corrections
+		    if (line.equals("oracle-spatial"))
+			line = "oraclespatial";
+		    if (line.equals("java2drendering"))
+			line = "Java2DRendering";
+		    if (line.equals("wmsserver"))
+			continue;
+		    System.out.println("target " + line);
+		    set.add(line);
+		}
+		File module = new File(path, "module");
+		String dir[] = module.list();
+		String src;
+		for (int i = 0; i < dir.length; i++) {
+		    src = dir[i];
+		    if (src.equals("CVS"))
+			continue;
+		    if (!set.contains(src)) {
+			System.out.println("target " + src
+					   + " was not built by maven");
+			continue;
+		    }
+		    System.out.println("target arc not build by maven");
+		    set.add(src);
+		}
+		// a few corrections
+		//
+		set.add("postgis");
+		set.add("j2se-demos");
+		return set;
+	    } catch (FileNotFoundException e) {
+		e.printStackTrace();
+		System.err
+		    .println("Please run maven.build from your gt directory."
+			     + "This will generate the file buildReports.txt required");
+		return null;
+	    } catch (IOException e) {
+		e.printStackTrace();
+		System.err
+		    .println("Problem understanding the file buildReports.txt");
+		return null;
+	    }
 	}
 	/**
 	 * Run through the repository and depending on everything.
@@ -190,12 +220,6 @@ public class GT2Eclipse {
 	 */
 	static public Set dependencies(String path, Set targets) {
 		Set depends = new TreeSet();
-		// read file
-		/*
-		 * for( Iterator i=targets.iterator(); i.hasNext();){ depends.addAll(
-		 * process( path, "geotools-src/"+i.next()+"/project.xml" )); }
-		 */
-		// This did not work so lets try another way
 		// grab everything in the repository
 		File repository = new File(System.getProperty("user.home")
 				+ "/.maven/repository");
@@ -206,7 +230,7 @@ public class GT2Eclipse {
 			for (int i = 0; i < content.length; i++) {
 				System.out.println("processing " + content[i] + "...");
 				if ("gt2".equals(content[i])) {
-					System.out.println("  skipping geotools2 build results");
+					System.out.println("  skipping geotools build results");
 					continue;
 				}
 				File dir = new File(repository, content[i] + "/jars");
@@ -256,115 +280,119 @@ public class GT2Eclipse {
 		}
 		return depends;
 	}
-	/** grabs first tag on line */
-	static public String tag(String line) {
-		int start = line.indexOf('>');
-		int end = line.indexOf('<', start);
-		return line.substring(start + 1, end);
-	}
-	static public List process(String path, String target) {
-		List list = new LinkedList();
-		try {
-			BufferedReader project = new BufferedReader(new FileReader(
-					new File(path, target)));
-			String line;
-			line = project.readLine(); // first line
-			// skip till dependencies
-			for (; line != null; line = project.readLine()) {
-				line = line.trim();
-				if (line.equals("<dependencies>"))
-					break;
-			}
-			for (; line != null; line = project.readLine()) {
-				line = line.trim();
-				if (line.equals("</dependencies>"))
-					break;
-				if (line.equals("<dependency>")) {
-					String id = null;
-					String version = null;
-					for (; line != null; line = project.readLine()) {
-						line = line.trim();
-						if (line.equals("</dependency>"))
-							break;
-						if (line.startsWith("<id>"))
-							id = tag(line);
-						if (line.startsWith("<version>"))
-							version = tag(line);
-						if (line.startsWith("<artifactId>"))
-							id = tag(line);
-					}
-					if (version.equals("0.1")
-							|| version.indexOf("SNAPSHOT") != -1) {
-						break; // intra module dependency
-					} else {
-						// JTS/jars/JTS-1.3.jar
-						list.add(id + "/jars/" + id + "-" + version + ".jar");
-					}
-				}
-			}
-			return list;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.err
-					.println("Please run maven.build from your geotools2 directory."
-							+ "This will generate the file buildReports.txt required");
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.err
-					.println("Problem understanding the file buildReports.txt");
+    /** grabs first tag on line */
+    static public String tag(String line) {
+	int start = line.indexOf('>');
+	int end = line.indexOf('<', start);
+	return line.substring(start + 1, end);
+    }
+    static public List process(String path, String target) {
+	List list = new LinkedList();
+	try {
+	    BufferedReader project = new BufferedReader(new FileReader(
+								       new File(path, target)));
+	    String line;
+	    line = project.readLine(); // first line
+	    // skip till dependencies
+	    for (; line != null; line = project.readLine()) {
+		line = line.trim();
+		if (line.equals("<dependencies>"))
+		    break;
+	    }
+	    for (; line != null; line = project.readLine()) {
+		line = line.trim();
+		if (line.equals("</dependencies>"))
+		    break;
+		if (line.equals("<dependency>")) {
+		    String id = null;
+		    String version = null;
+		    for (; line != null; line = project.readLine()) {
+			line = line.trim();
+			if (line.equals("</dependency>"))
+			    break;
+			if (line.startsWith("<id>"))
+			    id = tag(line);
+			if (line.startsWith("<version>"))
+			    version = tag(line);
+			if (line.startsWith("<artifactId>"))
+			    id = tag(line);
+		    }
+		    if (version.equals("0.1")
+			|| version.indexOf("SNAPSHOT") != -1) {
+			break; // intra module dependency
+		    } else {
+			// JTS/jars/JTS-1.3.jar
+			list.add(id + "/jars/" + id + "-" + version + ".jar");
+		    }
 		}
-		return list;
+	    }
+	    return list;
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	    System.err.println("Please run maven.build from your geotools2 directory."
+			       + "This will generate the file buildReports.txt required");
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.err
+		.println("Problem understanding the file buildReports.txt");
 	}
-	public static void entry(PrintStream classpath, String project,
-			String path, String target) {
-		if (new File(project, path + "/" + target + "/src").exists()) {
-			classpath.println("    <classpathentry kind=\"src\"");
-			classpath.println("        output=\"" + path + "/" + target
-					+ "/target/classes\"");
-			classpath.println("        path=\"" + path + "/" + target
-					+ "/src\"/>");
-		}
-		if (new File(project, path + "/" + target + "/tests/unit").exists()) {
-			classpath.print("      <classpathentry");
-			if (target.equals("defaultcore"))
-				classpath.print(" excluding=\"**/DummyFeatureType.java\"");
-			if (target.equals("map"))
-				classpath.print(" excluding=\"**/BoundingBoxImplTest.java\"");
-			classpath.println(" kind=\"src\"");
-			classpath.println("        output=\"" + path + "/" + target
-					+ "/target/test-classes\"");
-			classpath.println("        path=\"" + path + "/" + target
-					+ "/tests/unit\"/>");
-		}
+	return list;
+    }
+    public static void entry(PrintStream classpath, String project,
+			     String path, String target) {
+	if (new File(project, path + "/" + target + "/src").exists()) {
+	    classpath.println("    <classpathentry kind=\"src\"");
+	    classpath.println("        output=\"" + path + "/" + target
+			      + "/target/classes\"");
+	    classpath.println("        path=\"" + path + "/" + target
+			      + "/src\"/>");
 	}
+	if (new File(project, path + "/" + target + "/test").exists()) {
+	    classpath.print("      <classpathentry");
+	    classpath.println(" kind=\"src\"");
+	    classpath.println("        output=\"" + path + "/" + target
+			      + "/target/test-classes\"");
+	    classpath.println("        path=\"" + path + "/" + target
+			      + "/test\"/>");
+	}
+    }
+    public static void die( String msg ){
+	if( msg != null ){
+	    System.out.println( msg );
+	}
+	System.out.println("use: java GT2Eclipse [OPTION] [geotools directory]");
+	System.out.println();
+	System.out.println("OPTIONS:");
+	System.out.println("\t-a, --all\tincludes all directories in classpath ");
+	System.out.println("\t\t\tregardless of whether they were correctly compiled");
+	System.out.println("\t-b, --built\tthis is the default option.");
+	System.out.println("\t\t\tincludes directories in classpath only ");
+	System.out.println("\t\t\tif they were correctly compiled");
+	System.out.println("\t-h, --help\tprints this help list");
+	System.out.println("\nNOTE:");
+	System.out.println("\tPlease run maven build prior to using this utility");
+	System.exit( msg == null ? 0 : 1 );
+    }
+    
 	public static void main(String args[]) {
 		String dir = null;
-		if (args.length == 0) {
-			dir = "../geotools2";
-		} else if (args[0].equals("-h") || args[0].equals("--help")) {
-			System.out.println("use: java GT2Eclipse [OPTION] [geotools2directory]");
-			System.out.println();
-			System.out.println("OPTIONS:");
-			System.out.println("\t-a, --all\tincludes all directories in classpath ");
-			System.out.println("\t\t\tregardless of whether they were correctly compiled");
-			System.out.println("\t-b, --built\tthis is the default option.");
-			System.out.println("\t\t\tincludes directories in classpath only ");
-			System.out.println("\t\t\tif they were correctly compiled");
-			System.out.println("\t-h, --help\tprints this help list");
-			System.out.println("\nNOTE:");
-			System.out.println("\tPlease run maven build prior to using this utility");
-			System.exit(0);
-		} else if (args[0].equals("-all") || args[0].equals("-a")) {
-			if (args.length == 1)
-				dir="../geotools2";
-			else
-				dir=args[1];
-			generatebuildlist(dir);
+		if (args.length == 0 || args[0].equals("-h") || args[0].equals("--help")) {
+		    die( null );
+		} else if (args[0].equals("-all") || args[0].equals("-a")) {		    
+		    if( args.length == 2){
+			dir = args[1];
+		    }
+		    else {
+			die("Require geotools directory for "+args[0] );
+		    }
+		    generatebuildlist(dir);
 		} else if (args[0].equals("-built") || args[0].equals("-b")) {
-			if (args.length == 1)
-				dir="../geotools2";
-			else
-				dir=args[1];
+		    if (args.length == 2){
+			dir=args[1];
+		    }
+		    else {
+			die("Require geotools directory for "+args[0] );
+		    }
 		} else {
 			dir = args[0];
 		}
@@ -384,16 +412,21 @@ public class GT2Eclipse {
 		classpath.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 		classpath.println("<classpath>");
 		String target;
+
 		Set targets = targets(dir);
 		for (int t = 1; t < args.length; t++) {
-			targets.add(args[t]);
+		    targets.add(args[t]);
 		}
 		for (Iterator i = targets.iterator(); i.hasNext();) {
-			entry(classpath, dir, "geotools-src", (String) i.next());
+		    entry(classpath, dir, "module", (String) i.next());
 		}
-		Set demos = demos(dir);
-		for (Iterator i = demos.iterator(); i.hasNext();) {
-			entry(classpath, dir, "geotools-demos", (String) i.next());
+		Set plugin = plugin(dir);
+		for (Iterator i = plugin.iterator(); i.hasNext();) {
+		    entry(classpath, dir, "plugin", (String) i.next());
+		}
+		Set ext = ext(dir);
+		for (Iterator i = ext.iterator(); i.hasNext();) {
+		    entry(classpath, dir, "ext", (String) i.next());
 		}
 		classpath.println("");
 		//classpath.println(" <classpathentry kind=\"var\" path=\"JRE_LIB\"
