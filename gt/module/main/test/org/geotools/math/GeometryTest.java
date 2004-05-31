ae5bc638c36f800aee9bcece8079095aed8d3686
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2004-05-31 11:41:25 +0000

    Merged LineTest and PlaneTest
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@6256 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/test/org/geotools/math/GeometryTest.java
+++ gt/module/main/test/org/geotools/math/GeometryTest.java
@@ -20,8 +20,10 @@
  */
 package org.geotools.math;
 
-// J2SE and Vecmath dependencies
+// J2SE dependencies
 import java.util.Random;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import javax.vecmath.Point3d;
 
 // JUnit dependencies
@@ -31,12 +33,12 @@ import junit.framework.TestSuite;
 
 
 /**
- * Test the {@link Plane} class.
+ * Test the {@link Line} and {@link Plane} classes.
  *
  * @version $Id$
  * @author Martin Desruisseaux
  */
-public final class PlaneTest extends TestCase {
+public final class GeometryTest extends TestCase {
     /**
      * Tolerance factor for comparaisons.
      */
@@ -53,20 +55,76 @@ public final class PlaneTest extends TestCase {
      * Returns the test suite.
      */
     public static Test suite() {
-        return new TestSuite(PlaneTest.class);
+        return new TestSuite(GeometryTest.class);
     }
 
     /**
      * Constructs a test case with the given name.
      */
-    public PlaneTest(final String name) {
+    public GeometryTest(final String name) {
         super(name);
+    }
+
+    /**
+     * Test {@link Line#setLine}.
+     */
+    public void testLine() {
+        final Line line = new Line();
+        line.setLine(new Point2D.Double(-2, 2), new Point2D.Double(8, 22));
+        assertEquals("slope", 2, line.getSlope(), EPS);
+        assertEquals("x0",   -3, line.getX0(),    EPS);
+        assertEquals("y0",    6, line.getY0(),    EPS);
+
+        // Horizontal line
+        line.setLine(new Point2D.Double(-2, 2), new Point2D.Double(8, 2));
+        assertEquals("slope", 0, line.getSlope(), EPS);
+        assertTrue  ("x0", Double.isInfinite(line.getX0()));
+        assertEquals("y0",    2, line.getY0(),    EPS);
+
+        // Vertical line
+        line.setLine(new Point2D.Double(-2, 2), new Point2D.Double(-2, 22));
+        assertTrue  ("slope", Double.isInfinite(line.getSlope()));
+        assertEquals("x0", -2, line.getX0(), EPS);
+        assertTrue  ("y0", Double.isInfinite(line.getY0()));
+
+        // Horizontal line on the x axis
+        line.setLine(new Point2D.Double(-2, 0), new Point2D.Double(8, 0));
+        assertEquals("slope", 0, line.getSlope(), EPS);
+        assertTrue  ("x0", Double.isInfinite(line.getX0()));
+        assertEquals("y0", 0, line.getY0(), EPS);
+
+        // Vertical line on the y axis
+        line.setLine(new Point2D.Double(0, 2), new Point2D.Double(0, 22));
+        assertTrue  ("slope", Double.isInfinite(line.getSlope()));
+        assertEquals("x0", 0, line.getX0(), EPS);
+        assertTrue  ("y0", Double.isInfinite(line.getY0()));
+    }
+
+    /**
+     * Test {@link Line#isoscelesTriangleBase}.
+     */
+    public void testIsoscelesTriangleBase() {
+        final Line test = new Line();
+        test.setLine(new Point2D.Double(20,30), new Point2D.Double(80,95));
+        assertEquals("slope", 1.083333333333333333333333, test.getSlope(), EPS);
+        assertEquals("y0",    8.333333333333333333333333, test.getY0(),    EPS);
+
+        final double distance = 40;
+        final Point2D summit = new Point2D.Double(27, -9); // An arbitrary point.
+        final Line2D base = test.isoscelesTriangleBase(summit, distance);
+        assertEquals("distance P1", distance, base.getP1().distance(summit), EPS);
+        assertEquals("distance P2", distance, base.getP2().distance(summit), EPS);
+
+        final double x=10; // Can be any arbitrary point.
+        final double y=8;
+        assertEquals("nearest colinear point", base.ptLineDist(x,y),
+                     test.nearestColinearPoint(new Point2D.Double(x,y)).distance(x,y), EPS);
     }
 
     /**
      * Test {@link Plane#setPlane} methods.
      */
-    public void testFit() {
+    public void testPlaneFit() {
         final Random  rd = new Random(457821698762354L);
         final Plane plan = new Plane();
         final Point3d P1 = new Point3d(100*rd.nextDouble()+25, 100*rd.nextDouble()+25, Math.rint(100*rd.nextDouble()+40));
