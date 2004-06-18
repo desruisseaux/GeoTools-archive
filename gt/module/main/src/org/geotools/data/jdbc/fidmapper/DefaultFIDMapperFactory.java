f262f586e9cd36beabc4e6c2d679561973d5d948
Chris Holmes <cholmesny@users.sourceforge.net>
2004-06-18 18:00:43 +0000

    changed to return a typed mapper.  We can discuss this, but I think it makes more sense to always prepend the typename, and I need it for my geoserver release right now
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@6468 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/data/jdbc/fidmapper/DefaultFIDMapperFactory.java
+++ gt/module/main/src/org/geotools/data/jdbc/fidmapper/DefaultFIDMapperFactory.java
@@ -46,7 +46,7 @@ public class DefaultFIDMapperFactory implements FIDMapperFactory {
     /** The logger for the filter module. */
     private static final Logger LOGGER = Logger.getLogger(
             "org.geotools.data.jdbc");
-    private boolean returningTypedFIDMapper;
+    private boolean returningTypedFIDMapper = true;
 
     /**
      * @see org.geotools.data.jdbc.fidmapper.FIDMapperFactory#getMapper(java.lang.String,
