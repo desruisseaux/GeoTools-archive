260a1be6adf90e088ed441250c62e1f16808218e
Jody Garnett <jody.garnett@gmail.com>
2004-07-19 16:51:58 +0000

    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@6973 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/metadata/iso19115/Contact.java
+++ gt/module/main/src/org/geotools/metadata/iso19115/Contact.java
@@ -1,6 +1,6 @@
 package org.geotools.metadata.iso19115;
 
-import java.util.Locale;
+import org.opengis.util.InternationalString;
 
 import org.opengis.metadata.citation.Address;
 import org.opengis.metadata.citation.OnLineResource;
@@ -8,65 +8,41 @@ import org.opengis.metadata.citation.Telephone;
 
 public class Contact extends MetaData implements
 	org.opengis.metadata.citation.Contact {
-
-	private String contactInstructions;
-	private String hoursOfService;
+    
+    private InternationalString contactInstructions;
+	private InternationalString hoursOfService;
 	private OnLineResource onLineResource;
 	private Address address;
 	private Telephone phone;
 	
-	/* (non-Javadoc)
-	 * @see org.opengis.metadata.citation.Contact#getPhone()
-	 */
-	public Telephone getPhone() {
-		return phone;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.opengis.metadata.citation.Contact#getAddress()
-	 */
 	public Address getAddress() {
-		return address;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.opengis.metadata.citation.Contact#getOnLineResource()
-	 */
-	public OnLineResource getOnLineResource() {
-		return onLineResource;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.opengis.metadata.citation.Contact#getHoursOfService(java.util.Locale)
-	 */
-	public String getHoursOfService(Locale arg0) {
-		return hoursOfService;
-	}
-	public String getHoursOfService() {
-		return hoursOfService;
-	}
-	/* (non-Javadoc)
-	 * @see org.opengis.metadata.citation.Contact#getContactInstructions(java.util.Locale)
-	 */
-	public String getContactInstructions(Locale arg0) {
-		return contactInstructions;
-	}
-	public String getContactInstruction() {
-		return contactInstructions;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-	public void setContactInstruction(String contactInstructions) {
-		this.contactInstructions = contactInstructions;
-	}
-	public void setHoursOfService(String hoursOfService) {
-		this.hoursOfService = hoursOfService;
-	}
-	public void setOnLineResource(OnLineResource onLineResource) {
-		this.onLineResource = onLineResource;
-	}
-	public void setPhone(Telephone phone) {
-		this.phone = phone;
-	}
+        return address;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    public InternationalString getContactInstructions() {
+        return contactInstructions;
+    }
+    public void setContactInstructions(InternationalString contactInstructions) {
+        this.contactInstructions = contactInstructions;
+    }
+    public OnLineResource getOnLineResource() {
+        return onLineResource;
+    }
+    public void setOnLineResource(OnLineResource onLineResource) {
+        this.onLineResource = onLineResource;
+    }
+    public Telephone getPhone() {
+        return phone;
+    }
+    public void setPhone(Telephone phone) {
+        this.phone = phone;
+    }    
+    public InternationalString getHoursOfService() {
+        return hoursOfService;
+    }
+    public void setHoursOfService(InternationalString hoursOfService) {
+        this.hoursOfService = hoursOfService;
+    }
 }
