c7da4aca064a2f3c29f0c0202f629c108128c9ff
dzwiers <>
2004-07-19 21:36:14 +0000

    turned off one set of tests ... not needed
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@7040 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/test/org/geotools/xml/GMLParserTester2.java
+++ gt/module/main/test/org/geotools/xml/GMLParserTester2.java
@@ -17,146 +17,146 @@ import javax.xml.parsers.SAXParserFactory;
 
 /**
  * <p>
- * DOCUMENT ME!
+ * Big dataset tests ... more than you want for regular testing
  * </p>
  * @
  *
  * @author dzwiers www.refractions.net
  */
-public class GMLParserTester2 extends TestCase {
-    public void testFMEPostalFeatures() throws SAXException, IOException {
-        try {
-            SAXParserFactory spf = SAXParserFactory.newInstance();
-            spf.setNamespaceAware(true);
-            spf.setValidating(false);
-
-            SAXParser parser = spf.newSAXParser();
-
-            String path = "fme/postal/postal.gml";
-            File f = TestData.file(this,path);
-            URI u = f.toURI();
-
-            XMLSAXHandler xmlContentHandler = new XMLSAXHandler(u,null);
-            XMLSAXHandler.setLogLevel(Level.WARNING);
-            XSISAXHandler.setLogLevel(Level.WARNING);
-            XMLElementHandler.setLogLevel(Level.WARNING);
-            XSIElementHandler.setLogLevel(Level.WARNING);
-
-            parser.parse(f, xmlContentHandler);
-
-            Object doc = xmlContentHandler.getDocument();
-            assertNotNull("Document missing", doc);
-            System.out.println(doc);
-            
-            Object[] objs = (Object[])doc;
-            
-            assertTrue("Should have 95054 features + 1 bbox : "+objs.length,objs.length == 95055);
-                        
-        } catch (Throwable e) {
-            e.printStackTrace();
-            fail(e.toString());
-        }
-    }
-    
-    public void testFMEFedCenFeatures() throws SAXException, IOException {
-        try {
-            SAXParserFactory spf = SAXParserFactory.newInstance();
-            spf.setNamespaceAware(true);
-            spf.setValidating(false);
-
-            SAXParser parser = spf.newSAXParser();
-
-            String path = "fme/fed-cen/fed308_a.gml";
-            File f = TestData.file(this,path);
-            URI u = f.toURI();
-
-            XMLSAXHandler xmlContentHandler = new XMLSAXHandler(u,null);
-            XMLSAXHandler.setLogLevel(Level.WARNING);
-            XSISAXHandler.setLogLevel(Level.WARNING);
-            XMLElementHandler.setLogLevel(Level.WARNING);
-            XSIElementHandler.setLogLevel(Level.WARNING);
-
-            parser.parse(f, xmlContentHandler);
-
-            Object doc = xmlContentHandler.getDocument();
-            assertNotNull("Document missing", doc);
-            System.out.println(doc);
-            
-            Object[] objs = (Object[])doc;
-            
-            assertTrue("Should have N features + 1 bbox : "+objs.length,objs.length > 2);
-            
-        } catch (Throwable e) {
-            e.printStackTrace();
-            fail(e.toString());
-        }
-    }
-    
-    public void testFMEForestFeatures() throws SAXException, IOException {
-        try {
-            SAXParserFactory spf = SAXParserFactory.newInstance();
-            spf.setNamespaceAware(true);
-            spf.setValidating(false);
-
-            SAXParser parser = spf.newSAXParser();
-
-            String path = "fme/forest_districts/forest-districts-2003-04.gml";
-            File f = TestData.file(this,path);
-            URI u = f.toURI();
-
-            XMLSAXHandler xmlContentHandler = new XMLSAXHandler(u,null);
-            XMLSAXHandler.setLogLevel(Level.WARNING);
-            XSISAXHandler.setLogLevel(Level.WARNING);
-            XMLElementHandler.setLogLevel(Level.WARNING);
-            XSIElementHandler.setLogLevel(Level.WARNING);
-
-            parser.parse(f, xmlContentHandler);
-
-            Object doc = xmlContentHandler.getDocument();
-            assertNotNull("Document missing", doc);
-            System.out.println(doc);
-            
-            Object[] objs = (Object[])doc;
-            
-            assertTrue("Should have N features + 1 bbox : "+objs.length,objs.length > 2);
-            
-        } catch (Throwable e) {
-            e.printStackTrace();
-            fail(e.toString());
-        }
-    }
-    
-    public void testFMEVictoriaFeatures() throws SAXException, IOException {
-        try {
-            SAXParserFactory spf = SAXParserFactory.newInstance();
-            spf.setNamespaceAware(true);
-            spf.setValidating(false);
-
-            SAXParser parser = spf.newSAXParser();
-
-            String path = "fme/victoria/victoria.gml";
-            File f = TestData.file(this,path);
-            URI u = f.toURI();
-
-            XMLSAXHandler xmlContentHandler = new XMLSAXHandler(u,null);
-            XMLSAXHandler.setLogLevel(Level.WARNING);
-            XSISAXHandler.setLogLevel(Level.WARNING);
-            XMLElementHandler.setLogLevel(Level.WARNING);
-            XSIElementHandler.setLogLevel(Level.WARNING);
-
-            parser.parse(f, xmlContentHandler);
-
-            Object doc = xmlContentHandler.getDocument();
-            assertNotNull("Document missing", doc);
-            System.out.println(doc);
-            
-            Object[] objs = (Object[])doc;
-            
-            assertTrue("Should have N features + 1 bbox : "+objs.length,objs.length > 2);
-            
-        } catch (Throwable e) {
-            e.printStackTrace();
-            fail(e.toString());
-        }
-    }
+//public class GMLParserTester2 extends TestCase {
+//    public void testFMEPostalFeatures() throws SAXException, IOException {
+//        try {
+//            SAXParserFactory spf = SAXParserFactory.newInstance();
+//            spf.setNamespaceAware(true);
+//            spf.setValidating(false);
+//
+//            SAXParser parser = spf.newSAXParser();
+//
+//            String path = "fme/postal/postal.gml";
+//            File f = TestData.file(this,path);
+//            URI u = f.toURI();
+//
+//            XMLSAXHandler xmlContentHandler = new XMLSAXHandler(u,null);
+//            XMLSAXHandler.setLogLevel(Level.WARNING);
+//            XSISAXHandler.setLogLevel(Level.WARNING);
+//            XMLElementHandler.setLogLevel(Level.WARNING);
+//            XSIElementHandler.setLogLevel(Level.WARNING);
+//
+//            parser.parse(f, xmlContentHandler);
+//
+//            Object doc = xmlContentHandler.getDocument();
+//            assertNotNull("Document missing", doc);
+//            System.out.println(doc);
+//            
+//            Object[] objs = (Object[])doc;
+//            
+//            assertTrue("Should have 95054 features + 1 bbox : "+objs.length,objs.length == 95055);
+//                        
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//            fail(e.toString());
+//        }
+//    }
+//    
+//    public void testFMEFedCenFeatures() throws SAXException, IOException {
+//        try {
+//            SAXParserFactory spf = SAXParserFactory.newInstance();
+//            spf.setNamespaceAware(true);
+//            spf.setValidating(false);
+//
+//            SAXParser parser = spf.newSAXParser();
+//
+//            String path = "fme/fed-cen/fed308_a.gml";
+//            File f = TestData.file(this,path);
+//            URI u = f.toURI();
+//
+//            XMLSAXHandler xmlContentHandler = new XMLSAXHandler(u,null);
+//            XMLSAXHandler.setLogLevel(Level.WARNING);
+//            XSISAXHandler.setLogLevel(Level.WARNING);
+//            XMLElementHandler.setLogLevel(Level.WARNING);
+//            XSIElementHandler.setLogLevel(Level.WARNING);
+//
+//            parser.parse(f, xmlContentHandler);
+//
+//            Object doc = xmlContentHandler.getDocument();
+//            assertNotNull("Document missing", doc);
+//            System.out.println(doc);
+//            
+//            Object[] objs = (Object[])doc;
+//            
+//            assertTrue("Should have N features + 1 bbox : "+objs.length,objs.length > 2);
+//            
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//            fail(e.toString());
+//        }
+//    }
+//    
+//    public void testFMEForestFeatures() throws SAXException, IOException {
+//        try {
+//            SAXParserFactory spf = SAXParserFactory.newInstance();
+//            spf.setNamespaceAware(true);
+//            spf.setValidating(false);
+//
+//            SAXParser parser = spf.newSAXParser();
+//
+//            String path = "fme/forest_districts/forest-districts-2003-04.gml";
+//            File f = TestData.file(this,path);
+//            URI u = f.toURI();
+//
+//            XMLSAXHandler xmlContentHandler = new XMLSAXHandler(u,null);
+//            XMLSAXHandler.setLogLevel(Level.WARNING);
+//            XSISAXHandler.setLogLevel(Level.WARNING);
+//            XMLElementHandler.setLogLevel(Level.WARNING);
+//            XSIElementHandler.setLogLevel(Level.WARNING);
+//
+//            parser.parse(f, xmlContentHandler);
+//
+//            Object doc = xmlContentHandler.getDocument();
+//            assertNotNull("Document missing", doc);
+//            System.out.println(doc);
+//            
+//            Object[] objs = (Object[])doc;
+//            
+//            assertTrue("Should have N features + 1 bbox : "+objs.length,objs.length > 2);
+//            
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//            fail(e.toString());
+//        }
+//    }
+//    
+//    public void testFMEVictoriaFeatures() throws SAXException, IOException {
+//        try {
+//            SAXParserFactory spf = SAXParserFactory.newInstance();
+//            spf.setNamespaceAware(true);
+//            spf.setValidating(false);
+//
+//            SAXParser parser = spf.newSAXParser();
+//
+//            String path = "fme/victoria/victoria.gml";
+//            File f = TestData.file(this,path);
+//            URI u = f.toURI();
+//
+//            XMLSAXHandler xmlContentHandler = new XMLSAXHandler(u,null);
+//            XMLSAXHandler.setLogLevel(Level.WARNING);
+//            XSISAXHandler.setLogLevel(Level.WARNING);
+//            XMLElementHandler.setLogLevel(Level.WARNING);
+//            XSIElementHandler.setLogLevel(Level.WARNING);
+//
+//            parser.parse(f, xmlContentHandler);
+//
+//            Object doc = xmlContentHandler.getDocument();
+//            assertNotNull("Document missing", doc);
+//            System.out.println(doc);
+//            
+//            Object[] objs = (Object[])doc;
+//            
+//            assertTrue("Should have N features + 1 bbox : "+objs.length,objs.length > 2);
+//            
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//            fail(e.toString());
+//        }
+//    }
 }
