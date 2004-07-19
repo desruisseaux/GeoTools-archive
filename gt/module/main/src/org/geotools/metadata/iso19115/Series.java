f72ad12f60027e60a3a40474db2d0a4edf7ca6b6
Jody Garnett <jody.garnett@gmail.com>
2004-07-19 16:54:10 +0000

    Switched to InternationalString and SetOf
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@6975 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/metadata/iso19115/Series.java
+++ gt/module/main/src/org/geotools/metadata/iso19115/Series.java
@@ -1,33 +1,29 @@
 package org.geotools.metadata.iso19115;
 
-import java.util.Locale;
+import org.opengis.util.InternationalString;
 
 public class Series extends MetaData implements
 		org.opengis.metadata.citation.Series {
 
 	private String page;
 	private String issueIdentification;
-	private String name;
-
-	public String getName(Locale arg0) {
-		return name;
-	}
-	public String getIssueIdentification() {
-		return issueIdentification;
-	}
-	public void setIssueIdentification(String issueIdentification) {
-		this.issueIdentification = issueIdentification;
-	}
-	public void setPage(String page) {
-		this.page = page;
-	}
-	public String getPage() {
-		return page;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	private InternationalString name;
+    public String getIssueIdentification() {
+        return issueIdentification;
+    }
+    public void setIssueIdentification(String issueIdentification) {
+        this.issueIdentification = issueIdentification;
+    }
+    public InternationalString getName() {
+        return name;
+    }
+    public void setName(InternationalString name) {
+        this.name = name;
+    }
+    public String getPage() {
+        return page;
+    }
+    public void setPage(String page) {
+        this.page = page;
+    }
 }
