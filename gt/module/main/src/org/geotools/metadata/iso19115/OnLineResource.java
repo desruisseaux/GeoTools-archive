ac95ec7456cc8b3a8a0f2a006141d7274e999d39
Jody Garnett <jody.garnett@gmail.com>
2004-07-19 16:53:02 +0000

    Switched to InternationalString and SetOf
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@6974 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/metadata/iso19115/OnLineResource.java
+++ gt/module/main/src/org/geotools/metadata/iso19115/OnLineResource.java
@@ -4,47 +4,43 @@ import java.net.URL;
 import java.util.Locale;
 
 import org.opengis.metadata.citation.OnLineFunction;
+import org.opengis.util.InternationalString;
 
 public class OnLineResource extends MetaData implements
 		org.opengis.metadata.citation.OnLineResource {
-
 	private OnLineFunction function;
-	private String description;
+	private InternationalString description;
 	private String applicationProfile;
 	private String protocol;
 	private URL linkage;
-
-	public URL getLinkage() {
-		return linkage;
-	}
-	public String getProtocol() {
-		return protocol;
-	}
-	public String getApplicationProfile() {
-		return applicationProfile;
-	}
-	public String getDescription(Locale arg0) {
-		return description;
-	}
-	public OnLineFunction getFunction() {
-		return function;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public void setApplicationProfile(String applicationProfile) {
-		this.applicationProfile = applicationProfile;
-	}
-	public void setFunction(OnLineFunction function) {
-		this.function = function;
-	}
-	public void setLinkage(URL linkage) {
-		this.linkage = linkage;
-	}
-	public void setProtocol(String protocol) {
-		this.protocol = protocol;
-	}
+    public String getApplicationProfile() {
+        return applicationProfile;
+    }
+    public void setApplicationProfile(String applicationProfile) {
+        this.applicationProfile = applicationProfile;
+    }
+    public InternationalString getDescription() {
+        return description;
+    }
+    public void setDescription(InternationalString description) {
+        this.description = description;
+    }
+    public OnLineFunction getFunction() {
+        return function;
+    }
+    public void setFunction(OnLineFunction function) {
+        this.function = function;
+    }
+    public URL getLinkage() {
+        return linkage;
+    }
+    public void setLinkage(URL linkage) {
+        this.linkage = linkage;
+    }
+    public String getProtocol() {
+        return protocol;
+    }
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
 }
