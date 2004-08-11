4bcc62123b2155087d72ed738addcd771e3c02ad
Richard Gould <rgould@refractions.net>
2004-08-11 16:48:03 +0000

    Added proper Geotools2 header.
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@7458 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/wms/src/org/geotools/data/wms/gce/WMSReader.java
+++ gt/plugin/wms/src/org/geotools/data/wms/gce/WMSReader.java
@@ -1,8 +1,18 @@
 /*
- * Created on Jul 20, 2004
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2004, Geotools Project Managment Committee (PMC)
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
  */
 package org.geotools.data.wms.gce;
 
