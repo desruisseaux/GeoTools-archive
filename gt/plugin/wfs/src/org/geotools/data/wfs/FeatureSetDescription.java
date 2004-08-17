61365a533e4fc2568818b79e98889038f30e26eb
dzwiers <>
2004-08-17 18:44:37 +0000

    getCapab object created
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@7538 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/plugin/wfs/src/org/geotools/data/wfs/FeatureSetDescription.java
@@ -0,0 +1,128 @@
+
+package org.geotools.data.wfs;
+
+import org.geotools.data.wms.getCapabilities.MetadataURL;
+
+import com.vividsolutions.jts.geom.Envelope;
+
+/**
+ * <p> 
+ * Represents a wfs:FeatureType ... and didn't want to use FeatureType as it 
+ * could get confused with org.geotools.data.FeatureType
+ * </p>
+ * @author dzwiers
+ *
+ */
+public class FeatureSetDescription {
+    public static final int NO_OPERATION = -1;
+    public static final int QUERY_OPERATION = 0;
+    public static final int INSERT_OPERATION = 1;
+    public static final int UPDATE_OPERATION = 2;
+    public static final int DELETE_OPERATION = 4;
+    public static final int LOCK_OPERATION = 8;
+    
+    private String name;
+    private String title;
+    private String _abstract;
+    private String SRS;
+    private String keywords;
+    private Envelope[] latLongBoundingBox;
+    private int operations;
+    private MetadataURL[] metadataURL;
+    /**
+     * @return Returns the abstracT.
+     */
+    public String getAbstract() {
+        return _abstract;
+    }
+    /**
+     * @param abstracT The abstracT to set.
+     */
+    public void setAbstract(String _abstract) {
+        this._abstract = _abstract;
+    }
+    /**
+     * @return Returns the keywords.
+     */
+    public String getKeywords() {
+        return keywords;
+    }
+    /**
+     * @param keywords The keywords to set.
+     */
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+    /**
+     * @return Returns the latLongBoundingBox.
+     */
+    public Envelope[] getLatLongBoundingBox() {
+        return latLongBoundingBox;
+    }
+    /**
+     * @param latLongBoundingBox The latLongBoundingBox to set.
+     */
+    public void setLatLongBoundingBox(Envelope[] latLongBoundingBox) {
+        this.latLongBoundingBox = latLongBoundingBox;
+    }
+    /**
+     * @return Returns the metadataURL.
+     */
+    public MetadataURL[] getMetadataURL() {
+        return metadataURL;
+    }
+    /**
+     * @param metadataURL The metadataURL to set.
+     */
+    public void setMetadataURL(MetadataURL[] metadataURL) {
+        this.metadataURL = metadataURL;
+    }
+    /**
+     * @return Returns the name.
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @param name The name to set.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /**
+     * @return Returns the operations.
+     */
+    public int getOperations() {
+        return operations;
+    }
+    /**
+     * @param operations The operations to set.
+     */
+    public void setOperations(int operations) {
+        this.operations = operations;
+    }
+    /**
+     * @return Returns the sRS.
+     */
+    public String getSRS() {
+        return SRS;
+    }
+    /**
+     * @param srs The sRS to set.
+     */
+    public void setSRS(String srs) {
+        SRS = srs;
+    }
+    /**
+     * @return Returns the title.
+     */
+    public String getTitle() {
+        return title;
+    }
+    /**
+     * @param title The title to set.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ gt/plugin/wfs/src/org/geotools/data/wfs/WFSCapabilities.java
@@ -0,0 +1,80 @@
+
+package org.geotools.data.wfs;
+
+import org.geotools.data.wms.getCapabilities.Service;
+
+/**
+ * <p> 
+ * DOCUMENT ME!
+ * </p>
+ * @author dzwiers
+ *
+ */
+public class WFSCapabilities {
+    private Service service; // no contact info provided
+    private FeatureSetDescription[] featureTypes;
+    private Capability[] capabilities;
+    private String vendorSpecificCapabilities;
+//    private FilterCapabilities filterCapabilities;
+    private Object filterCapabilities;
+    /**
+     * @return Returns the capabilities.
+     */
+    public Capability[] getCapabilities() {
+        return capabilities;
+    }
+    /**
+     * @param capabilities The capabilities to set.
+     */
+    public void setCapabilities(Capability[] capabilities) {
+        this.capabilities = capabilities;
+    }
+    /**
+     * @return Returns the featureTypes.
+     */
+    public FeatureSetDescription[] getFeatureTypes() {
+        return featureTypes;
+    }
+    /**
+     * @param featureTypes The featureTypes to set.
+     */
+    public void setFeatureTypes(FeatureSetDescription[] featureTypes) {
+        this.featureTypes = featureTypes;
+    }
+/**
+ * @return Returns the filterCapabilities.
+ */
+public Object getFilterCapabilities() {
+    return filterCapabilities;
+}
+/**
+ * @param filterCapabilities The filterCapabilities to set.
+ */
+public void setFilterCapabilities(Object filterCapabilities) {
+    this.filterCapabilities = filterCapabilities;
+}
+    /**
+     * @return Returns the service.
+     */
+    public Service getService() {
+        return service;
+    }
+    /**
+     * @param service The service to set.
+     */
+    public void setService(Service service) {
+        this.service = service;
+    }
+    /**
+     * @return Returns the vendorSpecificCapabilities.
+     */
+    public String getVendorSpecificCapabilities() {
+        return vendorSpecificCapabilities;
+    }
+    /**
+     * @param vendorSpecificCapabilities The vendorSpecificCapabilities to set.
+     */
+    public void setVendorSpecificCapabilities(String vendorSpecificCapabilities) {
+        this.vendorSpecificCapabilities = vendorSpecificCapabilities;
+    }
+}
