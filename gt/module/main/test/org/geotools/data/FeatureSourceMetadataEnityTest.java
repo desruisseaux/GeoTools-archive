f8205e13008199dfe56eb7cd3483a6df986e8b25
Jody Garnett <jody.garnett@gmail.com>
2004-08-23 18:21:15 +0000

    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@7591 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/module/main/test/org/geotools/data/FeatureSourceMetadataEnityTest.java
@@ -0,0 +1,26 @@
+package org.geotools.data;
+
+import org.geotools.data.memory.MemoryDataStore;
+
+public class FeatureSourceMetadataEnityTest extends DataTestCase {
+    MemoryDataStore store;
+    
+    public FeatureSourceMetadataEnityTest(String test) {
+        super(test);
+    }
+    protected void setUp() throws Exception {
+        super.setUp();
+        store = new MemoryDataStore();
+        store.addFeatures( roadFeatures );
+        store.addFeatures( riverFeatures );
+        store.addFeatures( lakeFeatures );                               
+    }
+    public void testRoadMeta(){
+        FeatureSourceMetadataEnity roadMeta =
+            new FeatureSourceMetadataEnity( store, "road" );
+        
+    }
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
