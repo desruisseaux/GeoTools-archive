f4d217c1a859bea145fca5703505f25b76fed592
dzwiers <>
2004-08-24 17:58:24 +0000

    wfs datastore skeletons + meta-inf ... does not compile
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@7619 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/plugin/wfs/src/org/geotools/data/wfs/WFSDataStore.java
@@ -0,0 +1,15 @@
+
+package org.geotools.data.wfs;
+
+import org.geotools.data.AbstractDataStore;
+
+/**
+ * <p> 
+ * DOCUMENT ME!
+ * </p>
+ * @author dzwiers
+ *
+ */
+public class WFSDataStore extends AbstractDataStore{
+
+}
---
+++ gt/plugin/wfs/src/org/geotools/data/wfs/WFSDataStoreFactory.java
@@ -0,0 +1,15 @@
+
+package org.geotools.data.wfs;
+
+import org.geotools.data.AbstractDataStoreFactory;
+
+/**
+ * <p> 
+ * DOCUMENT ME!
+ * </p>
+ * @author dzwiers
+ *
+ */
+public class WFSDataStoreFactory extends AbstractDataStoreFactory{
+
+}
