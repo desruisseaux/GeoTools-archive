a44e09da19074a3a2cb9b81927c96b8e34535bd2
Jesse Eichar <jesse.eichar@camptocamp.com>
2004-08-30 18:12:15 +0000

    ArcgridReader now works with gzip
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@7695 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/data/coverage/grid/stream/IOExchange.java
+++ gt/module/main/src/org/geotools/data/coverage/grid/stream/IOExchange.java
@@ -19,7 +19,10 @@
  */
 package org.geotools.data.coverage.grid.stream;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -34,13 +37,14 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
-
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 /**
- * This class is used to exchange between different input and output sources
- * and destinations If a format, for example arcgrid, needs a Reader object
- * then IOExchange will create one from the Source object
- *
+ * This class is used to exchange between different input and output sources and
+ * destinations If a format, for example arcgrid, needs a Reader object then
+ * IOExchange will create one from the Source object
+ * 
  * @author jeichar
  */
 public class IOExchange {
@@ -49,7 +53,7 @@ public class IOExchange {
 
     /**
      * Factory method for creating a new IOExchange
-     *
+     * 
      * @return an instance of a IOExchange
      */
     public static IOExchange getIOExchange() {
@@ -58,15 +62,17 @@ public class IOExchange {
 
     /**
      * Uses the source object to create a Reader object for client
-     *
-     * @param source An object that identifies an input Current Implementation
-     *        accepts: Reader, URL, String, File, InputStream
-     *
+     * 
+     * @param source
+     *            An object that identifies an input Current Implementation
+     *            accepts: Reader, URL, String, File, InputStream
+     * 
      * @return Returns a Reader that reads from the source identified by the
      *         source object
-     *
-     * @throws IOException If Source object cannot be identified or if an
-     *         unexpected problem occurs.
+     * 
+     * @throws IOException
+     *             If Source object cannot be identified or if an unexpected
+     *             problem occurs.
      */
     public Reader getReader(Object source) throws IOException {
         if (source instanceof Reader) {
@@ -99,7 +105,64 @@ public class IOExchange {
         }
 
         throw new IOException(
-            "Source Object is not recognized as one of the accepted source objects");
+                "Source Object is not recognized as one of the accepted source objects");
+    }
+
+    /**
+     * Returns a Reader that wraps a GZIPInputStream. WARNING. If source is a
+     * reader it assumes that the Reader is of the correct type. There is no way
+     * to be certain aside from trying to read the data.
+     * 
+     * @param source
+     *            An object that identifies an input Current Implementation.
+     *            accepts: Reader, URL, String, File, InputStream Note: if
+     *            source is a reader it MUST already wrap a GZIPInputStream.
+     * @return Reader that wraps a GZIPInputStream.
+     * @throws IOException
+     *             if the destination cannot be converted to a Writer
+     */
+    public Reader getGZIPReader(Object source) throws IOException {
+        InputStream in = null;
+        if (source instanceof Reader) {
+            return (Reader) source;
+        }
+
+        if (source instanceof GZIPInputStream) {
+            in = (InputStream) source;
+        }
+
+        if (source instanceof InputStream) {
+            in = new GZIPInputStream((InputStream) source);
+        }
+
+        if (source instanceof String) {
+            String urlString = (String) source;
+            URL url = new URL(urlString);
+
+            in = new GZIPInputStream(url.openStream());
+        }
+
+        if (source instanceof URL) {
+            URL url = (URL) source;
+            
+            in = new GZIPInputStream(url.openStream());
+        }
+
+        if (source instanceof File) {
+            in = new GZIPInputStream(new FileInputStream((File) source));
+        }
+
+        if (source instanceof ReadableByteChannel) {
+            in = new GZIPInputStream(Channels
+                    .newInputStream((ReadableByteChannel) source));
+        }
+
+        if (in != null)
+            return new InputStreamReader(new BufferedInputStream(in));
+        else
+            throw new IOException(
+                    "Source Object is not recognized as one of the accepted source objects");
+
     }
 
     /**
@@ -110,15 +173,17 @@ public class IOExchange {
      * Takes the destination object and casts it to one of: Writer (Returns
      * Writer) OutputStream (Returns OutputStreamWriter) String (Returns
      * OutputStreamWriter) URL (Returns OutputStreamWriter) File (Returns
-     * FileWriter)  It then constructs a new Writer out of the destination and
+     * FileWriter) It then constructs a new Writer out of the destination and
      * returns it.
      * </p>
-     *
-     * @param destination the destination object to be converted
-     *
+     * 
+     * @param destination
+     *            the destination object to be converted
+     * 
      * @return a new Writer that can write to the destination
-     *
-     * @throws IOException if the destination cannot be converted to a Writer
+     * 
+     * @throws IOException
+     *             if the destination cannot be converted to a Writer
      */
     public Writer getWriter(Object destination) throws IOException {
         if (destination instanceof Writer) {
@@ -133,13 +198,15 @@ public class IOExchange {
             String urlString = (String) destination;
             URL url = new URL(urlString);
 
-            return new OutputStreamWriter(url.openConnection().getOutputStream());
+            return new OutputStreamWriter(url.openConnection()
+                    .getOutputStream());
         }
 
         if (destination instanceof URL) {
             URL url = (URL) destination;
 
-            return new OutputStreamWriter(url.openConnection().getOutputStream());
+            return new OutputStreamWriter(url.openConnection()
+                    .getOutputStream());
         }
 
         if (destination instanceof File) {
@@ -147,24 +214,26 @@ public class IOExchange {
         }
 
         if (destination instanceof WritableByteChannel) {
-            return Channels.newWriter((WritableByteChannel) destination, "UTF-8");
+            return Channels.newWriter((WritableByteChannel) destination,
+                    "UTF-8");
         }
 
         throw new IOException(
-            "destination Object is not recognized as one of the accepted destination objects");
+                "destination Object is not recognized as one of the accepted destination objects");
     }
 
     /**
      * Takes a destination object and creates a PrintWriter from it
-     *
-     * @param destination the destination object to be converted
-     *
+     * 
+     * @param destination
+     *            the destination object to be converted
+     * 
      * @return a new PrintWriter that can write to the destination
-     *
-     * @throws IOException if the destination cannot be converted to a Writer
+     * 
+     * @throws IOException
+     *             if the destination cannot be converted to a Writer
      */
-    public PrintWriter getPrintWriter(Object destination)
-        throws IOException {
+    public PrintWriter getPrintWriter(Object destination) throws IOException {
         if (destination instanceof Writer) {
             return new PrintWriter((Writer) destination);
         }
@@ -198,15 +267,77 @@ public class IOExchange {
         }
 
         throw new IOException(
-            "destination Object is not recognized as one of the accepted destination objects");
+                "destination Object is not recognized as one of the accepted destination objects");
     }
 
     /**
-     * When implemented this method will determine the format of a source
-     * object
-     *
-     * @param arg0 The source object
-     *
+     * Returns a PrintWriter that wraps a GZIPOutputStream. WARNING. If source
+     * is a PrintWriter or a Writer it assumes that stream is of the correct
+     * type. There is no way to be certain aside from trying to write the data.
+     * 
+     * @param source
+     *            An object that identifies an input Current Implementation.
+     *            accepts: Writer, URL, String, File, OutputStream Note: if
+     *            source is a Writer it MUST already wrap a GZIPOutputStream.
+     * @return Writer that wraps a GZIPOutputStream.
+     * @throws IOException
+     *             if the destination cannot be converted to a Writer
+     */
+    public PrintWriter getGZIPPrintWriter(Object destination)
+            throws IOException {
+        if (destination instanceof PrintWriter) {
+            return (PrintWriter) destination;
+        }
+        if (destination instanceof Writer) {
+            return new PrintWriter((Writer) destination);
+        }
+
+        if (destination instanceof GZIPOutputStream) {
+            return new PrintWriter((OutputStream) destination);
+        }
+
+        if (destination instanceof OutputStream) {
+            return new PrintWriter(new GZIPOutputStream(
+                    (OutputStream) destination));
+        }
+
+        if (destination instanceof String) {
+            String urlString = (String) destination;
+            URL url = new URL(urlString);
+            OutputStream out = new GZIPOutputStream(url.openConnection()
+                    .getOutputStream());
+            return new PrintWriter(out);
+        }
+
+        if (destination instanceof URL) {
+            URL url = (URL) destination;
+            OutputStream out = new GZIPOutputStream(url.openConnection()
+                    .getOutputStream());
+            return new PrintWriter(out);
+        }
+
+        if (destination instanceof File) {
+            OutputStream out = new GZIPOutputStream(new FileOutputStream(
+                    (File) destination));
+            return new PrintWriter(out);
+        }
+
+        if (destination instanceof WritableByteChannel) {
+            OutputStream out = new GZIPOutputStream(Channels
+                    .newOutputStream((WritableByteChannel) destination));
+            return new PrintWriter(out);
+        }
+
+        throw new IOException(
+                "destination Object is not recognized as one of the accepted destination objects");
+    }
+
+    /**
+     * When implemented this method will determine the format of a source object
+     * 
+     * @param arg0
+     *            The source object
+     * 
      * @return an integer indicating a particular format
      */
     public static int determineSourceFormat(Object arg0) {
--- gt/plugin/arcgrid/test/org/geotools/data/arcgrid/test/ArcGridReaderTest.java
+++ gt/plugin/arcgrid/test/org/geotools/data/arcgrid/test/ArcGridReaderTest.java
@@ -4,9 +4,16 @@
 package org.geotools.data.arcgrid.test;
 
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Locale;
 
+import org.geotools.data.arcgrid.ArcGridFormatFactory;
 import org.geotools.data.arcgrid.ArcGridReader;
-
+import org.geotools.data.coverage.grid.Format;
+import org.geotools.data.coverage.grid.GridCoverageReader;
+import org.opengis.parameter.GeneralOperationParameter;
+import org.opengis.parameter.GeneralParameterValue;
+import org.opengis.parameter.ParameterValue;
 
 /**
  * 
@@ -15,38 +22,73 @@ import org.geotools.data.arcgrid.ArcGridReader;
  */
 public class ArcGridReaderTest extends TestCaseSupport {
 
-	ArcGridReader reader;
-	String TESTFILE="ArcGrid.asc";
-	InputStream in;
+    GridCoverageReader reader;
 
-	/**
-	 * @param name
-	 */
-	public ArcGridReaderTest(String name) {
-		super(name);
-	}
-	/*
-	 * @see TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
-		in=getTestResourceAsStream(TESTFILE);
-		reader=new ArcGridReader(in);
-	}
-	/*
-	 * @see TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
-	public void testGetSource() {
-		assertEquals(in,reader.getSource());
-	}
-	/*
-	 * Class to test for GridCoverage read( Parameter[])
-	 */
-	public void testReadStringParameterArray() throws Exception{
-			assertNotNull(reader.read(null));		
-	}
+    String TESTFILE = "ArcGrid.asc";
+
+    String GZIP_TESTFILE = "spearfish_dem.asc.gz";
+    
+    Format format;
+
+    URL url;
+
+    /**
+     * @param name
+     */
+    public ArcGridReaderTest(String name) {
+        super(name);
+    }
+
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        url = getTestResource(TESTFILE);
+        format=(new ArcGridFormatFactory()).createFormat();
+        reader = format.getReader(url);
+    }
+
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testGetSource() {
+        assertEquals(url, reader.getSource());
+    }
+
+    /*
+     * Class to test for GridCoverage read( Parameter[]) not gzipped and not
+     * GRASS format
+     */
+    public void testReadStringParameterArray() throws Exception {
+        assertNotNull(reader.read(null));
+    }
+
+    /*
+     * Class to test for GridCoverage read( Parameter[]) gzipped but not GRASS
+     * format
+     */
+    public void testGZIPReadStringParameterArray() throws Exception {
+        reader=format.getReader(getTestResource(GZIP_TESTFILE));
+        GeneralOperationParameter[] params = format.getReadParameters();
+        GeneralParameterValue[] pvalues =new ParameterValue[2];
+        ParameterValue value;
+        for (int i = 0; i < params.length; i++) {
+            if( params[i].getName(Locale.ENGLISH).equalsIgnoreCase("GRASS")){
+                value=(ParameterValue)params[i].createValue();
+                value.setValue(true);
+                pvalues[0]=value;
+            }
+            if( params[i].getName(Locale.ENGLISH).equalsIgnoreCase("Compressed")){
+                value=(ParameterValue)params[i].createValue();
+                value.setValue(true);
+                pvalues[1]=value;
+            }
+        }
+    }
 
 }
