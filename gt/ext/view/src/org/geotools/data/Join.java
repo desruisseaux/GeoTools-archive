4ca3213498f6c8b1a84209be8213757e2e50e486
Jody Garnett <jody.garnett@gmail.com>
2004-09-17 00:02:39 +0000

    Commented
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@8053 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/ext/view/src/org/geotools/data/Join.java
+++ gt/ext/view/src/org/geotools/data/Join.java
@@ -1,22 +1,15 @@
-/*
- * Created on 16-Sep-2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package org.geotools.data;
 
 import java.net.URI;
 
 /**
  * @author dzwiers
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
-public abstract class Join extends NewQuery{
-	
-	// used to return the set of inputs to the join
+public abstract class Join extends NewQuery{	
+	/**
+     * Used to return the set of inputs to the join.
+     * 
+     */
 	public abstract NewQuery[] getQueries();
 
     /**
--- gt/ext/view/src/org/geotools/data/NewQuery.java
+++ gt/ext/view/src/org/geotools/data/NewQuery.java
@@ -1,9 +1,3 @@
-/*
- * Created on 14-Sep-2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package org.geotools.data;
 
 import java.net.URI;
@@ -16,14 +10,19 @@ import org.geotools.filter.Filter;
  *
  */
 public abstract class NewQuery {
-
     
     /**
      * TODO: should this be ANY_URI
+     * (JG:xml is in main so you could point to it)
      */
 	public static final URI NO_NAMESPACE = null;
     
-    /** So getMaxFeatures does not return null we use a very large number. */
+    /**
+     * So getMaxFeatures does not return null we use a very large number.
+     * 
+     * TODO: The existing implementation does that - I would perfer to use -1 to prevent
+     * any misunderstanding.
+     */
     public static final int DEFAULT_MAX = Integer.MAX_VALUE;
     
     /**
@@ -31,6 +30,7 @@ public abstract class NewQuery {
      * <p>
      * Note the query will still return a result - limited to FeatureIDs.
      * </p>
+     * (JG: This looks like a great improvement)
      */
     public static final QueryAs[] NO_ATTRIBUTES = new QueryAs[0];
     
@@ -40,7 +40,10 @@ public abstract class NewQuery {
     public static final QueryAs[] ALL_ATTRIBUTES = null;
 
     /**
-     * The resulting property names from executing this query (note: output name/value may be the result of an expression ... @see QueryAs). 
+     * The resulting property names from executing this query (note: output name/value may be the result of an expression ... @see QueryAs).
+     * <p>
+     * Note this is a convience method - does not *have* to be here.
+     * </p> 
      */
     public String[] getPropertyNames(){
     	QueryAs[] qa = getProperties();
@@ -53,17 +56,24 @@ public abstract class NewQuery {
     }
     
     /**
-     * The property mappings for this query (note: output name/value may be the result of an expression ... @see QueryAs).
+     * The property mappings for this query.
+     * <p>
+     * Note: output name/value may be the result of an expression ...
+     * 
+     * @see QueryAs
+     * </p>
      */
     public abstract QueryAs[] getProperties();
 
     /**
      * Convenience method to determine if the query should use the full schema
-     * (all properties) of the data source for the features returned.  This
-     * method is equivalent to if (query.getProperties() == null), but allows
+     * (all properties) of the data source for the features returned.
+     * <p>
+     * This method is equivalent to if (query.getProperties() == null), but allows
      * for more clarity on the part of datasource implementors, so they do not
      * need to examine and use null values.  All Query implementations should
      * return true for this function if getProperties returns null.
+     *</p>
      *
      * @return if all datasource attributes should be included in the schema of
      *         the returned FeatureCollection.
@@ -89,10 +99,23 @@ public abstract class NewQuery {
     public abstract int getMaxFeatures();
 
     /**
+     * A starting offset (taken from Catalog Specification)
+     * <p>
+     * This recomendation allows us to step through feature content in bite sized
+     * chunks (well getMaxFeatures() sized chunks).
+     * </p>
+     * 
+     * @return starting offset (counting from 0)
+     */
+    public abstract int getStartingOffset();
+    
+    /**
      * The Filter can be used to define constraints on a query.  If no Filter
      * is present then the query is unconstrained and all feature instances
      * should be retrieved.
-     *
+     * <p>
+     * TODO: This is a Filter as defined by the OGC Filter Specification 58XXXX
+     * </p>
      * @return The filter that defines constraints on the query.
      */
     public abstract Filter getFilter();
@@ -117,14 +140,25 @@ public abstract class NewQuery {
 
     /**
      * aaime -- this is you hint spot :)
+     * <p>
+     * interface magic extends wish {
+     *    CoordinateReferenceSystem force();
+     *    CoordinateReferenceSystem reproject();
+     * }
+     * </p>
      */
     public abstract Object getCoordinateSystemReprojectHint();
     
+    /**
+     * (This idea is the best!)
+     * 
+     * @author davidz
+     */
     public static class QueryAs{
     	// assume the same namespace as the query ...
     	
-    	// the output typename
-    	private String typeName;
+    	// the output attributeName
+    	private String attributeName;
     	
     	// used to derive a property value ... this allows simple attribute mangling or selection ...
     	private Expression formula;
@@ -142,10 +176,10 @@ public abstract class NewQuery {
 			return formula;
 		}
 		/**
-		 * @return Returns the typeName.
+		 * @return Returns the attributeName.
 		 */
-		public String getTypeName() {
-			return typeName;
+		public String getAttributeName() {
+			return attributeName;
 		}
     }
 }
