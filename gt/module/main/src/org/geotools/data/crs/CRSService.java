7782a5cb383d8325c8eec4c4912a322f4710db76
Jody Garnett <jody.garnett@gmail.com>
2004-09-23 22:47:54 +0000

    moved CRSService into main
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@8188 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/module/main/src/org/geotools/data/crs/CRSService.java
@@ -0,0 +1,145 @@
+/*
+ * Geotools 2 - OpenSource mapping toolkit
+ * (C) 2004, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.geotools.data.crs;
+
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import org.geotools.cs.AxisInfo;
+import org.geotools.cs.CSAUTOFactory;
+import org.geotools.cs.CSEPSGFactory;
+import org.geotools.cs.CoordinateSystem;
+import org.geotools.cs.CoordinateSystemAuthorityFactory;
+import org.geotools.cs.CoordinateSystemFactory;
+import org.geotools.cs.Ellipsoid;
+import org.geotools.cs.GeographicCoordinateSystem;
+import org.geotools.cs.HorizontalDatum;
+import org.geotools.cs.NoSuchAuthorityCodeException;
+import org.geotools.cs.PrimeMeridian;
+import org.geotools.pt.CoordinatePoint;
+import org.geotools.resources.CTSUtilities;
+import org.geotools.units.Unit;
+
+/**
+ * Utility method isolating data source providers from CRS production.
+ * <p>
+ * This should be reworked as a Martins new CoordianteReferenceSystem work
+ * comes along, it is factory based an should take care of most of the functionality
+ * of this module.
+ * </p>
+ * @author Jody Garnett, Refractions Research
+ */
+public class CRSService {
+    
+    /**
+     * List of CoordinateSystemAuthorityFactory.
+     * <p>
+     * With a normal geotools install plugins exist for AUTO and EPSG.
+     * </p>
+     */ 
+	List authorities = new ArrayList();
+	
+	/**
+	 * Default Reprojection Service using EPSG and AUTO.
+	 */
+	public CRSService(){
+		register( CSAUTOFactory.getDefault() );
+		register( CSEPSGFactory.getDefault() );		
+	}
+		
+	public void register( CoordinateSystemAuthorityFactory factory ){
+	    authorities.add( factory );			
+	}
+		
+	/**
+	 * Locate for CoordinateSystem for specific code.
+	 * <p>
+	 * Note the code needs to mention the authority.
+	 * <pre><code>
+	 * EPSG:1234
+	 * AUTO:42001, ..., ..., ...
+	 * </code></pre>
+	 * </p>
+	 * </p>
+	 * Due to common use EPSG is now assumed as the authority
+	 * if not otherwise specified.
+	 * @param code
+	 * @return coordinate system for the provided code
+	 * @throws FactoryException
+	 */
+	CoordinateSystem createCoordianteSystem( String code ) throws FactoryException{
+		int split = code.indexOf(":");
+		String authority = "EPSG";
+		if( split != -1 ){
+		    authority = code.substring(0,split);
+			code = code.substring( split+1 );
+		}
+		NoSuchAuthorityCodeException noCodeException = null;
+		for( Iterator i=authorities.iterator(); i.hasNext(); ){
+		    CoordinateSystemAuthorityFactory factory =
+		        (CoordinateSystemAuthorityFactory) i.next();
+		    
+		    if( !factory.getAuthority().equals( authority ) ){
+		        continue;
+		    }
+		    try {
+		        CoordinateSystem cs = factory.createCoordinateSystem( code );
+		        if( cs != null ) return cs;		        
+		    }
+		    catch (NoSuchAuthorityCodeException notFound){
+		        noCodeException = notFound;
+		        continue;
+		    }		    
+		}
+		if( noCodeException== null ){
+		    noCodeException = new NoSuchAuthorityCodeException( "Unabled to locate definition of '"+code+"'");
+		}
+		throw noCodeException;
+	}
+		
+	/**
+	 * Locate for CoordinateReferenceSystem for specific code.
+	 * <p>
+	 * Note the code needs to mention the authority.
+	 * <pre><code>
+	 * EPSG:1234
+	 * AUTO:42001, ..., ..., ...
+	 * </code></pre>
+	 * </p>
+	 * </p>
+	 * Due to common use EPSG is now assumed as the authority
+	 * if not otherwise specified.
+	 * @param code
+	 * @return coordinate system for the provided code
+	 * @throws FactoryException
+	 */	
+	public CoordinateReferenceSystem createCRS( String code ) throws FactoryException {
+	    return createCoordianteSystem( code );
+	}
+}
