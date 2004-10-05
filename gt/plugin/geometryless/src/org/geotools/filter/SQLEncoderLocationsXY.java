9307c064feaed1de06041b406f8deac6826e3af2
Rob Atkinson <>
2004-10-05 12:07:45 +0000

    Added SQLEncoder in filter - passes tests but not yet working in geoserver
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@8474 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/plugin/geometryless/src/org/geotools/filter/SQLEncoderLocationsXY.java
@@ -0,0 +1,183 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
+package org.geotools.filter;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Envelope;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+
+/**
+ * Encodes a filter into a SQL WHERE statement for MySQL.  This class adds
+ * the ability to turn geometry filters into sql statements if they are
+ * bboxes.
+ *
+ * @author Rob Atkinson , SCO
+ * @author Debasish Sahu, debasish.sahu@rmsi.com
+ * 
+ */
+public class SQLEncoderLocationsXY extends SQLEncoder
+    implements org.geotools.filter.FilterVisitor {
+    /** Standard java logger */
+    private static Logger LOGGER = Logger.getLogger("org.geotools.filter");
+
+    /**
+     * The filters that this encoder can processed. (Note this value shadows
+     * private capabilities in superclass)
+     */
+    private FilterCapabilities capabilities = new FilterCapabilities();
+
+    /**
+     * The srid of the schema, so the bbox conforms.  Could be better to have
+     * it in the bbox filter itself, but this works for now.
+     */
+    private int srid;
+
+  // names of sql addressable columns containing numerical coordinates
+   private String xcolumn = null;
+   private String ycolumn = null;
+   
+    /** The geometry attribute to use if none is specified. */
+    private String defaultGeom;
+
+    /**
+     * Empty constructor TODO: rethink empty constructor, as BBOXes _need_ an
+     * SRID, must make client set it somehow.  Maybe detect when encode is
+     * called?
+     */
+    public SQLEncoderLocationsXY(String xcolumn, String ycolumn) {
+        capabilities = createFilterCapabilities();
+        this.xcolumn = xcolumn;
+        this.ycolumn = ycolumn;
+
+        setColnameEscape("");
+    }
+
+    public SQLEncoderLocationsXY(int srid) {
+        this.srid = srid;
+    }
+
+    /**
+     * @see org.geotools.filter.SQLEncoder#createFilterCapabilities()
+     */
+    protected FilterCapabilities createFilterCapabilities() {
+        FilterCapabilities capabilities = new FilterCapabilities();
+
+        capabilities.addType(AbstractFilter.LOGIC_OR);
+        capabilities.addType(AbstractFilter.LOGIC_AND);
+        capabilities.addType(AbstractFilter.LOGIC_NOT);
+        capabilities.addType(AbstractFilter.COMPARE_EQUALS);
+        capabilities.addType(AbstractFilter.COMPARE_NOT_EQUALS);
+        capabilities.addType(AbstractFilter.COMPARE_LESS_THAN);
+        capabilities.addType(AbstractFilter.COMPARE_GREATER_THAN);
+        capabilities.addType(AbstractFilter.COMPARE_LESS_THAN_EQUAL);
+        capabilities.addType(AbstractFilter.COMPARE_GREATER_THAN_EQUAL);
+        capabilities.addType(AbstractFilter.NULL);
+        capabilities.addType(AbstractFilter.BETWEEN);
+        capabilities.addType((short) 12345);
+        capabilities.addType((short) -12345);
+        capabilities.addType(AbstractFilter.GEOMETRY_BBOX);
+        capabilities.addType(AbstractFilter.FID);
+
+        return capabilities;
+    }
+
+    /**
+     * Sets a spatial reference system ESPG number, so that the geometry can be
+     * properly encoded for postgis.  If geotools starts actually creating
+     * geometries with valid srids then this method will no longer be needed.
+     *
+     * @param srid the integer code for the EPSG spatial reference system.
+     */
+    public void setSRID(int srid) {
+        this.srid = srid;
+    }
+
+    /**
+     * Sets the default geometry, so that filters with null for one of their
+     * expressions can assume that the default geometry is intended.
+     *
+     * @param name the name of the default geometry Attribute.
+     *
+     * @task REVISIT: pass in a featureType so that geometries can figure out
+     *       their own default geometry?
+     */
+    public void setDefaultGeometry(String name) {
+        //Do we really want clients to be using malformed filters?  
+        //I mean, this is a useful method for unit tests, but shouldn't 
+        //fully formed filters usually be used?  Though I guess adding 
+        //the option wouldn't hurt. -ch
+        this.defaultGeom = name;
+    }
+
+    /**
+     * Turns a geometry filter into the postgis sql bbox statement.
+     *
+     * @param filter the geometry filter to be encoded.
+     *
+     * @throws RuntimeException for IO exception (need a better error)
+     */
+    public void visit(GeometryFilter filter) throws RuntimeException {
+        LOGGER.finer("exporting GeometryFilter");
+		System.out.println("exporting GeometryFilter");
+
+        if (filter.getFilterType() == AbstractFilter.GEOMETRY_BBOX) {
+            DefaultExpression left = (DefaultExpression) filter.getLeftGeometry();
+            DefaultExpression right = (DefaultExpression) filter
+                .getRightGeometry();
+
+            // left and right have to be valid expressions
+            try {
+                /*   if (left == null) {
+                   out.write(defaultGeom);
+                   } else {
+                       left.accept(this);
+                   }*/
+                if (right == null) {
+                    out.write(defaultGeom);
+                } else {
+                    right.accept(this);
+                }
+            } catch (java.io.IOException ioe) {
+                LOGGER.warning("Unable to export filter" + ioe);
+            }
+        } else {
+            LOGGER.warning("exporting unknown filter type, only bbox supported");
+            throw new RuntimeException("Only BBox is currently supported");
+        }
+    }
+
+    /**
+     * Checks to see if the literal is a geometry, and encodes it if it  is, if
+     * not just sends to the parent class.
+     *
+     * @param expression the expression to visit and encode.
+     *
+     * @throws IOException for IO exception (need a better error)
+     */
+    public void visitLiteralGeometry(LiteralExpression expression)
+        throws IOException {
+        Geometry bbox = (Geometry) expression.getLiteral();
+        Envelope e = bbox.getEnvelopeInternal();
+        double x1 = e.getMinX();
+       double x2 = e.getMaxX();
+        double y1 = e.getMinY();
+        double y2 = e.getMaxY();
+       out.write( "( " +  xcolumn  + " < " + x2 + " and " + xcolumn + " > " + x1 + " and " +  ycolumn  + " < " + y2 + " and " + ycolumn + " > " + y1 + " )"        );
+    }
+}
---
+++ gt/plugin/geometryless/test/org/geotools/data/geometryless/JDBCDataStoreFactoryTest.java
@@ -0,0 +1,140 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
+package org.geotools.data.geometryless;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PropertyResourceBundle;
+import junit.framework.TestCase;
+
+import org.geotools.data.DataSourceException;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFactorySpi.Param;
+
+/**
+ * Test Params used by JDBCDataStoreFactory.
+ * 
+ * @auther Rob Atkinson, Social Change Online
+ * @author jgarnett, Refractions Research, Inc.
+ * @author $Author: aaime $ (last modification)
+ * @version $Id:JDBCDataStoreFactoryTest.java,v 1.1.2.1 2004/05/09 15:35:34 aaime Exp $
+ */
+public class JDBCDataStoreFactoryTest extends TestCase {
+    static JDBCDataStoreFactory factory
+        = new JDBCDataStoreFactory();
+    
+    Map local;
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+  
+        
+      local = new HashMap();
+        local.put("dbtype","jdbc");        
+  
+        
+         PropertyResourceBundle resource =
+            new PropertyResourceBundle(this.getClass().getResourceAsStream("fixture.properties"));
+
+        String namespace = resource.getString("namespace");
+        String host = resource.getString("host");
+              local.put("host",host);
+              
+        String port = resource.getString("port");
+               local.put("port", port);
+               
+        String database = resource.getString("database");
+        local.put("database",database);
+
+        String user = resource.getString("user");
+               local.put("user", user);
+        String password = resource.getString("password");
+            local.put("passwd", password);
+       
+             String Driver = resource.getString("driver");
+            local.put("driver", Driver);
+
+             String urlprefix = resource.getString("urlprefix");
+            local.put("urlprefix", urlprefix);
+
+            
+        if (namespace.equals("http://www.geotools.org/data/postgis")) {
+            throw new IllegalStateException(
+                "The fixture.properties file needs to be configured for your own database");
+        }
+        
+        super.setUp();
+
+    }
+    
+    public void testParamCHARSET() throws Throwable {
+        Param p = JDBCDataStoreFactory.CHARSET;
+        try {
+            p.parse(null);
+            fail("expected error for parse null");
+        }
+        catch( Exception e){}
+        
+        try {
+            p.parse("");
+            fail("expected error for parse empty");
+        }
+        catch( Exception e){}
+        assertNotNull( "parse ISO-8859-1", p.parse("ISO-8859-1"));
+        
+        assertNull("handle null", p.handle(null) );
+        assertNull("handle empty", p.handle("") );
+        assertNotNull( "handle ISO-8859-1", p.handle("ISO-8859-1"));
+        
+        Map map = new HashMap();
+        Charset latin1=Charset.forName("ISO-8859-1");
+        map.put("charset", latin1 );
+        assertEquals( latin1, p.lookUp( map ));
+        
+        try {
+            assertNotNull( "handle ISO-LATIN-1", p.handle("ISO-LATIN-1"));            
+        } catch (IOException expected){            
+        }
+        System.out.println( latin1.toString() );
+        System.out.println( latin1.name() );
+        System.out.println( p.text( latin1 ));
+        assertEquals("ISO-8859-1", p.text(latin1) );
+        try {
+            assertEquals("ISO-8859-1", p.text("ISO-8859-1") );
+            fail("Should not handle bare text");
+        }
+        catch( ClassCastException expected ){            
+        }
+    }
+    public void testLocal() throws Exception {
+        Map map = local;
+        System.out.println( "local:"+map );
+ 
+        assertTrue( "canProcess", factory.canProcess(map));
+        try {
+            DataStore temp = factory.createDataStore(map);
+            assertNotNull( "created", temp );
+        }
+        catch( DataSourceException expected){
+            assertEquals("Could not get connection",expected.getMessage());
+        }                        
+    }
+ 
+}
