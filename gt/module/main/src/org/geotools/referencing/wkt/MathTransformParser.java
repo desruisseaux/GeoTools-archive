1f61dc951ea4e91052e25c1275333b2bd12b0c12
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2004-10-27 04:50:54 +0000

    Fixed compilation error caused by GeoAPI change
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@8876 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/referencing/wkt/MathTransformParser.java
+++ gt/module/main/src/org/geotools/referencing/wkt/MathTransformParser.java
@@ -17,7 +17,6 @@
  *    License along with this library; if not, write to the Free Software
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package org.geotools.referencing.wkt;
 
 // J2SE dependencies
@@ -160,7 +159,9 @@ public final class MathTransformParser extends AbstractParser {
         }
         element.close();
         try {
-            return mtFactory.createParameterizedTransform(classification, parameters);
+            return mtFactory.createParameterizedTransform(
+                new org.geotools.parameter.ParameterGroup(
+                Collections.singletonMap("name", classification), parameters));
         } catch (FactoryException exception) {
             throw element.parseFailed(exception, null);
         }
