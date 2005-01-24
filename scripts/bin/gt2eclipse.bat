c9ffadd7877ade76bb0d81ac38b05b9d0a6516f5
Richard Gould <rgould@refractions.net>
2005-01-24 20:37:02 +0000

    Fixed issue with spaces in JAVA_HOME var
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@10746 e5c1c795-43da-0310-a71f-fac65c449510

--- scripts/bin/gt2eclipse.bat
+++ scripts/bin/gt2eclipse.bat
@@ -2,4 +2,4 @@
 
 REM Lets find out where we are
 set BASE_DIR=%~dp0\..
-%JAVA_HOME%\bin\java.exe -cp %BASE_DIR%\target\classes GT2Eclipse %1 %2 %3 %4 %5
+"%JAVA_HOME%\bin\java.exe" -cp %BASE_DIR%\target\classes GT2Eclipse %1 %2 %3 %4 %5
