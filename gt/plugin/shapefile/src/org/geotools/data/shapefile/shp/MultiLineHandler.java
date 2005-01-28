9d143ea689ed05fd5c4f28f2ea1b0146c4010361
dzwiers <>
2005-01-28 22:08:09 +0000

    crs
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@10820 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/shapefile/src/org/geotools/data/shapefile/shp/MultiLineHandler.java
+++ gt/plugin/shapefile/src/org/geotools/data/shapefile/shp/MultiLineHandler.java
@@ -22,16 +22,14 @@ package org.geotools.data.shapefile.shp;
 
 import java.nio.ByteBuffer;
 
-<<<<<<< .mine
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.CoordinateSequence;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.MultiLineString;
-=======
 import org.geotools.geometry.coordinatesequence.CSBuilder;
 import org.geotools.geometry.coordinatesequence.CSBuilderFactory;
->>>>>>> .r10795
 
 
 /*
