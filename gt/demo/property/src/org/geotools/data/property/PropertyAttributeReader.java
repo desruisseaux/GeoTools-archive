cb2eed1e219d0f723e0b9026a72181fa250b285f
Gabriel Roldan <gabriel.roldan@gmail.com>
2005-02-04 14:34:15 +0000

    fixed bug in parsing geometry attributes
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@11103 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/demo/property/src/org/geotools/data/property/PropertyAttributeReader.java
+++ gt/demo/property/src/org/geotools/data/property/PropertyAttributeReader.java
@@ -1,3 +1,19 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
 package org.geotools.data.property;
 
 import java.io.BufferedReader;
@@ -12,105 +28,231 @@ import org.geotools.data.DataUtilities;
 import org.geotools.feature.AttributeType;
 import org.geotools.feature.FeatureType;
 import org.geotools.feature.SchemaException;
+
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
+
+
 /**
  * Simple AttributeReader that works against Java properties files.
+ * 
  * <p>
- * This AttributeReader is part of the geotools2 DataStore tutorial, and
- * should be considered a Toy.
+ * This AttributeReader is part of the geotools2 DataStore tutorial, and should
+ * be considered a Toy.
  * </p>
+ * 
  * <p>
- * The content of this file should start with a the property "_" with the
- * value being the typeSpec describing the featureType. Thereafter each line
- * will should have a FeatureID as the property and the attribtues
- * as the value separated by | characters.
- * </p> 
+ * The content of this file should start with a the property "_" with the value
+ * being the typeSpec describing the featureType. Thereafter each line will
+ * should have a FeatureID as the property and the attribtues as the value
+ * separated by | characters.
+ * </p>
  * <pre><code>
  * _=id:Integer|name:String|geom:Geometry
  * fid1=1|Jody|<i>well known text</i>
  * fid2=2|Brent|<i>well known text</i>
  * fid3=3|Dave|<i>well known text</i>
  * </code></pre>
+ *
  * @author jgarnett
  */
 public class PropertyAttributeReader implements AttributeReader {
+    /** DOCUMENT ME!  */
+    private static final WKTReader wktReader = new WKTReader(new GeometryFactory());
+
+    /** DOCUMENT ME! */
     BufferedReader reader;
+
+    /** DOCUMENT ME! */
     FeatureType type;
+
+    /** DOCUMENT ME! */
     String line;
+
+    /** DOCUMENT ME! */
     String next;
-    
-    public PropertyAttributeReader( File file ) throws IOException {
-        String typeName = typeName( file );
-        String namespace = namespace( file );    
-        reader = new BufferedReader( new FileReader( file )  );
-        line = reader.readLine();                        
-        if( line == null || !line.startsWith("_=")){
-            throw new IOException( typeName+" schema not available" );            
+
+    /**
+     * Creates a new PropertyAttributeReader object.
+     *
+     * @param file DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     * @throws DataSourceException DOCUMENT ME!
+     */
+    public PropertyAttributeReader(File file) throws IOException {
+        String typeName = typeName(file);
+        String namespace = namespace(file);
+        reader = new BufferedReader(new FileReader(file));
+        line = reader.readLine();
+
+        if ((line == null) || !line.startsWith("_=")) {
+            throw new IOException(typeName + " schema not available");
         }
+
         String typeSpec = line.substring(2);
-        try {            
-            type = DataUtilities.createType( namespace+typeName, typeSpec );
-            
+
+        try {
+            type = DataUtilities.createType(namespace + typeName, typeSpec);
         } catch (SchemaException e) {
-            throw new DataSourceException( typeName+" schema not available", e );
-        }        
+            throw new DataSourceException(typeName + " schema not available", e);
+        }
+
         line = null;
-        next = null;        
+        next = null;
     }
-    private static String typeName( File file ){
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param file DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    private static String typeName(File file) {
         String name = file.getName();
         int split = name.lastIndexOf('.');
-        return split == -1 ? name : name.substring(0, split );        
+
+        return (split == -1) ? name : name.substring(0, split);
     }
-    private static String namespace( File file ){
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param file DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    private static String namespace(File file) {
         File parent = file.getParentFile();
-        return parent == null ? "" : parent.getName()+".";
+
+        return (parent == null) ? "" : (parent.getName() + ".");
     }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
     public int getAttributeCount() {
         return type.getAttributeCount();
-    }        
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param index DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws ArrayIndexOutOfBoundsException DOCUMENT ME!
+     */
     public AttributeType getAttributeType(int index)
         throws ArrayIndexOutOfBoundsException {
-        return type.getAttributeType( index );
-        
+        return type.getAttributeType(index);
     }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     */
     public void close() throws IOException {
         reader.close();
         reader = null;
     }
-    public boolean hasNext() throws IOException {        
-        if( next != null){
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     */
+    public boolean hasNext() throws IOException {
+        if (next != null) {
             return true;
         }
+
         next = reader.readLine();
+
         return next != null;
     }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     * @throws NoSuchElementException DOCUMENT ME!
+     */
     public void next() throws IOException {
-        if( hasNext() ){
+        if (hasNext()) {
             line = next;
             next = null;
-        }
-        else {
-            throw new NoSuchElementException();        
+        } else {
+            throw new NoSuchElementException();
         }
     }
-    public String getFeatureID(){
-        if( line == null ){
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    public String getFeatureID() {
+        if (line == null) {
             return null;
         }
+
         int split = line.indexOf('=');
-        if( split == -1){
+
+        if (split == -1) {
             return null;
         }
-        return line.substring( 0, split );        
+
+        return line.substring(0, split);
     }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param index DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     * @throws ArrayIndexOutOfBoundsException DOCUMENT ME!
+     * @throws DataSourceException DOCUMENT ME!
+     */
     public Object read(int index)
         throws IOException, ArrayIndexOutOfBoundsException {
-        if( line == null ){
-            throw new IOException( "No content available - did you remeber to call next?" );            
+        if (line == null) {
+            throw new IOException(
+                "No content available - did you remeber to call next?");
         }
+
         int split = line.indexOf('=');
-        String fid = line.substring( 0, split );
-        String text[] = line.substring( split+1 ).split("\\|");
-        return type.getAttributeType( index ).parse( text[ index ] );
+        String fid = line.substring(0, split);
+        String[] text = line.substring(split + 1).split("\\|");
+
+        AttributeType attType = type.getAttributeType(index);
+
+        String stringValue = text[index];
+
+        Object value = null;
+
+        if (attType.isGeometry()) {
+            try {
+                value = wktReader.read(stringValue);
+            } catch (ParseException e) {
+                throw new DataSourceException("Can't parse WKT for fid#" + fid,
+                    e);
+            }
+        } else {
+            value = attType.parse(stringValue);
+        }
+
+        return value;
     }
 }
--- gt/demo/property/src/org/geotools/data/property/PropertyFeatureReader.java
+++ gt/demo/property/src/org/geotools/data/property/PropertyFeatureReader.java
@@ -1,39 +1,112 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
 package org.geotools.data.property;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.NoSuchElementException;
 
 import org.geotools.data.FeatureReader;
 import org.geotools.feature.Feature;
 import org.geotools.feature.FeatureType;
 import org.geotools.feature.IllegalAttributeException;
+import java.io.File;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.logging.Logger;
 
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author jgarnett
+ * @version $Id
+ */
 public class PropertyFeatureReader implements FeatureReader {
-    PropertyAttributeReader reader;    
-    public PropertyFeatureReader( File directory, String typeName ) throws IOException {
-        File file = new File( directory, typeName+".properties");
-        reader = new PropertyAttributeReader( file );                        
+	private static final Logger LOGGER = Logger.getLogger("org.geotools.data.property");
+    /** DOCUMENT ME! */
+    PropertyAttributeReader reader;
+
+    /**
+     * Creates a new PropertyFeatureReader object.
+     *
+     * @param directory DOCUMENT ME!
+     * @param typeName DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     */
+    public PropertyFeatureReader(File directory, String typeName)
+        throws IOException {
+        File file = new File(directory, typeName + ".properties");
+        reader = new PropertyAttributeReader(file);
     }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
     public FeatureType getFeatureType() {
         return reader.type;
     }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     * @throws IllegalAttributeException DOCUMENT ME!
+     * @throws NoSuchElementException DOCUMENT ME!
+     */
     public Feature next()
         throws IOException, IllegalAttributeException, NoSuchElementException {
-        reader.next();            
+        reader.next();
+
         FeatureType type = reader.type;
         String fid = reader.getFeatureID();
-        Object values[] = new Object[ reader.getAttributeCount() ];
-        for( int i=0; i< reader.getAttributeCount(); i++){
-            values[i]=reader.read( i );
+        Object[] values = new Object[reader.getAttributeCount()];
+
+        for (int i = 0; i < reader.getAttributeCount(); i++) {
+            values[i] = reader.read(i);
         }
-        return type.create( values, fid );                
+
+        return type.create(values, fid);
     }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     */
     public boolean hasNext() throws IOException {
         return reader.hasNext();
     }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     */
     public void close() throws IOException {
-        reader.close();
-        reader = null;        
+        if (reader == null) {
+            LOGGER.warning("Stream seems to be already closed.");
+        }else{
+        	reader.close();
+        }
+
+        reader = null;
     }
 }
