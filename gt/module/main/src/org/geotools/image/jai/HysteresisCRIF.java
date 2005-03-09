fdfc77ded8664d213c67b9e6aca3c08f7ef5415e
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2005-03-09 04:47:56 +0000

    Move JAI operations from org.geotools.coverage to org.geotools.image
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@11901 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/image/jai/HysteresisCRIF.java
+++ gt/module/main/src/org/geotools/image/jai/HysteresisCRIF.java
@@ -17,7 +17,7 @@
  *    License along with this library; if not, write to the Free Software
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package org.geotools.coverage.processing.jai;
+package org.geotools.image.jai;
 
 // J2SE dependencies
 import java.awt.RenderingHints;
@@ -29,31 +29,32 @@ import javax.media.jai.CRIFImpl;
 import javax.media.jai.ImageLayout;
 import javax.media.jai.JAI;
 
- 
+
 /**
- * The factory for the {@link NodataFilter} operation.
+ * The factory for the {@link Hysteresis} operation.
  *
  * @version $Id$
  * @author Lionel Flahaut
  */
-public class NodataFilterCRIF extends CRIFImpl {
+public class HysteresisCRIF extends CRIFImpl {
     /**
-     * Construct a default factory.
+     * Constructs a default factory.
      */
-    public NodataFilterCRIF() {
+    public HysteresisCRIF() {
     }
 
     /**
-     * Creates a {@link RenderedImage} representing the results of an imaging
+     * Creates a {@link RenderedImage} for the results of an imaging
      * operation for a given {@link ParameterBlock} and {@link RenderingHints}.
      */
     public RenderedImage create(final ParameterBlock param,
                                 final RenderingHints hints)
     {
-        final RenderedImage   image = (RenderedImage)param.getSource(0);
-        final ImageLayout    layout = (ImageLayout)hints.get(JAI.KEY_IMAGE_LAYOUT);
-        final int           padding = param.getIntParameter(0);
-        final int validityThreshold = param.getIntParameter(1);
-        return new NodataFilter(image, layout, hints, padding, validityThreshold);
+        final RenderedImage image = (RenderedImage)param.getSource(0);
+        final ImageLayout  layout = (ImageLayout)hints.get(JAI.KEY_IMAGE_LAYOUT);
+        final double      low = param.getDoubleParameter(0);
+        final double     high = param.getDoubleParameter(1);
+        final double padValue = param.getDoubleParameter(2);
+        return new Hysteresis(image, layout, hints, low, high, padValue);
     }
 }	
