fe6ef5c64ae62a056e335d0ab77874daaa7e6667
Gabriel Roldan <gabriel.roldan@gmail.com>
2005-03-11 13:22:25 +0000

    added NamedLayer and NamedStyle test cases
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@12018 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/test/org/geotools/styling/SLDStyleTest.java
+++ gt/module/main/test/org/geotools/styling/SLDStyleTest.java
@@ -4,29 +4,40 @@
  *
  * Created on November 6, 2003, 11:32 AM
  */
-
 package org.geotools.styling;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
 import org.geotools.resources.TestData;
 
+
 /**
+ * DOCUMENT ME!
  *
  * @author jamesm
  */
 public class SLDStyleTest extends TestCase {
-    
+    /**
+     * Creates a new SLDStyleTest object.
+     *
+     * @param testName DOCUMENT ME!
+     */
     public SLDStyleTest(java.lang.String testName) {
         super(testName);
     }
-    
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
     public static Test suite() {
         TestSuite suite = new TestSuite(SLDStyleTest.class);
+
         return suite;
     }
+
     //
     //    /** Test of setInput method, of class org.geotools.styling.SLDStyle. */
     //    public void testSetInput() {
@@ -52,20 +63,18 @@ public class SLDStyleTest extends TestCase {
     //        fail("The test case is empty.");
     //    }
     //
-    //    /** Test of parseSLD method, of class org.geotools.styling.SLDStyle. */
-    //    public void testParseSLD() {
-    //        System.out.println("testParseSLD");
-    //
-    //        // Add your test code below by replacing the default call to fail.
-    //        fail("The test case is empty.");
-    //    }
-    
-    /** Test of parseStyle method, of class org.geotools.styling.SLDStyle. */
-    public void testParseStyle() throws Exception{
+
+    /**
+     * Test of parseStyle method, of class org.geotools.styling.SLDStyle.
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public void testParseStyle() throws Exception {
         //java.net.URL base = getClass().getResource("testData/");
-       // base = getClass().getResource("testData");
-       // base = getClass().getResource("/testData");
+        // base = getClass().getResource("testData");
+        // base = getClass().getResource("/testData");
         StyleFactory factory = StyleFactory.createStyleFactory();
+
         //java.net.URL surl = new java.net.URL(base + "/test-sld.xml");
         java.net.URL surl = TestData.getResource(this, "test-sld.xml");
         SLDParser stylereader = new SLDParser(factory, surl);
@@ -73,25 +82,88 @@ public class SLDStyleTest extends TestCase {
         assertEquals("My Layer", sld.getName());
         assertEquals("A layer by me", sld.getTitle());
         assertEquals("this is a sample layer", sld.getAbstract());
-        assertEquals(1,sld.getStyledLayers().length);
-        UserLayer layer = (UserLayer)sld.getStyledLayers()[0];
+        assertEquals(1, sld.getStyledLayers().length);
+
+        UserLayer layer = (UserLayer) sld.getStyledLayers()[0];
         assertNull(layer.getName());
-        assertEquals(1,layer.getUserStyles().length);
+        assertEquals(1, layer.getUserStyles().length);
+
         Style style = layer.getUserStyles()[0];
-        assertEquals(1,style.getFeatureTypeStyles().length);
+        assertEquals(1, style.getFeatureTypeStyles().length);
         assertEquals("My User Style", style.getName());
         assertEquals("A style by me", style.getTitle());
         assertEquals("this is a sample style", style.getAbstract());
+
         FeatureTypeStyle fts = style.getFeatureTypeStyles()[0];
         Rule rule = fts.getRules()[0];
-        LineSymbolizer lineSym = (LineSymbolizer)rule.getSymbolizers()[0];
-        assertEquals(4,((Number)lineSym.getStroke().getWidth().getValue(null)).intValue());
+        LineSymbolizer lineSym = (LineSymbolizer) rule.getSymbolizers()[0];
+        assertEquals(4,
+            ((Number) lineSym.getStroke().getWidth().getValue(null)).intValue());
+    }
+
+    /**
+     * Test of parseSLD method to ensure NamedLayer/Name and
+     * NamedLayer/NamedStyle are parsed correctly
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public void testParseSLDNamedLayersOnly() throws Exception {
+        StyleFactory factory = StyleFactory.createStyleFactory();
+        java.net.URL surl = TestData.getResource(this, "namedLayers.sld");
+        SLDParser stylereader = new SLDParser(factory, surl);
+
+        StyledLayerDescriptor sld = stylereader.parseSLD();
+
+        final int expectedLayerCount = 3;
+        final String[] layerNames = { "Rivers", "Roads", "Houses" };
+        final String[] namedStyleNames = { "CenterLine", "CenterLine", "Outline" };
+        StyledLayer[] layers = sld.getStyledLayers();
+
+        assertEquals(expectedLayerCount, layers.length);
+
+        for (int i = 0; i < expectedLayerCount; i++) {
+            assertTrue(layers[i] instanceof NamedLayer);
+        }
+
+        for (int i = 0; i < expectedLayerCount; i++) {
+            assertEquals(layerNames[i], layers[i].getName());
+        }
+
+        for (int i = 0; i < expectedLayerCount; i++) {
+            NamedLayer layer = (NamedLayer) layers[i];
+            assertEquals(1, layer.getStyles().length);
+            assertTrue(layer.getStyles()[0] instanceof NamedStyle);
+            assertEquals(namedStyleNames[i], layer.getStyles()[0].getName());
+        }
+    }
+
+    /**
+     * Test of parseSLD method to ensure NamedLayer/Name and
+     * NamedLayer/NamedStyle are parsed correctly
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public void testParseSLDNamedAndUserLayers() throws Exception {
+        StyleFactory factory = StyleFactory.createStyleFactory();
+        java.net.URL surl = TestData.getResource(this, "mixedLayerTypes.sld");
+        SLDParser stylereader = new SLDParser(factory, surl);
+
+        StyledLayerDescriptor sld = stylereader.parseSLD();
+
+        final int expectedLayerCount = 4;
+
+        StyledLayer[] layers = sld.getStyledLayers();
+
+        assertEquals(expectedLayerCount, layers.length);
+        assertTrue(layers[0] instanceof NamedLayer);
+        assertTrue(layers[1] instanceof UserLayer);
+        assertTrue(layers[2] instanceof NamedLayer);
+        assertTrue(layers[3] instanceof UserLayer);    
     }
     
     // Add test methods here, they have to start with 'test' name.
     // for example:
     // public void testHello() {}
-    
     public static void main(java.lang.String[] args) {
         junit.textui.TestRunner.run(suite());
     }
