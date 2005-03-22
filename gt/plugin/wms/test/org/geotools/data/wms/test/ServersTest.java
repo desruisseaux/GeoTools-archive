02e57890f06b32a03648f59a1660cf9e9ae793bc
Richard Gould <rgould@refractions.net>
2005-03-22 06:13:52 +0000

    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@12529 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/wms/test/org/geotools/data/wms/test/ServersTest.java
+++ gt/plugin/wms/test/org/geotools/data/wms/test/ServersTest.java
@@ -1,18 +1,15 @@
-/*
- * Created on 14-Oct-2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package org.geotools.data.wms.test;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
@@ -35,25 +32,35 @@ import org.geotools.data.wms.request.GetMapRequest;
 import org.geotools.data.wms.response.GetFeatureInfoResponse;
 import org.geotools.data.wms.response.GetMapResponse;
 import org.geotools.ows.ServiceException;
+import org.geotools.resources.TestData;
 import org.xml.sax.SAXException;
 
-/**
- * @author Richard Gould
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 public class ServersTest extends TestCase {
     public void testServers() throws Exception{
-    	URL[] servers = new URL[50];
+
+    	List servers = new ArrayList();
+/*
+    	File serverFile = TestData.file(this, "servers.txt");
+    	BufferedReader br = new BufferedReader(new FileReader(serverFile));
+    	String line = null;
+    	while ((line = br.readLine()) != null) {
+    		try {
+    			servers.add(new URL(line));
+    		} catch (MalformedURLException e) {
+
+    		}
+    	}
+    	*/
+
+//    	URL[] servers = new URL[50];
 //		servers[0] = new URL("http://wms.jpl.nasa.gov/wms.cgi?VERSION=1.1.1&SERVICE=WMS&REQUEST=GetCapabilities");
 //    	servers[1] = new URL("http://demo.cubewerx.com/demo/cubeserv/cubeserv.cgi?CONFIG=main&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetCapabilities");
 //    	servers[2] = new URL("http://www2.dmsolutions.ca/cgi-bin/mswms_gmap?VERSION=1.1.0&REQUEST=GetCapabilities");
-//  This returning Gzip content  	
+//  This returning Gzip content
 //    	servers[3] = new URL("http://wms.cits.rncan.gc.ca/cgi-bin/cubeserv.cgi?VERSION=1.1.0&REQUEST=GetCapabilities");
 //    	servers[4] = new URL("http://terraservice.net/ogccapabilities.ashx?version=1.1.1&request=GetCapabilties");
 //    	servers[5] = new URL("http://www2.demis.nl/mapserver/Request.asp?VERSION=1.3.0&SERVICE=WMS&REQUEST=GetCapabilities");
-//THIS ONE OFFLINE    	
+//THIS ONE OFFLINE
 //        servers[6] = new URL("http://datamil.udel.edu/servlet/com.esri.wms.Esrimap?servicename=DE_census2k_sf1&VERSION=1.0.0&request=capabilities");
 //    	servers[7] = new URL("http://www.lifemapper.org/Services/WMS/?Service=WMS&VERSION=1.1.1&request=getcapabilities");
 //    	this server returns OGC for 1.3.0
@@ -61,7 +68,7 @@ public class ServersTest extends TestCase {
 //    	servers[9] = new URL("http://www.geographynetwork.ca/wmsconnector/com.esri.wsit.WMSServlet/Geobase_NRN_NewfoundlandAndLabrador_I_Detail?request=GetCapabilities");
 //    	servers[10] = new URL("http://gisdata.usgs.net/servlet/com.esri.wms.Esrimap?REQUEST=GetCapabilities&VERSION=1.3.0&SERVICE=WMS");
 //    	servers[11] = new URL("http://www.refractions.net:8080/geoserver/wms/?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0"); //$NON-NLS-1$
-//1.0.0 freezes.    	
+//1.0.0 freezes.
 //    	servers[12] = new URL("http://mapserv2.esrin.esa.it/cubestor/cubeserv/cubeserv.cgi?VERSION=1.1.1&REQUEST=GetCapabilities&SERVICE=WMS");
 //		servers[13] = new URL("http://mesonet.agron.iastate.edu/wms/comprad.php?request=getcapabilities");
 //		servers[14] = new URL("http://redspider.us/ionicweb/wfs/METRODC?request=getcapabilities&service=WMS&version=1.1.1");
@@ -77,44 +84,46 @@ public class ServersTest extends TestCase {
 //    	servers[24] = new URL("http://iceds.ge.ucl.ac.uk/cgi-bin/wms?map=wms.map&SERVICE=WMS&REQUEST=GetCapabilities");
 //    	servers[25] = new URL("http://test.landmap.ac.uk/ecwp/ecw_wms.dll?request=GetCapabilities&service=wms");
 //    	servers[26] = new URL("http://emandev.cciw.ca/cgi-bin/mapserver/mapserv.exe?REQUEST=GetCapabilities&MAP=C:/Inetpub/wwwroot/emanco/cgi-bin/mapserver/naturewatch.map&VERSION=1.1.1&SERVICE=WMS");
-   	
+
         int total = 0;
         int passedCount = 0;
-        
-    	for (int i = 0; i < servers.length; i++) {
-    		if (servers[i] != null) {
-                total++;
-                Random random = new Random();
-                String dir = "tests";
-                String filename = URLEncoder.encode(servers[i].getHost()+random.nextInt(10000));
-                File file = new File("C:\\"+dir+"\\"+filename+".txt");
-                file.createNewFile();
-                PrintStream out = new PrintStream(file);
-                boolean passed = serverTest(out, servers[i]);
-                out.flush();
-                out.close();
-                
-                if (passed) {
-                    System.out.println(servers[i].toExternalForm() + " passed.");
-                    passedCount++;
-                    file.delete();
-                }
+
+    	for (int i = 0; i < servers.size(); i++) {
+    		URL server = (URL) servers.get(i);
+            total++;
+            Random random = new Random();
+            String dir = "tests";
+            String filename = URLEncoder.encode(server.getHost()+random.nextInt(10000),"UTF-8");
+            File file = new File("C:\\"+dir+"\\"+filename+".txt");
+            file.createNewFile();
+            PrintStream out = new PrintStream(file);
+            boolean passed = serverTest(out, server);
+            out.flush();
+            out.close();
+
+            if (passed) {
+                System.out.println(server.toExternalForm() + " passed.");
+                passedCount++;
+                file.delete();
+            } else {
+            	System.out.println(server.toExternalForm() + " failed.");
+            }
 //    		    WebMapServer wms = new WebMapServer(servers[i]);
 //    			assertNotNull("Missing Capabilities",wms.getCapabilities());
 //    		    WMSCapabilities capabilities = wms.getCapabilities();
 //    		    assertNotNull(capabilities.getRequest());
-    		}
-    	}
-        
+		}
+
         System.out.println("Total tested: "+total);
         System.out.println("Total passed: "+passedCount);
     }
-    
+
     public boolean serverTest(PrintStream out, URL url) {
         WebMapServer wms = null;
-        
+
         boolean passed = true;
-        
+        out.println("Beginning tests for server:");
+        out.println(url);
         out.print("Parsing Capabilities...");
         try {
             wms = new WebMapServer(url);
@@ -143,10 +152,10 @@ public class ServersTest extends TestCase {
             e.printStackTrace(out);
             return passed;
         }
-        
+
         WMSCapabilities caps = wms.getCapabilities();
         assertNotNull(caps);
-        
+
         out.println("Validating layer LatLonBoundingBoxes...");
         Iterator iter = caps.getLayerList().iterator();
         while (iter.hasNext()) {
@@ -158,7 +167,7 @@ public class ServersTest extends TestCase {
                 }
             }
         }
-        
+
         Layer layer = null;
         out.print("Looking for a named layer...");
         iter = caps.getLayerList().iterator();
@@ -188,17 +197,17 @@ public class ServersTest extends TestCase {
                     format = "image/png";
                     out.println("using 'image/png'.");
                 }
-                
+
                 if (format == null && formats.contains("image/gif")) {
                     format = "image/gif";
                     out.println("using 'image/gif'.");
                 }
-                
+
                 if (format == null && formats.contains("image/jpeg")) {
                     format = "image/jpeg";
                     out.println("using 'image/jpeg'.");
                 }
-                
+
                 if (format == null) {
                     format = (String) formats.get(0);
                     out.println("server does not support GIF, PNG or JPEG. Using '"+format+"'");
@@ -207,7 +216,7 @@ public class ServersTest extends TestCase {
             }
         }
 
-        
+
         out.print("Performing GetMap operation...");
         GetMapRequest request = wms.createGetMapRequest();
         //TODO make this easier
@@ -218,11 +227,11 @@ public class ServersTest extends TestCase {
         request.setFormat(format);
         request.setSRS("EPSG:4326");
         request.setDimensions("100","100");
-        
+
         try {
             GetMapResponse response = wms.issueRequest(request);
             out.println("received a response.");
-            
+
             out.print("Checking returned format...");
             if (response.getContentType().indexOf(format) == -1) {
                 out.println("server returned bad format. Expected "+format+", got "+response.getContentType()+".");
@@ -230,10 +239,13 @@ public class ServersTest extends TestCase {
             } else {
                 out.println("passed.");
             }
-            
+
             out.print("Checking dimensions...");
             BufferedImage image = ImageIO.read(response.getInputStream());
-            if (image.getWidth() != 100 || image.getHeight() != 100) {
+            if (image == null) {
+            	out.println("returned a bad image. ContentType is "+response.getContentType());
+            	passed = false;
+            } else if (image.getWidth() != 100 || image.getHeight() != 100) {
                 out.println("server returned bad dimensions. Expect 100, 100. Returned "+image.getWidth()+","+image.getHeight());
                 passed = false;
             } else {
@@ -262,70 +274,70 @@ public class ServersTest extends TestCase {
         } finally {
             out.println(request.getFinalURL());
         }
-        
-        if (caps.getRequest().getGetFeatureInfo() != null) {
-            out.println("");
-            out.println("Server supports GetFeatureInfo requests. Beginning tests.");
-            
-            Layer qLayer = null;
-            out.print("Locating a queryable layer...");
-            Set qLayers = WMSUtils.getQueryableLayers(caps);
-            if (qLayers != null && qLayers.size() != 0) {
-                qLayer = (Layer) qLayers.iterator().next();
-                out.println("found layer '"+qLayer.getName()+"'.");
-            } else {
-                out.println("NOT FOUND");
-                passed = false;
-            }
-            
-            if (qLayer != null) {
-                GetFeatureInfoRequest gfiRequest = wms.createGetFeatureInfoRequest(request);
-                gfiRequest.addQueryLayer(qLayer);
-                gfiRequest.setFeatureCount(5);
-                gfiRequest.setQueryPoint(50,50);
-                
-                String gfiFormat = caps.getRequest().getGetFeatureInfo().getFormatStrings()[0];
-                out.println("Using "+gfiFormat+" as format during GetFeatureInfo request.");
-                
-                gfiRequest.setInfoFormat(gfiFormat);
-                
-                try {
-                    out.print("Performing GetFeatureInfo request...");
-                    GetFeatureInfoResponse response = wms.issueRequest(gfiRequest);
-                    out.println("response received.");
-                    
-                    out.print("Checking returned format...");
-                    if (response.getContentType().indexOf(gfiFormat) == -1) {
-                        out.println("server returned bad format. Expected "+gfiFormat+", got "+response.getContentType()+".");
-                        passed = false;
-                    } else {
-                        out.println("passed.");
-                    }
-                } catch (ServiceException e) {
-                    out.println("failed.");
-                    passed = false;
-                    while (e != null) {
-                        if (e.getLocator() != null && e.getLocator().length() != 0) {
-                            out.println("ServiceException at "+e.getLocator()+": "+e.getMessage()+"("+e.getCode()+")");
-                        }
-                        out.println("ServiceException: "+e.getMessage()+"("+e.getCode()+")");
-                        e = e.getNext();
-                    }
-                } catch (IOException e) {
-                    out.println("failed.");
-                    passed = false;
-                    out.println("IOException: "+e.getMessage());
-                    e.printStackTrace(out);
-                } catch (SAXException e) {
-                    out.println("failed.");
-                    passed = false;
-                    out.println("SAXException: "+e.getMessage());
-                    e.printStackTrace(out);
-                } finally {
-                    out.println(request.getFinalURL());
-                }
-            }
-        }
+
+//        if (caps.getRequest().getGetFeatureInfo() != null) {
+//            out.println("");
+//            out.println("Server supports GetFeatureInfo requests. Beginning tests.");
+//
+//            Layer qLayer = null;
+//            out.print("Locating a queryable layer...");
+//            Set qLayers = WMSUtils.getQueryableLayers(caps);
+//            if (qLayers != null && qLayers.size() != 0) {
+//                qLayer = (Layer) qLayers.iterator().next();
+//                out.println("found layer '"+qLayer.getName()+"'.");
+//            } else {
+//                out.println("NOT FOUND");
+//                passed = false;
+//            }
+//
+//            if (qLayer != null) {
+//                GetFeatureInfoRequest gfiRequest = wms.createGetFeatureInfoRequest(request);
+//                gfiRequest.addQueryLayer(qLayer);
+//                gfiRequest.setFeatureCount(5);
+//                gfiRequest.setQueryPoint(50,50);
+//
+//                String gfiFormat = caps.getRequest().getGetFeatureInfo().getFormatStrings()[0];
+//                out.println("Using "+gfiFormat+" as format during GetFeatureInfo request.");
+//
+//                gfiRequest.setInfoFormat(gfiFormat);
+//
+//                try {
+//                    out.print("Performing GetFeatureInfo request...");
+//                    GetFeatureInfoResponse response = wms.issueRequest(gfiRequest);
+//                    out.println("response received.");
+//
+//                    out.print("Checking returned format...");
+//                    if (response.getContentType().indexOf(gfiFormat) == -1) {
+//                        out.println("server returned bad format. Expected "+gfiFormat+", got "+response.getContentType()+".");
+//                        passed = false;
+//                    } else {
+//                        out.println("passed.");
+//                    }
+//                } catch (ServiceException e) {
+//                    out.println("failed.");
+//                    passed = false;
+//                    while (e != null) {
+//                        if (e.getLocator() != null && e.getLocator().length() != 0) {
+//                            out.println("ServiceException at "+e.getLocator()+": "+e.getMessage()+"("+e.getCode()+")");
+//                        }
+//                        out.println("ServiceException: "+e.getMessage()+"("+e.getCode()+")");
+//                        e = e.getNext();
+//                    }
+//                } catch (IOException e) {
+//                    out.println("failed.");
+//                    passed = false;
+//                    out.println("IOException: "+e.getMessage());
+//                    e.printStackTrace(out);
+//                } catch (SAXException e) {
+//                    out.println("failed.");
+//                    passed = false;
+//                    out.println("SAXException: "+e.getMessage());
+//                    e.printStackTrace(out);
+//                } finally {
+//                    out.println(gfiRequest.getFinalURL());
+//                }
+//            }
+//        }
         return passed;
     }
 }
