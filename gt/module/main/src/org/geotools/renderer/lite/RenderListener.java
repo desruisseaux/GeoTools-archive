9dce5a418d40a791d4d326476fb83fc6b145b1c1
Jesse Eichar <jesse.eichar@camptocamp.com>
2005-03-24 00:46:29 +0000

    added call renderlisteners that are interested in capturing LiteRenderer's exceptions and what features have been rendered
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@12641 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/module/main/src/org/geotools/renderer/lite/RenderListener.java
@@ -0,0 +1,22 @@
+/*
+ * Geotools2 - OpenSource mapping toolkit http://geotools.org (C) 2002, Geotools Project Managment
+ * Committee (PMC) This library is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * version 2.1 of the License. This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ */
+package org.geotools.renderer.lite;
+
+import org.geotools.feature.Feature;
+
+/**
+ * A RenderListener is notified each time a feature is rendered and each time
+ * an error occurs during rendering.  Therefore <b>VERY LITTLE WORK</b> should be done in the listener!!!
+ * 
+ * @author jeichar
+ */
+public interface RenderListener {
+    public void featureRenderer( Feature feature );
+    public void errorOccurred( Exception e);
+}
