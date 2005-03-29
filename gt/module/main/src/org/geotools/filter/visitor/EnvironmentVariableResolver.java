8a348505421e0bd23b634ae57ab6c9f743f1f0f7
James Macgill <jmacgill@users.sourceforge.net>
2005-03-29 17:57:48 +0000

    added implementation to resolve(Filter... added javadocs
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@12819 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/filter/visitor/EnvironmentVariableResolver.java
+++ gt/module/main/src/org/geotools/filter/visitor/EnvironmentVariableResolver.java
@@ -28,51 +28,61 @@ import org.geotools.filter.parser.ParseException;
  *
  */
 public class EnvironmentVariableResolver extends AbstractFilterVisitor implements org.geotools.filter.FilterVisitor {
-   
-
-  
+    
+    
+    
     /** Standard java logger */
     private static Logger LOGGER = Logger.getLogger("org.geotools.filter");
-   
-  
+    
+    
     /**
      * Empty constructor
      */
     public EnvironmentVariableResolver() {
     }
-
+    
     /**
-     *
+     * Find all instances of MapScaleDenominator and replace them with 
+     * the a literal expression for the provided map scale.
+     * The passed in filter is NOT modifed by calls to this method.
+     * @param filter The Filter to check for MapScaleDenominator Environment Variables
+     * @return A Filter with the map scale env variable replaced by literals
      */
-    public void resolve(Filter filter, double mapScale) throws ParseException {  
+    public Filter resolve(Filter filter, double mapScale) throws ParseException {
         String input = filter.toString();
+        input = input.replaceAll("sld:MapScaleDenominator", ""+mapScale);
         Filter output = (Filter)ExpressionBuilder.parse(input);
+        return output;
     }
     
     
     /**
-     * 
+     * Find all instances of MapScaleDenominator and replace them with 
+     * the a literal expression for the provided map scale.
+     * The passed in filter is NOT modifed by calls to this method.
+     * @param exp The Expression to check for MapScaleDenominator Environment Variables
+     * @return An Epression with the map scale env variable replaced by literals
      */
-    public Expression resolve(Expression exp, double mapScale) throws ParseException {     
-           
-                String input = exp.toString();
-                input = input.replaceAll("sld:MapScaleDenominator", ""+mapScale);
-                Expression output = (Expression)ExpressionBuilder.parse(input);
-                return output;
+    public Expression resolve(Expression exp, double mapScale) throws ParseException {
+        
+        String input = exp.toString();
+        input = input.replaceAll("sld:MapScaleDenominator", ""+mapScale);
+        Expression output = (Expression)ExpressionBuilder.parse(input);
+        return output;
     }
     
     public boolean needsResolving(Filter f){
         return true;
     }
-
-   
+    
+    
     /**
      * @see org.geotools.filter.FilterVisitor#visit(org.geotools.filter.Expression)
      */
     public void visit(Expression expression) {
-      if(expression instanceof MapScaleDenominator){
-        System.err.println("FOUND ONE");
-      }
+        if(expression instanceof MapScaleDenominator){
+            System.err.println("FOUND ONE");
+        }
     }
-
+    
 }
