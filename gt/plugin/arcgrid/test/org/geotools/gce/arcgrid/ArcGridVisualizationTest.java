3437bf71090af5ef2a10331529a7e05611162013
Simone Giannecchini <simboss1@gmail.com>
2005-04-07 08:26:01 +0000

    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@13012 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/arcgrid/test/org/geotools/gce/arcgrid/ArcGridVisualizationTest.java
+++ gt/plugin/arcgrid/test/org/geotools/gce/arcgrid/ArcGridVisualizationTest.java
@@ -1,7 +1,6 @@
 package org.geotools.gce.arcgrid;
 
 import org.geotools.coverage.grid.GridCoverage2D;
-import org.geotools.coverage.grid.GridCoverageImpl;
 import org.geotools.resources.TestData;
 import org.opengis.coverage.grid.Format;
 import org.opengis.coverage.grid.GridCoverage;
@@ -17,10 +16,25 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 
 import java.awt.image.BufferedImage;
-
+import javax.media.jai.*;
 import java.io.File;
 
 import java.net.URL;
+import java.awt.image.RenderedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import javax.media.jai.RenderedOp;
+import java.awt.image.WritableRaster;
+import java.awt.image.RescaleOp;
+import java.awt.image.ColorModel;
+import java.awt.color.ColorSpace;
+import java.awt.Transparency;
+import java.awt.image.DataBuffer;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import com.sun.media.jai.codec.TIFFEncodeParam;
+import com.sun.media.jai.codec.ImageEncoder;
+import com.sun.media.jai.codec.ImageCodec;
 
 /**
  * <p>Title: TestArcGridClass</p>
@@ -30,78 +44,84 @@ import java.net.URL;
  * @author <a href="mailto:simboss_ml@tiscali.it">Simone Giannecchini (simboss)</a>
  * @version 1.0
  */
-public class ArcGridVisualizationTest extends TestCaseSupport {
-    /** ArcGrid files (and associated parameters) to test*/
-    final TestParams[] params = new TestParams[] {
-            new TestParams("spearfish_dem.asc.gz", true, true),
-            new TestParams("ArcGrid.asc", false, false),
-            new TestParams("vandem.asc.gz", true, false)
-        };
+public class ArcGridVisualizationTest  extends TestCaseSupport {
+  /** ArcGrid files (and associated parameters) to test*/
+  final TestParams[] params = new TestParams[] {
+      new TestParams("vandem.asc.gz", true, false),
+      new TestParams("ArcGrid.asc", false, false),
+      new TestParams("spearfish_dem.asc.gz", true, true)
+  };
 
-    /** Creates a new instance of ArcGridReadWriteTest */
-    public ArcGridVisualizationTest(String name) {
-        super(name);
+  /** Creates a new instance of ArcGridReadWriteTest */
+  public ArcGridVisualizationTest(String name) {
+     super(name);
+  }
+
+  public void testAll() throws Exception {
+    StringBuffer errors = new StringBuffer();
+
+    for (int i = 0; i < params.length; i++) {
+      try {
+        test(params[i]);
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+        errors.append("\nFile " + params[i].fileName + " : "
+                      + e.getMessage());
+      }
     }
 
-    public void testAll() throws Exception {
-        StringBuffer errors = new StringBuffer();
+    if (errors.length() > 0) {
+       fail(errors.toString());
+    }
+  }
 
-        for (int i = 0; i < params.length; i++) {
-            try {
-                test(params[i]);
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-                errors.append("\nFile " + params[i].fileName + " : "
-                    + e.getMessage());
-            }
-        }
+  void test(TestParams testParam) throws Exception {
+    //create a temporary output file
+    //temporary file to use
+    File tmpFile = null;
 
-        if (errors.length() > 0) {
-            fail(errors.toString());
-        }
+    if (testParam.compressed) {
+      tmpFile = File.createTempFile("temp", ".gz");
+    }
+    else {
+      tmpFile = File.createTempFile("temp", ".asc");
     }
 
-    void test(TestParams testParam)
-        throws Exception {
-        //create a temporary output file
-        //temporary file to use
-        File tmpFile = null;
+    tmpFile.deleteOnExit();
 
-        if (testParam.compressed) {
-            tmpFile = File.createTempFile("temp", ".gz");
-        }
-        else {
-            tmpFile = File.createTempFile("temp", ".asc");
-        }
+    //file to use
+    URL file = TestData.getResource(this, testParam.fileName);
 
-        tmpFile.deleteOnExit();
+    /*Step 1 read it*/
 
-        //file to use
-        URL file = TestData.getResource(this, testParam.fileName);
+    //read in the grid coverage
+    GridCoverageReader reader = new ArcGridReader( (TestData.getResource(
+        this, testParam.fileName)));
 
-        /*Step 1 read it*/
+    //reading the coverage
+    GridCoverage2D gc = ((GridCoverage2D) reader.read(null)).geophysics(false);
 
-        //read in the grid coverage
-        GridCoverageReader reader = new ArcGridReader((TestData.getResource(
-                    this, testParam.fileName)));
 
-        //reading the coverage
-        GridCoverage2D gc = (GridCoverage2D) reader.read(null);
+    BufferedImage bufferedImage = ( (PlanarImage) gc.getRenderedImage())
+        .getAsBufferedImage();
+    ImageIcon icon = new ImageIcon(bufferedImage);
+    JLabel label = new JLabel(icon);
+    JFrame frame = new JFrame();
+    frame.setTitle(
+        testParam.fileName);
 
-        ImageIcon icon = new ImageIcon(((PlanarImage) gc.getRenderedImage())
-                .getAsBufferedImage());
-        JLabel label = new JLabel(icon);
-        JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new JScrollPane(label));
+    frame.pack();
+    frame.show();
 
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new JScrollPane(label));
-        frame.pack();
-        frame.show();
-    }
+  }
 
-    public static final void main(String[] args)
-        throws Exception {
-        junit.textui.TestRunner.run(suite(ArcGridReadWriteTest.class));
-    }
+  public static final void main(String[] args)
+     throws Exception {
+     junit.textui.TestRunner.run(suite(ArcGridVisualizationTest.class));
+
+
+  }
 }
