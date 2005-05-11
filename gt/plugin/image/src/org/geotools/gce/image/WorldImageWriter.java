50a8afa431b4c1cc57124b0a0480d8373a4a7510
Simone Giannecchini <simboss1@gmail.com>
2005-05-11 22:35:34 +0000

    new improvements to gif output
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@13450 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/image/src/org/geotools/gce/image/WorldImageWriter.java
+++ gt/plugin/image/src/org/geotools/gce/image/WorldImageWriter.java
@@ -16,24 +16,9 @@
  */
 package org.geotools.gce.image;
 
-
-import org.geotools.coverage.grid.GridCoverage2D;
-import org.geotools.parameter.Parameter;
-import org.opengis.coverage.MetadataNameNotFoundException;
-import org.opengis.coverage.grid.Format;
-import org.opengis.coverage.grid.GridCoverage;
-import org.opengis.coverage.grid.GridCoverageWriter;
-import org.opengis.parameter.GeneralParameterValue;
-import org.opengis.spatialschema.geometry.Envelope;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.RenderingHints;
-import java.awt.Transparency;
-import java.awt.color.ColorSpace;
 import java.awt.image.ComponentColorModel;
 import java.awt.image.DataBuffer;
-import java.awt.image.IndexColorModel;
 import java.awt.image.RenderedImage;
 import java.awt.image.renderable.ParameterBlock;
 import java.io.BufferedOutputStream;
@@ -43,19 +28,23 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.URL;
+
 import javax.imageio.ImageIO;
 import javax.media.jai.ColorCube;
-import javax.media.jai.IHSColorSpace;
 import javax.media.jai.ImageLayout;
 import javax.media.jai.JAI;
 import javax.media.jai.KernelJAI;
 import javax.media.jai.PlanarImage;
 import javax.media.jai.RenderedOp;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.parameter.Parameter;
+import org.opengis.coverage.MetadataNameNotFoundException;
+import org.opengis.coverage.grid.Format;
+import org.opengis.coverage.grid.GridCoverage;
+import org.opengis.coverage.grid.GridCoverageWriter;
+import org.opengis.parameter.GeneralParameterValue;
+import org.opengis.spatialschema.geometry.Envelope;
 
 
 /**
@@ -262,9 +251,9 @@ public class WorldImageWriter implements GridCoverageWriter {
     }
 
     /**
-     * Encode.
+     * Encode the given coverage to the requsted output format.
      *
-     * @param sourceCoverage DOCUMENT ME!
+     * @param sourceCoverage the coverage to be encoded.s
      * @param output OutputStream
      *
      * @throws IOException
@@ -288,15 +277,17 @@ public class WorldImageWriter implements GridCoverageWriter {
             //in such a case we have to get the visual representation for this data
             //by calling geophysiscs(false)
             int i = 0;
-            
-            for (; i < sourceCoverage.getNumSampleDimensions(); i++)
+
+            for (; i < sourceCoverage.getNumSampleDimensions(); i++) {
                 if (!sourceCoverage.getSampleDimension(0).getSampleToGeophysics()
                                        .isIdentity()) {
                     surrogateImage = ((PlanarImage) (sourceCoverage).geophysics(false)
                                                      .getRenderedImage());
-
                     break;
                 }
+
+               
+            }
 
             if (i == sourceCoverage.getNumSampleDimensions()) {
                 /**
@@ -309,26 +300,22 @@ public class WorldImageWriter implements GridCoverageWriter {
             }
 
             /**
-             * writing a gif
+             * writing a gif requires a lot of code to be writtens
              */
             if ((((String) (this.format.getWriteParameters().parameter("format")
                                            .getValue())).compareToIgnoreCase(
                         "gif") == 0)) {
-  
                 if (surrogateImage.getColorModel() instanceof ComponentColorModel) {
                     surrogateImage = componentColorModel2GIF(surrogateImage);
                 }
-
-            } 
+            }
 
             /**
              * write using jai for the others formats
              */
-
             ImageIO.write(surrogateImage,
                 (String) (this.format.getWriteParameters().parameter("format")
                                      .getValue()), output);
-
         } catch (Exception e) {
             throw new IOException(e.getMessage());
         }
@@ -352,91 +339,171 @@ public class WorldImageWriter implements GridCoverageWriter {
      */
     private PlanarImage componentColorModel2GIF(PlanarImage surrogateImage) {
         {
+            //parameter block
+            ParameterBlock pb = new ParameterBlock();
+            RenderedImage alphaChannel = null;
+
+            /**
+             * AMPLITUDE RESCALINGs
+             */
+
             //I might also need to reformat the image in order to get it to 8 bits
             //samples
             if (surrogateImage.getSampleModel().getTransferType() != DataBuffer.TYPE_BYTE) {
-            	surrogateImage=rescale2Byte(surrogateImage);
+                surrogateImage = rescale2Byte(surrogateImage);
             }
 
-            //parameter block
-            ParameterBlock pb = new ParameterBlock();
+            /**
+             * ALPHA CHANNEL  getting the alpha channel and separating from the
+             * others bands.
+             */
+            if (surrogateImage.getColorModel().hasAlpha()) {
+                int numBands = surrogateImage.getSampleModel().getNumBands();
 
-            //check the number of bands looking for alpha band
-            if (surrogateImage.getSampleModel().getNumBands() > 3) {
-                surrogateImage = JAI.create("bandSelect", surrogateImage,
-                        new int[] { 0, 1, 2 });
+                //alpha channel
+                alphaChannel = JAI.create("bandSelect", surrogateImage,
+                        new int[] { numBands - 1 });
+
+                surrogateImage = getBandsFromImage(surrogateImage, numBands);
+            }
+            
+            /**
+             * BAND MERGE
+             * 
+             * If we do not have 3 bands we have no way to go to
+             * index color model in a simple way using jai. Therefore we add 
+             * thebands we need in order to get there. This trick works fine with gray
+             * scale images.
+             * 
+             */
+            
+            if(surrogateImage.getSampleModel().getNumBands() == 1
+            		&&alphaChannel!=null){
+            	int numBands = surrogateImage.getSampleModel().getNumBands();
+            	//getting first band
+            	RenderedImage firstBand = JAI.create("bandSelect", surrogateImage,
+                        new int[] {0 });
+            	
+            	
+            	//adding to the image
+            	for(int i=0;i<3-numBands;i++){
+            			pb.removeParameters();
+            			pb.removeSources();
+            			
+            			pb.addSource(surrogateImage);
+            			pb.addSource(firstBand);
+            			surrogateImage=JAI.create("bandmerge",pb);
+            			System.out.println(i);
+            	}
+
+            }
+            
+            
+            /**
+             * ERROR DIFFUSION 
+             * 
+             * we create a single banded image with index color model.
+             */
+            if (surrogateImage.getSampleModel().getNumBands() == 3) {
+                surrogateImage = reduction2IndexColorModel(surrogateImage, pb);
+            
             }
 
-            if(surrogateImage.getSampleModel().getNumBands()==3){
-            //error dither
-            KernelJAI ditherMask = KernelJAI.ERROR_FILTER_FLOYD_STEINBERG; //KernelJAI.DITHER_MASK_443;
-            ColorCube colorMap = ColorCube.BYTE_496;
-
-            //PARAMETER BLOCK
-            pb.removeParameters();
-            pb.removeSources();
-
-            //color map
-            pb.addSource(surrogateImage);
-            pb.add(colorMap);
-            pb.add(ditherMask);
-
-    
-            RenderedOp op1 = JAI.create("errordiffusion", pb, null);
-            surrogateImage=op1.createSnapshot();
-            
-            
-            //adding transparency
-            //setting up the right layout for this image
-//            ImageLayout layout=new ImageLayout(op1);
-//            byte [] []rgb=colorMap.getByteData();
-//            layout.setColorModel(new IndexColorModel(
-//            		8,//8 bits
-//            		rgb[0].length,
-//            		rgb[0],
-//            		rgb[1],
-//            		rgb[1],
-//            		0));
-//            
-//            pb.removeParameters();
-//            pb.removeSources();
-//            pb.addSource(op1);
-//            RenderingHints hints=new RenderingHints(JAI.KEY_IMAGE_LAYOUT,layout);
-//            surrogateImage=JAI.create("format",pb,hints);
-            
+            /**
+             * TRANSPARENCY
+             * 
+             * Adding transparency if needed, which means using the alpha channel to build a new color
+             * model 
+             */
+            if(alphaChannel!=null){
+            	surrogateImage = addTransparency2IndexXolorModel(surrogateImage,alphaChannel, pb);
             }
-//            else{
-//            	 //adding transparency
-//                //setting up the right layout for this image
-//                ImageLayout layout=new ImageLayout(op1);
-//                byte [] []rgb=colorMap.getByteData();
-//                layout.setColorModel(new IndexColorModel(
-//                		8,//8 bits
-//                		rgb[0].length,
-//                		rgb[0],
-//                		rgb[1],
-//                		rgb[1],
-//                		0));
-//                
-//                pb.removeParameters();
-//                pb.removeSources();
-//                pb.addSource(op1);
-//                RenderingHints hints=new RenderingHints(JAI.KEY_IMAGE_LAYOUT,layout);
-//                surrogateImage=JAI.create("format",pb,hints);            	
-//            }
-
         }
+
         return surrogateImage;
     }
 
-    /**
+	/**
+	 * This method is used to add transparency to a preexisting image whose color model is 
+	 * indexcolormodel.
+	 * 
+	 * There are quite a few step to perform here.
+	 * 1>Creating a new IndexColorModel which supports transparency, using the given image's colormodel
+	 * 
+	 * 2>creating a suitable sample model 
+	 * 
+	 * 3>copying the old sample model to the new sample model.
+	 * 
+	 * 4>looping through the alphaChannel and setting the corresponding pixels
+	 * in the new sample model to the index for transparency
+	 * 
+	 * 5>creating a bufferedimage
+	 * 
+	 * 6>creating a planar image
 	 * @param surrogateImage
+	 * @param alphaChannel
+	 * @param pb
 	 * @return
 	 */
-	private PlanarImage rescale2Byte(PlanarImage surrogateImage) {
+	private PlanarImage addTransparency2IndexXolorModel(PlanarImage surrogateImage, RenderedImage alphaChannel, ParameterBlock pb) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	/**
+	 * @param surrogateImage
+	 * @param pb
+	 * @return
+	 */
+	private PlanarImage reduction2IndexColorModel(PlanarImage surrogateImage, ParameterBlock pb) {
+		//error dither
+		KernelJAI ditherMask = KernelJAI.ERROR_FILTER_STUCKI; //KernelJAI.DITHER_MASK_443;
+		ColorCube colorMap = ColorCube.BYTE_496;
+		//PARAMETER BLOCK
+		pb.removeParameters();
+		pb.removeSources();
+		//color map
+		pb.addSource(surrogateImage);
+		pb.add(colorMap);
+		pb.add(ditherMask);
+		RenderedOp op1 = JAI.create("errordiffusion", pb, null);
+		surrogateImage = op1.createSnapshot();
+		return surrogateImage;
+	}
+
+	/**
+	 * @param surrogateImage
+	 * @param numBands
+	 * @return
+	 */
+	private PlanarImage getBandsFromImage(PlanarImage surrogateImage, int numBands) {
+		switch (numBands-1) {
+		case 1:
+		    surrogateImage = JAI.create("bandSelect", surrogateImage,
+		            new int[] { 0 });
+
+		    break;
+
+		case 3:
+		    surrogateImage = JAI.create("bandSelect", surrogateImage,
+		            new int[] { 0, 1, 2 });
+
+		    break;
+		}
+		return surrogateImage;
+	}
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param surrogateImage
+     *
+     * @return
+     */
+    private PlanarImage rescale2Byte(PlanarImage surrogateImage) {
         //rescale the initial image in order
         //to expand the dynamic
+
         /** EXTREMA */
 
         // Set up the parameter block for the source image and
@@ -458,7 +525,7 @@ public class WorldImageWriter implements GridCoverageWriter {
         pb.removeParameters();
 
         //set the levels for the dynamic
-         pb.addSource(surrogateImage);
+        pb.addSource(surrogateImage);
 
         //rescaling each band to 8 bits
         double[] scale = new double[extrema[0].length];
@@ -469,96 +536,96 @@ public class WorldImageWriter implements GridCoverageWriter {
             offset[i] = -((255 * extrema[0][i]) / (extrema[1][i]
                 - extrema[0][i]));
         }
+
         pb.add(scale);
         pb.add(offset);
+
         RenderedOp image2return = JAI.create("rescale", pb);
 
-        
-        
         //setting up the right layout for this image
-        ImageLayout layout=new ImageLayout(image2return);
+        ImageLayout layout = new ImageLayout(image2return);
         pb.removeParameters();
         pb.removeSources();
         pb.addSource(image2return);
         pb.add(DataBuffer.TYPE_BYTE);
-        RenderingHints hints=new RenderingHints(JAI.KEY_IMAGE_LAYOUT,layout);
-        image2return=JAI.create("format",pb,hints);
 
-      
+        RenderingHints hints = new RenderingHints(JAI.KEY_IMAGE_LAYOUT, layout);
+        image2return = JAI.create("format", pb, hints);
+
         return image2return;
-	}
+    }
 
-//	private RenderedImage highlightImage(RenderedImage stillImg) {
-//        RenderedImage dispImg = null;
-//
-//        // Highlight the human pixels (detection results img)
-//        // Create a constant image
-//        Byte[] bandValues = new Byte[1];
-//
-//        bandValues[0] = new Byte("65"); //32 -- orangeish, 65 -- greenish
-//
-//        ParameterBlock pbConstant = new ParameterBlock();
-//
-//        pbConstant.add(new Float(stillImg.getWidth())); // The width
-//
-//        pbConstant.add(new Float(stillImg.getHeight())); // The height
-//
-//        pbConstant.add(bandValues); // The band values
-//
-//        PlanarImage imgConstant = (PlanarImage) JAI.create("constant",
-//                pbConstant);
-//
-//        //System.out.println("Making multiply image");
-//        // Multiply the mask by 255 so the values are 0 or 255
-//        ParameterBlock pbMultiply = new ParameterBlock();
-//
-//        pbMultiply.addSource((PlanarImage) stillImg);
-//
-//        double[] multiplyArray = new double[] { 255.0 };
-//
-//        pbMultiply.add(multiplyArray);
-//
-//        PlanarImage imgMask = (PlanarImage) JAI.create("multiplyconst",
-//                pbMultiply);
-//
-//        //System.out.println("Making IHS image");
-//        // Create a Intensity, Hue, Saturation image
-//        ParameterBlock pbIHS = new ParameterBlock();
-//
-//        pbIHS.setSource(stillImg, 0); //still img is the intensity
-//
-//        pbIHS.setSource(imgConstant, 1); //constant img is the hue
-//
-//        pbIHS.setSource(imgMask, 2); //mask is the saturation
-//
-//        //create rendering hint for IHS image to specify the color model
-//        ComponentColorModel IHS_model = new ComponentColorModel(IHSColorSpace
-//                .getInstance(), new int[] { 8, 8, 8 }, false, false,
-//                Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
-//
-//        ImageLayout layout = new ImageLayout();
-//
-//        layout.setColorModel(IHS_model);
-//
-//        RenderingHints rh = new RenderingHints(JAI.KEY_IMAGE_LAYOUT, layout);
-//
-//        PlanarImage IHSImg = JAI.create("bandmerge", pbIHS, rh);
-//
-//        //System.out.println("Making RGB image");
-//        // Convert IHS image to a RGB image
-//        ParameterBlock pbRGB = new ParameterBlock();
-//
-//        //create rendering hint for RGB image to specify the color model
-//        ComponentColorModel RGB_model = new ComponentColorModel(ColorSpace
-//                .getInstance(ColorSpace.CS_sRGB), new int[] { 8, 8, 8 }, false,
-//                false, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
-//
-//        pbRGB.addSource(IHSImg);
-//
-//        pbRGB.add(RGB_model);
-//
-//        dispImg = JAI.create("colorconvert", pbRGB);
-//
-//        return dispImg;
-//    }
+    //	private RenderedImage highlightImage(RenderedImage stillImg) {
+    //        RenderedImage dispImg = null;
+    //
+    //        // Highlight the human pixels (detection results img)
+    //        // Create a constant image
+    //        Byte[] bandValues = new Byte[1];
+    //
+    //        bandValues[0] = new Byte("65"); //32 -- orangeish, 65 -- greenish
+    //
+    //        ParameterBlock pbConstant = new ParameterBlock();
+    //
+    //        pbConstant.add(new Float(stillImg.getWidth())); // The width
+    //
+    //        pbConstant.add(new Float(stillImg.getHeight())); // The height
+    //
+    //        pbConstant.add(bandValues); // The band values
+    //
+    //        PlanarImage imgConstant = (PlanarImage) JAI.create("constant",
+    //                pbConstant);
+    //
+    //        //System.out.println("Making multiply image");
+    //        // Multiply the mask by 255 so the values are 0 or 255
+    //        ParameterBlock pbMultiply = new ParameterBlock();
+    //
+    //        pbMultiply.addSource((PlanarImage) stillImg);
+    //
+    //        double[] multiplyArray = new double[] { 255.0 };
+    //
+    //        pbMultiply.add(multiplyArray);
+    //
+    //        PlanarImage imgMask = (PlanarImage) JAI.create("multiplyconst",
+    //                pbMultiply);
+    //
+    //        //System.out.println("Making IHS image");
+    //        // Create a Intensity, Hue, Saturation image
+    //        ParameterBlock pbIHS = new ParameterBlock();
+    //
+    //        pbIHS.setSource(stillImg, 0); //still img is the intensity
+    //
+    //        pbIHS.setSource(imgConstant, 1); //constant img is the hue
+    //
+    //        pbIHS.setSource(imgMask, 2); //mask is the saturation
+    //
+    //        //create rendering hint for IHS image to specify the color model
+    //        ComponentColorModel IHS_model = new ComponentColorModel(IHSColorSpace
+    //                .getInstance(), new int[] { 8, 8, 8 }, false, false,
+    //                Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+    //
+    //        ImageLayout layout = new ImageLayout();
+    //
+    //        layout.setColorModel(IHS_model);
+    //
+    //        RenderingHints rh = new RenderingHints(JAI.KEY_IMAGE_LAYOUT, layout);
+    //
+    //        PlanarImage IHSImg = JAI.create("bandmerge", pbIHS, rh);
+    //
+    //        //System.out.println("Making RGB image");
+    //        // Convert IHS image to a RGB image
+    //        ParameterBlock pbRGB = new ParameterBlock();
+    //
+    //        //create rendering hint for RGB image to specify the color model
+    //        ComponentColorModel RGB_model = new ComponentColorModel(ColorSpace
+    //                .getInstance(ColorSpace.CS_sRGB), new int[] { 8, 8, 8 }, false,
+    //                false, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+    //
+    //        pbRGB.addSource(IHSImg);
+    //
+    //        pbRGB.add(RGB_model);
+    //
+    //        dispImg = JAI.create("colorconvert", pbRGB);
+    //
+    //        return dispImg;
+    //    }
 }
