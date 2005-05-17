47c71387c0406d81578cba630b3e64420ceabdf0
Simone Giannecchini <simboss1@gmail.com>
2005-05-17 16:04:23 +0000

    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@13551 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/image/test/org/geotools/gce/image/WorldImageWriterTest.java
+++ gt/plugin/image/test/org/geotools/gce/image/WorldImageWriterTest.java
@@ -18,13 +18,31 @@ package org.geotools.gce.image;
 
 import junit.framework.*;
 import org.geotools.coverage.grid.*;
+import org.geotools.coverage.operation.Resampler2D;
+import org.geotools.coverage.processing.GridCoverageProcessor2D;
+import org.geotools.factory.Hints;
+import org.geotools.geometry.GeneralDirectPosition;
+import org.geotools.geometry.GeneralEnvelope;
+import org.geotools.referencing.FactoryFinder;
+import org.geotools.referencing.crs.CoordinateReferenceSystem;
+import org.geotools.referencing.operation.CoordinateOperation;
+import org.geotools.referencing.operation.CoordinateOperationFactory;
+import org.geotools.referencing.wkt.Parser;
 import org.geotools.resources.*;
 import org.opengis.coverage.grid.*;
 import org.opengis.parameter.*;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CRSAuthorityFactory;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
+import org.opengis.spatialschema.geometry.MismatchedDimensionException;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.io.*;
 import java.net.*;
+import java.text.ParseException;
+
 import javax.media.jai.PlanarImage;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -58,7 +76,7 @@ public class WorldImageWriterTest extends TestCase {
     protected void tearDown() throws Exception {
         File testDir = TestData.file(this, "");
         String[] files = testDir.list();
-//
+
 //        //removing unnecessary files
 //        if (files != null) {
 //            for (int i = 0; i < files.length; i++)
@@ -72,7 +90,7 @@ public class WorldImageWriterTest extends TestCase {
         super.tearDown();
     }
 
-    public void testWrite() throws MalformedURLException, IOException {
+    public void testWrite() throws MalformedURLException, IOException, IllegalArgumentException, FactoryException, TransformException, ParseException {
         URL url = null;
         File file = null;
         InputStream in = null;
@@ -82,12 +100,12 @@ public class WorldImageWriterTest extends TestCase {
         test_data_dir = TestData.file(this, null);
         String[] fileList = test_data_dir.list(new MyFileFilter());
 
-        for (int i = 0; i < fileList.length; i++) {
-            //url
-            url = TestData.getResource(this, fileList[i]);
-            assertTrue(url != null);
-            this.write(url);
-
+//        for (int i = 0; i < fileList.length; i++) {
+//            //url
+//            url = TestData.getResource(this, fileList[i]);
+//            assertTrue(url != null);
+//            this.write(url);
+//
 //            //getting file
 //            file = TestData.file(this, fileList[i]);
 //
@@ -100,17 +118,17 @@ public class WorldImageWriterTest extends TestCase {
 //            in = new FileInputStream(TestData.file(this, fileList[i]));
 //
 //            this.write(in);
-        }
+//        }
 
         //checking an http link
-//        url = new URL("http://www.sun.com/im/homepage-powered_by_sun.gif");
-//
-//        this.write(url);
-//
-//        in = new URL("http://www.sun.com/im/homepage-powered_by_sun.gif")
-//            .openStream();
-//
-//        this.write(in);
+        url = new URL("http://www.sun.com/im/homepage-powered_by_sun.gif");
+
+        this.write(url);
+
+        in = new URL("http://www.sun.com/im/homepage-powered_by_sun.gif")
+            .openStream();
+
+        this.write(in);
     }
 
     /**
@@ -119,8 +137,12 @@ public class WorldImageWriterTest extends TestCase {
      * @param source Object
      *
      * @throws IOException
+     * @throws TransformException 
+     * @throws FactoryException 
+     * @throws IllegalArgumentException 
+     * @throws ParseException 
      */
-    private void write(Object source) throws IOException {
+    private void write(Object source) throws IOException, IllegalArgumentException, FactoryException, TransformException, ParseException {
         wiReader = new WorldImageReader(source);
 
         Format readerFormat = wiReader.getFormat();
@@ -129,8 +151,10 @@ public class WorldImageWriterTest extends TestCase {
         //setting crs
         GridCoverage2D coverage = null;
 
+		
         coverage = (GridCoverage2D) wiReader.read(null);
-
+//		reprojectAndShow(coverage);
+		
         assertNotNull(coverage);
         assertNotNull(((GridCoverage2D) coverage).getRenderedImage());
         assertNotNull(coverage.getEnvelope());
@@ -153,16 +177,110 @@ public class WorldImageWriterTest extends TestCase {
 
         //writing
         wiWriter.write(coverage, null);
-		
-	    
-//
-//        //reading again
-//        assertTrue(tempFile.exists());
-//        wiReader = new WorldImageReader(tempFile);
-//
-//        coverage = (GridCoverage2D) wiReader.read(null);
-		
-		
 
+        //reading again
+        assertTrue(tempFile.exists());
+        wiReader = new WorldImageReader(tempFile);
+
+        coverage = (GridCoverage2D) wiReader.read(null);
+
+        //displaying
+        JFrame frame = new JFrame();
+        JLabel label = new JLabel(new ImageIcon(
+                    ((PlanarImage) coverage.getRenderedImage())
+                    .getAsBufferedImage()));
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.show();
     }
+
+//	private void reprojectAndShow(GridCoverage2D coverage) throws FactoryException, TransformException, IllegalArgumentException, IOException, ParseException {
+//		//crs authority
+//		final CRSAuthorityFactory factory=FactoryFinder.getCRSAuthorityFactory("EPSG",null);
+//		final CoordinateReferenceSystem wgs84=(CoordinateReferenceSystem) factory.createCoordinateReferenceSystem("EPSG:4326");
+//		//mercator cs
+//		final CSAuthorityFactory factoryCS=FactoryFinder.getCSAuthorityFactory("EPSG",null);
+//		fin
+//		
+//		
+//		
+//		CoordinateReferenceSystem mercator=(CoordinateReferenceSystem) parser.parseCoordinateReferenceSystem(wkt);
+//		
+//          //getting an operation between source and destination crs
+//
+//        CoordinateOperationFactory operationFactory=new CoordinateOperationFactory(new Hints(Hints.LENIENT_DATUM_SHIFT,Boolean.TRUE)));
+//        CoordinateOperation operationMercator2WGS84=(CoordinateOperation) operationFactory.createOperation(mercator,
+//				wgs84);
+//		CoordinateOperation operationWGS842Mercator=(CoordinateOperation) operationFactory.createOperation(wgs84,
+//				mercator);
+//
+//		MathTransform transformWGS842Mercator=operationWGS842Mercator.getMathTransform();
+//		
+//        //reproject the envelope
+//        GeneralEnvelope newEnvelope=(GeneralEnvelope) coverage.getEnvelope(),
+//		oldEnvelope=null;
+//		oldEnvelope=new GeneralEnvelope(
+//        		(GeneralDirectPosition )transformWGS842Mercator.transform(newEnvelope.getLowerCorner(),null),
+//        		(GeneralDirectPosition )transformWGS842Mercator.transform(newEnvelope.getUpperCorner(),null)
+//        		);
+//		//construct the right coverage
+//		GridCoverage2D gc=new GridCoverage2D(
+//				"a",
+//				coverage.getRenderedImage(),
+//				mercator,
+//				oldEnvelope,
+//				new Hints(Hints.AVOID_NON_GEOPHYSICS,Boolean.TRUE));
+//		
+//        
+//        //creating the new grid range keeping the old range
+//        GeneralGridRange newGridrange = new GeneralGridRange(new int[] { 0, 0 },
+//                new int[] { gc.getGridGeometry().getGridRange().getLength(0),  gc.getGridGeometry().getGridRange().getLength(1) });
+//        GridGeometry2D newGridGeometry = new GridGeometry2D(newGridrange,
+//                newEnvelope, new boolean[] { false, true });
+//
+//        //getting the needed operation
+//        Resampler2D.Operation op = new Resampler2D.Operation();
+//
+//        //getting parameters
+//        ParameterValueGroup group = op.getParameters();
+//        group.parameter("Source").setValue(gc.geophysics(false));
+//        group.parameter("CoordinateReferenceSystem").setValue(wgs84);
+//        group.parameter("GridGeometry").setValue(newGridGeometry);
+//
+//        GridCoverageProcessor2D processor2D = new GridCoverageProcessor2D(GridCoverageProcessor2D
+//            .getDefault(),new
+//			Hints(Hints.LENIENT_DATUM_SHIFT, Boolean.TRUE));
+//        GridCoverage2D gcOp = (GridCoverage2D) processor2D.doOperation(op, group);
+//   	
+//        //displaying
+//        JFrame frame = new JFrame();
+//        JPanel topPanel = new JPanel();
+//        topPanel.setLayout(new BorderLayout());
+//        frame.getContentPane().add(topPanel);
+//
+//        frame.setBackground(Color.black);
+//
+//		JScrollPane pane = new JScrollPane();
+//        pane.getViewport().add(new JLabel(
+//                new ImageIcon(
+//                    ((PlanarImage) gcOp.getRenderedImage())
+//                    .getAsBufferedImage())));
+//        topPanel.add(pane, BorderLayout.CENTER);
+//        frame.getContentPane().add(pane);
+//        frame.getContentPane().add(pane, BorderLayout.CENTER);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.show();
+//        GridCoverageWriter writer = new WorldImageWriter(new File(
+//        "c:\\a1.tif"));
+//		writer.write(gcOp, null);
+//
+////        //printing
+//        System.out.println(gc.getCoordinateReferenceSystem().toWKT());
+//        System.out.println(gcOp.getCoordinateReferenceSystem().toWKT());
+//		System.out.println(gcOp.getEnvelope().toString());
+//
+//		
+//	}
 }
