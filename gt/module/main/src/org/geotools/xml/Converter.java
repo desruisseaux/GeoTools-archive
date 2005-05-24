9ae0cbe50bed41473d7c581c83a125c8d6a05873
Richard Gould <rgould@refractions.net>
2005-05-24 23:40:37 +0000

    Beginning of XDO wrapper class
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@13774 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/xml/Converter.java
+++ gt/module/main/src/org/geotools/xml/Converter.java
@@ -6,24 +6,59 @@
  */
 package org.geotools.xml;
 
+import org.geotools.xml.schema.Element;
+import org.geotools.xml.schema.Schema;
+
 /**
  * @author Administrator
  */
 public class Converter {
 	protected static org.geotools.xml.schema.Schema convert(com.vividsolutions.xdo.xsi.Schema in){
-		// TODO fill me in
-		return null;
+	    SchemaWrapper wrapper = (SchemaWrapper) in;
+        return wrapper.getGtSchema();
 	}
 	protected static com.vividsolutions.xdo.xsi.Schema convert(org.geotools.xml.schema.Schema in){
-		// TODO fill me in
-		return null;
+        return new SchemaWrapper(in);
 	}
 	protected static org.geotools.xml.schema.Element convert(com.vividsolutions.xdo.xsi.Element in){
-		// TODO fill me in
-		return null;
+	    ElementWrapper wrapper = (ElementWrapper) in;
+        return wrapper.getGtElement();
 	}
 	protected static com.vividsolutions.xdo.xsi.Element convert(org.geotools.xml.schema.Element in){
-		// TODO fill me in
-		return null;
+        return new ElementWrapper(in);
 	}
+    
+    static class SchemaWrapper extends com.vividsolutions.xdo.xsi.Schema {
+        org.geotools.xml.schema.Schema gtSchema;
+
+        public SchemaWrapper( Schema schema ) {
+            super();
+            gtSchema = schema;
+        }
+
+        public org.geotools.xml.schema.Schema getGtSchema() {
+            return gtSchema;
+        }
+
+        public void setGtSchema( org.geotools.xml.schema.Schema gtSchema ) {
+            this.gtSchema = gtSchema;
+        }
+    }
+    
+    static class ElementWrapper extends com.vividsolutions.xdo.xsi.Element {
+        org.geotools.xml.schema.Element gtElement;
+
+        public ElementWrapper( Element element ) {
+            super();
+            gtElement = element;
+        }
+
+        public org.geotools.xml.schema.Element getGtElement() {
+            return gtElement;
+        }
+
+        public void setGtElement( org.geotools.xml.schema.Element gtElement ) {
+            this.gtElement = gtElement;
+        }
+    }
 }
