1fca5206401720ded6a3161943539434a6713607
Richard Gould <rgould@refractions.net>
2005-05-25 00:17:25 +0000

    Second iteration of XDO wrapper class - attempting to map the two apis together
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@13776 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/xml/Converter.java
+++ gt/module/main/src/org/geotools/xml/Converter.java
@@ -6,8 +6,18 @@
  */
 package org.geotools.xml;
 
+import java.net.URI;
+
 import org.geotools.xml.schema.Element;
 import org.geotools.xml.schema.Schema;
+
+import com.vividsolutions.xdo.xsi.Attribute;
+import com.vividsolutions.xdo.xsi.AttributeGroup;
+import com.vividsolutions.xdo.xsi.ComplexType;
+import com.vividsolutions.xdo.xsi.Group;
+import com.vividsolutions.xdo.xsi.Import;
+import com.vividsolutions.xdo.xsi.SimpleType;
+import com.vividsolutions.xdo.xsi.Type;
 
 /**
  * @author Administrator
@@ -27,6 +37,14 @@ public class Converter {
 	protected static com.vividsolutions.xdo.xsi.Element convert(org.geotools.xml.schema.Element in){
         return new ElementWrapper(in);
 	}
+	protected static org.geotools.xml.schema.Type convert(com.vividsolutions.xdo.xsi.Type in){
+        TypeWrapper wrapper = (TypeWrapper) in;
+        return wrapper.getGtType();
+    }
+	protected static com.vividsolutions.xdo.xsi.Type convert(org.geotools.xml.schema.Type in){
+        return new TypeWrapper(in);
+    }
+    
     
     static class SchemaWrapper extends com.vividsolutions.xdo.xsi.Schema {
         org.geotools.xml.schema.Schema gtSchema;
@@ -43,6 +61,126 @@ public class Converter {
         public void setGtSchema( org.geotools.xml.schema.Schema gtSchema ) {
             this.gtSchema = gtSchema;
         }
+
+        @Override
+        public AttributeGroup[] getAttributeGroups() {
+            return super.getAttributeGroups();
+        }
+
+        @Override
+        public Attribute[] getAttributes() {
+            return super.getAttributes();
+        }
+
+        @Override
+        public int getBlockDefault() {
+            return super.getBlockDefault();
+        }
+
+        @Override
+        public ComplexType[] getComplexTypes() {
+            return super.getComplexTypes();
+        }
+
+        @Override
+        public com.vividsolutions.xdo.xsi.Element[] getElements() {
+            return super.getElements();
+        }
+
+        @Override
+        public int getFinalDefault() {
+            return super.getFinalDefault();
+        }
+
+        @Override
+        public Group[] getGroups() {
+            return super.getGroups();
+        }
+
+        @Override
+        public Import[] getImports() {
+            return super.getImports();
+        }
+
+        @Override
+        public String getPrefix() {
+            return super.getPrefix();
+        }
+
+        @Override
+        public SimpleType[] getSimpleTypes() {
+            return super.getSimpleTypes();
+        }
+
+        @Override
+        public URI getTargetNamespace() {
+            return super.getTargetNamespace();
+        }
+
+        @Override
+        public URI[] getUris() {
+            return super.getUris();
+        }
+
+        @Override
+        public String getVersion() {
+            return super.getVersion();
+        }
+
+        @Override
+        public boolean isAttributeFormDefault() {
+            return super.isAttributeFormDefault();
+        }
+
+        @Override
+        public boolean isElementFormDefault() {
+            return super.isElementFormDefault();
+        }
+
+        @Override
+        public String getId() {
+            return super.getId();
+        }
+        
+        
+    }
+    
+    static class TypeWrapper extends com.vividsolutions.xdo.xsi.Type {
+        org.geotools.xml.schema.Type gtType;
+
+        public TypeWrapper( org.geotools.xml.schema.Type type ) {
+            super();
+            gtType = type;
+        }
+
+        @Override
+        public int getFinal() {
+            return -1; //TODO no direct mapping
+        }
+
+        @Override
+        public String getName() {
+            return gtType.getName();
+        }
+
+        @Override
+        public URI getNamespace() {
+            return gtType.getNamespace();
+        }
+
+        @Override
+        public String getId() {
+            return null; //TODO no direct mapping
+        }
+
+        public org.geotools.xml.schema.Type getGtType() {
+            return gtType;
+        }
+
+        public void setGtType( org.geotools.xml.schema.Type gtType ) {
+            this.gtType = gtType;
+        }
+        
     }
     
     static class ElementWrapper extends com.vividsolutions.xdo.xsi.Element {
@@ -59,6 +197,86 @@ public class Converter {
 
         public void setGtElement( org.geotools.xml.schema.Element gtElement ) {
             this.gtElement = gtElement;
+        }
+
+        @Override
+        public int getBlock() {
+            return gtElement.getBlock();
+        }
+
+        @Override
+        public String getDefault() {
+            return gtElement.getDefault();
+        }
+
+        @Override
+        public int getFinal() {
+            return gtElement.getFinal();
+        }
+
+        @Override
+        public String getFixed() {
+            return gtElement.getFixed();
+        }
+
+        @Override
+        public String getName() {
+            return gtElement.getName();
+        }
+
+        @Override
+        public URI getNamespace() {
+            return gtElement.getNamespace();
+        }
+
+        @Override
+        public com.vividsolutions.xdo.xsi.Element getSubstitutionGroup() {
+            return convert(gtElement.getSubstitutionGroup());
+        }
+
+        @Override
+        public Type getType() {
+            return convert(gtElement.getType());
+        }
+
+        @Override
+        public boolean isAbstract() {
+            return gtElement.isAbstract();
+        }
+
+        @Override
+        public boolean isForm() {
+            return gtElement.isForm();
+        }
+
+        @Override
+        public boolean isNillable() {
+            return gtElement.isNillable();
+        }
+
+        @Override
+        public int getGrouping() {
+            return gtElement.getGrouping();
+        }
+
+        @Override
+        public int getMaxOccurs() {
+            return gtElement.getMaxOccurs();
+        }
+
+        @Override
+        public int getMinOccurs() {
+            return gtElement.getMinOccurs();
+        }
+
+        @Override
+        public String getId() {
+            return gtElement.getId();
+        }
+
+        @Override
+        public boolean equals( Object obj ) {
+            return gtElement.equals(obj);
         }
     }
 }
