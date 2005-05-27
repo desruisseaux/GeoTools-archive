6d29945df85a4975530b7db73d8e4151ce666f33
James Macgill <jmacgill@users.sourceforge.net>
2005-05-27 17:17:36 +0000

    excluded Geot553Test until it is chagned or URL handling is fixed to acomodate what it is trying to do.
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@13885 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/wms/test/org/geotools/data/wms/test/Geot553Test.java
+++ gt/plugin/wms/test/org/geotools/data/wms/test/Geot553Test.java
@@ -30,8 +30,9 @@ import org.opengis.spatialschema.geometry.Envelope;
 public class Geot553Test extends TestCase {
     
     public void testGeot553 () throws Exception {
-        File getCaps = TestData.file(this, "geot553capabilities.xml");
-        URL getCapsURL = getCaps.toURL();
+       // File getCaps = TestData.file(this, "geot553capabilities.xml");
+        
+        URL getCapsURL = TestData.getResource(this, "geot553capabilities.xml");
         
         WebMapServer wms = new WebMapServer(getCapsURL);
         Layer layer = wms.getCapabilities().getLayer().getChildren()[2];
