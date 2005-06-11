0a6cf12450c210a58c368ce80b11d9f0ca697145
Jesse Eichar <jesse.eichar@camptocamp.com>
2005-06-11 00:35:04 +0000

    forgotten
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@14259 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/module/main/test/org/geotools/data/DiffWriterTest.java
@@ -0,0 +1,76 @@
+package org.geotools.data;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import junit.framework.TestCase;
+
+import org.geotools.feature.Feature;
+import org.geotools.feature.FeatureType;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+
+public class DiffWriterTest extends TestCase {
+
+	DiffFeatureWriter writer;
+	private Point geom;
+	private FeatureType type;
+	
+	protected void setUp() throws Exception {
+        type = DataUtilities.createType("default", "name:String,*geom:Geometry");
+        GeometryFactory fac=new GeometryFactory();
+        geom = fac.createPoint(new Coordinate(10,10));
+		
+		HashMap diff=new HashMap();
+		diff.put("diff1", type.create(new Object[]{ "diff1", geom }, "diff1"));
+		FeatureReader reader=new TestReader(type,type.create(new Object[]{ "original", geom }, "original") );
+		writer=new DiffFeatureWriter(reader, diff){
+
+			protected void fireNotification(int eventType, Envelope bounds) {
+				// 
+			}
+			
+		};
+	}
+
+	public void testRemove() throws Exception {
+		writer.next();
+		Feature feature=writer.next();
+		writer.remove();
+		assertNull(writer.diff.get(feature.getID()));
+	}
+
+	public void testHasNext() throws Exception {
+		assertTrue(writer.hasNext());
+		assertEquals(1, writer.diff.size());
+		Feature feature=writer.next();
+		assertTrue(writer.hasNext());
+		assertEquals(1, writer.diff.size());
+		feature=writer.next();
+		assertFalse(writer.hasNext());
+		assertEquals(1, writer.diff.size());
+	}
+	
+	public void testWrite() throws IOException, Exception {
+		while( writer.hasNext() ){
+			writer.next();
+		}
+		Feature feature=writer.next();
+		feature.setAttribute("name", "new1");
+		
+		writer.write();
+		
+		assertEquals(2, writer.diff.size() );
+		feature=writer.next();
+		feature.setAttribute("name", "new2");
+		
+		writer.write();
+		
+		assertEquals(3, writer.diff.size() );
+	}
+
+
+}
---
+++ gt/module/main/test/org/geotools/data/TestReader.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.geotools.data;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+import org.geotools.feature.Feature;
+import org.geotools.feature.FeatureType;
+import org.geotools.feature.IllegalAttributeException;
+
+class TestReader implements FeatureReader{
+
+    /**
+	 * 
+	 */
+	private FeatureType type;
+	private Feature feature;
+
+    public TestReader(FeatureType type, Feature f) {
+        this.type = type;
+		this.feature=f;
+    }
+    
+    public FeatureType getFeatureType() {
+        return type;
+    }
+
+    public Feature next() throws IOException, IllegalAttributeException, NoSuchElementException {
+        next=false;
+        return feature;
+    }
+
+    boolean next=true;
+    public boolean hasNext() throws IOException {
+        return next;
+    }
+
+    public void close() throws IOException {
+    }
+    
+}
