e89cc9aef474e72a7884f8aedd867daa8cbc7993
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2005-07-11 13:09:49 +0000

    Removed accidental J2SE 1.5 dependency
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@14765 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/ext/widgets-swing/src/org/geotools/gui/swing/DisjointLists.java
+++ gt/ext/widgets-swing/src/org/geotools/gui/swing/DisjointLists.java
@@ -180,7 +180,7 @@ public class DisjointLists extends JPanel {
                 System.arraycopy(visibles, insertAt, visibles, insertAt+length, size-insertAt);
                 System.arraycopy(source.visibles, subLower, visibles, insertAt, length);
                 size += length;
-                assert isSorted() : Arrays.toString(visibles); // TODO: uncomment for J2SE 1.5.
+                assert isSorted(); // : Arrays.toString(visibles); // TODO: uncomment for J2SE 1.5.
                 fireIntervalAdded(this, insertAt, insertAt+length-1);
             }
             source.hide(lower, upper);
