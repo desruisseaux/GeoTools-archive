7ba7bf5f6d061bdc9475b566591a1ddddc2511b6
Jody Garnett <jody.garnett@gmail.com>
2005-07-29 04:16:21 +0000

    Rolled back changes, pending dzwiers approval
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@14975 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/wfs/test/org/geotools/data/wfs/GeoServerTest.java
+++ gt/plugin/wfs/test/org/geotools/data/wfs/GeoServerTest.java
@@ -1,15 +1,9 @@
 package org.geotools.data.wfs;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.ConnectException;
 import java.net.MalformedURLException;
-import java.net.NoRouteToHostException;
 import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 import junit.framework.TestCase;
@@ -21,16 +15,12 @@ import org.geotools.data.FeatureEvent;
 import org.geotools.data.FeatureListener;
 import org.geotools.data.FeatureReader;
 import org.geotools.data.FeatureSource;
-import org.geotools.data.Query;
 import org.geotools.data.Transaction;
 import org.geotools.factory.FactoryConfigurationError;
 import org.geotools.feature.Feature;
-import org.geotools.feature.FeatureCollection;
-import org.geotools.feature.FeatureIterator;
 import org.geotools.feature.FeatureType;
 import org.geotools.feature.IllegalAttributeException;
 import org.geotools.filter.FidFilter;
-import org.geotools.filter.Filter;
 import org.geotools.filter.FilterFactory;
 import org.geotools.filter.IllegalFilterException;
 import org.xml.sax.SAXException;
@@ -45,18 +35,19 @@ import com.vividsolutions.jts.geom.Polygon;
 
 public class GeoServerTest extends TestCase {
 
-    private URL locale = null;
-    private URL remote = null;
+    private URL url = null;
+    
     public void setUp() throws MalformedURLException { 
-         locale = new URL("http://localhost:8080/geoserver/wfs?REQUEST=GetCapabilities");
-         if( locale != null && locale.toString().indexOf("localhost")!= -1 ) {
+         url = new URL("http://localhost:8080/geoserver/wfs?REQUEST=GetCapabilities");
+//       url = new URL("http://www.refractions.net:8080/geoserver/wfs?REQUEST=GetCapabilities");
+         if( url != null && url.toString().indexOf("localhost")!= -1 ) {
              InputStream stream = null;             
              try {
-                 stream = locale.openStream();
+                 stream = url.openStream();
              }
              catch( Throwable t ) {
                  System.err.println("Warning you local geoserver is not available - "+getName()+" test disabled ");
-                 locale = null;
+                 url = null;
              }
              finally {
                  if( stream != null )
@@ -67,98 +58,30 @@ public class GeoServerTest extends TestCase {
                     }
              }
          }
-         remote = new URL("http://www.refractions.net:8000/geoserver/wfs?REQUEST=GetCapabilities");         
     }
-    public WFSDataStore connect(URL capabilities) throws Exception {
-        Map m = new HashMap();
-        m.put(WFSDataStoreFactory.URL.key,capabilities);
-        m.put(WFSDataStoreFactory.TIMEOUT.key,new Integer(10000)); // was 1000000 for debug
-        return (WFSDataStore)(new WFSDataStoreFactory()).createNewDataStore(m);
-    }   
-    public void testTypes() throws Exception {
-        WFSDataStore wfs; 
-        try {
-            wfs = connect( remote );
-        } catch (ConnectException e) {
-            e.printStackTrace(System.err);
-            return;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(System.err);
-            return;
-        } catch (UnknownHostException e) {
-            e.printStackTrace(System.err);
-            return;
-        } catch (NoRouteToHostException e) {
-            e.printStackTrace(System.err);
-            return;
-        }
-        String types[] = wfs.getTypeNames();
-        String typeName = "unknown";        
-        for( int i=0; i<types.length;i++){
-            typeName = types[i];
-            System.out.println("Type:"+typeName );
-            try {
-                FeatureType type = wfs.getSchema( typeName );
-                System.out.print( " -  typeName: " + type.getTypeName() );
-                System.out.println( " - namespace: "); System.out.println( type.getNamespace() );
-                System.out.println( " -      geom:"); System.out.println( type.getDefaultGeometry() );
-                System.out.println( " -       crs:"); System.out.println(type.getDefaultGeometry().getCoordinateSystem() );
-                
-                FeatureSource source = wfs.getFeatureSource( typeName );
-                Envelope e = source.getBounds();
-                System.out.println( "-     bounds:"); System.out.println(source.getBounds() );
-                
-                FeatureCollection features = source.getFeatures();
-                System.out.println( "- all bounds:"); System.out.println(features.getBounds() );
-                System.out.println( "- all schema:"); System.out.println(features.getSchema() );
-                System.out.println( "- all   type:"); System.out.println(features.getFeatureType() );
-                
-                DefaultQuery query = new DefaultQuery( typeName, Filter.NONE, 20, Query.ALL_NAMES, "work already" );
-                features = source.getFeatures( query );
-                System.out.println( "- 20   size:"); System.out.println(features.size() );
-                System.out.println( "- 20   count:"); System.out.println(features.getCount() );
-                FeatureReader reader = features.reader();
-                while( reader.hasNext() ){
-                    Feature feature = reader.next();
-                    System.out.println( feature.getID()+":"+feature );
-                }
-                reader.close();
-                
-                FeatureIterator iterator = features.features();
-                while( iterator.hasNext() ){
-                    Feature feature = iterator.next();
-                    System.out.println( feature.getID()+":"+feature );
-                }
-                features.close( iterator );
-            }
-            catch (Throwable t ){
-                System.out.println("Failure on "+typeName );
-                t.printStackTrace();
-            }
-        }
-    }
+    
     public void testFeatureType() throws NoSuchElementException, IOException, SAXException{
-        WFSDataStoreReadTest.doFeatureType(locale,true,true,0);
+        WFSDataStoreReadTest.doFeatureType(url,true,true,0);
     }
     public void testFeatureReader() throws NoSuchElementException, IOException, IllegalAttributeException, SAXException{
-        WFSDataStoreReadTest.doFeatureReader(locale,true,true,0);
+        WFSDataStoreReadTest.doFeatureReader(url,true,true,0);
     }
     public void testFeatureReaderWithFilter() throws NoSuchElementException, IllegalAttributeException, IOException, SAXException{
-        WFSDataStoreReadTest.doFeatureReaderWithFilter(locale,true,true,0);
+        WFSDataStoreReadTest.doFeatureReaderWithFilter(url,true,true,0);
     }    
     public void testFeatureReaderWithFilterGET() throws NoSuchElementException, IllegalAttributeException, IOException, SAXException{
-        WFSDataStoreReadTest.doFeatureReaderWithFilter(locale,true,false,0);
+        WFSDataStoreReadTest.doFeatureReaderWithFilter(url,true,false,0);
     }
     public void testFeatureReaderWithFilterPOST() throws NoSuchElementException, IllegalAttributeException, IOException, SAXException{
-        WFSDataStoreReadTest.doFeatureReaderWithFilter(locale,false,true,0);
+        WFSDataStoreReadTest.doFeatureReaderWithFilter(url,false,true,0);
     }    
     public void testFeatureReaderWithFilterBBoxGET() throws NoSuchElementException, IllegalAttributeException, IOException, SAXException, IllegalFilterException{
         Envelope bbox = new Envelope(-75.791435,38.44949,-75.045998,39.840008);        
-        WFSDataStoreReadTest.doFeatureReaderWithBBox(locale,true,false,0,bbox);
+        WFSDataStoreReadTest.doFeatureReaderWithBBox(url,true,false,0,bbox);
     }
     public void testFeatureReaderWithFilterBBoxPOST() throws NoSuchElementException, IllegalAttributeException, IOException, SAXException, IllegalFilterException{
         Envelope bbox = new Envelope(-75.791435,38.44949,-75.045998,39.840008);        
-        WFSDataStoreReadTest.doFeatureReaderWithBBox(locale,true,false,0,bbox);
+        WFSDataStoreReadTest.doFeatureReaderWithBBox(url,true,false,0,bbox);
     }    
 //    public void testWrite() throws NoSuchElementException, IllegalFilterException, FactoryConfigurationError, IOException, IllegalAttributeException{
 //        DataStore post = WFSDataStoreWriteTest.getDataStore(url);
@@ -221,10 +144,10 @@ public class GeoServerTest extends TestCase {
      * </p>
      */
     public void testWrite() throws NoSuchElementException, IllegalFilterException, FactoryConfigurationError, IOException, IllegalAttributeException{
-        if( locale == null || locale.toString().indexOf("localhost") != -1 ) {        
+        if( url == null || url.toString().indexOf("localhost") != -1 ) {        
             return; // only test writing against developers local geoserver
         }
-        DataStore post = WFSDataStoreWriteTest.getDataStore(locale);        
+        DataStore post = WFSDataStoreWriteTest.getDataStore(url);        
         FeatureType ft = post.getSchema( "states" );
         FeatureSource fs = post.getFeatureSource( "states" );        
         class Watcher implements FeatureListener {
