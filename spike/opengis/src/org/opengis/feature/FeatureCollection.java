00293a7395f2e14bab4bd18419b32c1b2cb34957
Jody Garnett <jody.garnett@gmail.com>
2005-08-26 08:22:30 +0000

    Good local revision history - saves me again.
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@15522 e5c1c795-43da-0310-a71f-fac65c449510

--- spike/opengis/src/org/opengis/feature/FeatureCollection.java
+++ spike/opengis/src/org/opengis/feature/FeatureCollection.java
@@ -1,2 +1,22 @@
 package org.opengis.feature;
 
+import java.util.Iterator;
+
+import org.opengis.feature.type.FeatureCollectionType;
+
+public interface FeatureCollection {
+    FeatureCollectionType getType();
+
+    /**
+	 * Access contents of this collection.
+	 * <p>
+	 * Note XPath: the contents of a GML collection are represented by either
+	 * featureMember or featureMembers. When interpretting an XPath expression,
+	 * you should consider this function to visit both elements for you.
+	 */
+    Iterator<Feature> features();
+
+    /** You are required to close iterators after use */
+    void close( Iterator<Feature> iterator );
+}
+
--- spike/opengis/src/org/opengis/feature/GeometryAttribute.java
+++ spike/opengis/src/org/opengis/feature/GeometryAttribute.java
@@ -1,2 +1,28 @@
 package org.opengis.feature;
 
+import org.opengis.feature.type.Type;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.spatialschema.geometry.Geometry;
+
+/**
+ * Represent a Geometry as complex content.
+ * <p>
+ * List of information to make available through Complex api:
+ * <ul>
+ * <li>srs
+ * <li>bounds
+ * </ul>
+ */
+interface GeometryAttribute {
+   CoordinateReferenceSystem getCRS();
+   Object getBounds();
+   /**
+    * Type should be configued with a Geometry for getJavaType.
+    * <p>
+    * If needed a set of well-known GeometryType can be constructed.
+    */
+   Type getType();
+   Geometry get();
+   void set( Geometry geom );
+}
+
