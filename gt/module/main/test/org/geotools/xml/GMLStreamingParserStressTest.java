43868c5b142fab10a03d5bd2679577e265179e5c
James Macgill <jmacgill@users.sourceforge.net>
2005-09-26 17:32:41 +0000

    Renamed streaming parser test to match 'Stress' catagory.  Will be omitted from day to day builds.
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@15851 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/test/org/geotools/xml/GMLStreamingParserStressTest.java
+++ gt/module/main/test/org/geotools/xml/GMLStreamingParserStressTest.java
@@ -22,7 +22,7 @@ import org.geotools.xml.gml.FCBuffer;
  *
  * @author dzwiers www.refractions.net
  */
-public class GMLStreamingParserTest extends TestCase {
+public class GMLStreamingParserStressTest extends TestCase {
 
 //    public void testOSDNFFeatures() throws SAXException, IOException {
 //        FeatureReader fr = null;
