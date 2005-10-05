c47abbd090cf0b9423a97e71807c6b2228bf86b5
Gabriel Roldan <gabriel.roldan@gmail.com>
2005-10-05 13:44:27 +0000

    commented out since cannot implement that way. Jody: could you create an implementation of this? may its just my knowledge of Java5 isn't enough
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@16056 e5c1c795-43da-0310-a71f-fac65c449510

--- spike/opengis/src/org/opengis/feature/type/GeometryType.java
+++ spike/opengis/src/org/opengis/feature/type/GeometryType.java
@@ -3,5 +3,5 @@ package org.opengis.feature.type;
 import org.opengis.spatialschema.geometry.Geometry;
 
 public interface GeometryType extends AttributeType {	
-	public <T extends Geometry> Class<T> getBinding();	
+	//public <T extends Geometry> Class<T> getBinding();	
 }
