042ff6ac4f4afd79bc259d96b62092d7898611d3
Gabriel Roldan <gabriel.roldan@gmail.com>
2005-10-05 13:51:28 +0000

    restricted default geom to GeometryType
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@16061 e5c1c795-43da-0310-a71f-fac65c449510

--- spike/opengis/test/org/geotools/feature/type/FeatureCollectionTypeImpl.java
+++ spike/opengis/test/org/geotools/feature/type/FeatureCollectionTypeImpl.java
@@ -5,19 +5,18 @@ import java.util.Set;
 import javax.xml.namespace.QName;
 
 import org.opengis.feature.schema.Descriptor;
-import org.opengis.feature.type.AttributeType;
-import org.opengis.feature.type.ComplexType;
 import org.opengis.feature.type.FeatureCollectionType;
+import org.opengis.feature.type.GeometryType;
 import org.opengis.filter.Filter;
 
 public class FeatureCollectionTypeImpl extends FeatureTypeImpl implements FeatureCollectionType {
 	protected final Descriptor MEMBERDESCRIPTOR;
 
-	public FeatureCollectionTypeImpl( QName name, Descriptor members, Descriptor schema, AttributeType defaultGeom) {
+	public FeatureCollectionTypeImpl( QName name, Descriptor members, Descriptor schema, GeometryType defaultGeom) {
 		super(name, schema, defaultGeom);
 		MEMBERDESCRIPTOR = members;
 	}
-	public FeatureCollectionTypeImpl( QName name, Descriptor members, Descriptor schema, AttributeType defaultGeom, Set<Filter> restrictions, FeatureCollectionType superType, boolean isAbstract){
+	public FeatureCollectionTypeImpl( QName name, Descriptor members, Descriptor schema, GeometryType defaultGeom, Set<Filter> restrictions, FeatureCollectionType superType, boolean isAbstract){
 		super( name, schema, defaultGeom, restrictions, superType, isAbstract );
 		MEMBERDESCRIPTOR = members;
 	}	
--- spike/opengis/test/org/geotools/feature/type/FeatureTypeImpl.java
+++ spike/opengis/test/org/geotools/feature/type/FeatureTypeImpl.java
@@ -6,25 +6,25 @@ import javax.xml.namespace.QName;
 
 import org.opengis.feature.Feature;
 import org.opengis.feature.schema.Descriptor;
-import org.opengis.feature.type.AttributeType;
-import org.opengis.feature.type.ComplexType;
 import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.GeometryType;
 import org.opengis.filter.Filter;
 
 public class FeatureTypeImpl extends ComplexTypeImpl implements FeatureType {
-	protected AttributeType DEFAULT;
+	protected GeometryType DEFAULT;
 
-	public FeatureTypeImpl(String name, Descriptor schema, AttributeType defaultGeom ) {
+	public FeatureTypeImpl(String name, Descriptor schema, GeometryType defaultGeom ) {
 		this( new QName( name ), schema, defaultGeom );
 	}	
-	public FeatureTypeImpl(QName name, Descriptor schema, AttributeType defaultGeom ) {
+	public FeatureTypeImpl(QName name, Descriptor schema, GeometryType defaultGeom ) {
 		super(name, schema, true, Feature.class, false, null);
 		DEFAULT = defaultGeom;
 	}
-	public FeatureTypeImpl(QName name, Descriptor schema, AttributeType defaultGeom, Set<Filter> restrictions, FeatureType superType, boolean isAbstract){
+	public FeatureTypeImpl(QName name, Descriptor schema, GeometryType defaultGeom, Set<Filter> restrictions, FeatureType superType, boolean isAbstract){
 		super(name, schema, true, Feature.class, false, restrictions, superType, isAbstract );
+		DEFAULT = defaultGeom;
 	}
-	public AttributeType getDefaultGeometry() {
+	public GeometryType getDefaultGeometry() {
 		return DEFAULT;
 	}
 }
