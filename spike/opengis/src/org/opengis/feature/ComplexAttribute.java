5e1e9743993b1a10438dff03c7f52b3957cadc03
Gabriel Roldan <gabriel.roldan@gmail.com>
2005-10-10 08:26:48 +0000

    established List.class as the prescribed type binding of Complex
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@16174 e5c1c795-43da-0310-a71f-fac65c449510

--- spike/opengis/src/org/opengis/feature/ComplexAttribute.java
+++ spike/opengis/src/org/opengis/feature/ComplexAttribute.java
@@ -5,72 +5,104 @@ import java.util.List;
 import org.opengis.feature.type.AttributeType;
 import org.opengis.feature.type.ComplexType;
 
+/**
+ * 
+ * @author Jody Garnett
+ * @author Gabriel Roldan
+ */
 public interface ComplexAttribute extends Attribute {
-   /** 
-    * Access the type of this construct.
-    */
-   ComplexType getType();
-   
-   /**
-    * Access to contents of this Feature.
-    * <p>
-    * This represents the contents of this ComplexAttribute value, the methods
-    * types() and values() return "views" into this data structure in a manner
-    * similar to Maps.keySet() and values().
-    * </p>
-    * <p>
-    * Java Bean property conventions are used to indicate that this is
-    * part of our data model.
-    * </p>
-    * @see types()
-    * @see values()
-    */
-   List<Attribute> getAttributes();
-   
-   /**
-    * List view of attribtue types, in a manner similar Map.keys().
-    * <p>
-    * The content avalable through types() an values() are considered a view of attribtues().
-    * Order is maintained, and removing content will result in a modification to all three lists.
-    * in a manner simialr to Map.keysSet() and Map.values().
-    * <p>
-    * Collections naming conventions are used to indicate this is a view into
-    * our data model.
-    */
-   List<AttributeType> types();
+	/**
+	 * Access the type of this construct.
+	 */
+	ComplexType getType();
 
-   /**
-    * Value view of attribtue types, in a manner similar Map.keys().
-    * <p>
-    * The content avalable through types() an values() are considered a view of attribtues().
-    * Order is maintained, and removing content will result in a modification to all three lists.
-    * in a manner simialr to Map.keysSet() and Map.values().
-    * <p>
-    * Collections naming conventions are used to indicate this is a view into
-    * our data model.
-    */
-   List<Object> values();
-   
-   /**
-    * Access to value associated with name.
-    * <p>
-    * This method acts as a precanned search of getAttribtues() based on AttributeType,
-    * where type is determined by getType().type( name )
-    * <ul>
-    * <li>AttributeType by Descriptor 1:1 - it will return an Object of the bound java class indicated by AttributeType
-    * <li>AttributeType by Descriptor 0:* - it will return a possibly empty List of the bound java class indicated by AttributeType
-    * </p>
-    * <p>
-    * This method is not considered useful for accessing content with multiplicity
-    * @param name
-    * @return Object, or List based on type
-    *
-   Object get( String name );
-    */
-   /**
-    * Access to value associated with type.
-    * @param type
-    * @return Object, or List based on schema referencing AttributeType
-    */
-   Object get( AttributeType type );
+	/**
+	 * Access to contents of this Feature.
+	 * <p>
+	 * This represents the contents of this ComplexAttribute value, the methods
+	 * types() and values() return "views" into this data structure in a manner
+	 * similar to Maps.keySet() and values().
+	 * </p>
+	 * <p>
+	 * For a complex type, overrides {@linkplain Attribute#get()} to denote the
+	 * prescribed content type of a complex is a list of Attribute instances.
+	 * </p>
+	 * 
+	 * @see types()
+	 * @see values()
+	 */
+	List<Attribute> get();
+
+	/*
+	 * * <p> Java Bean property conventions are used to indicate that this is
+	 * part of our data model. </p> 
+	 * List<Attribute> getAttributes();
+	 */
+
+	/**
+	 * Sets the complete contents of this Attribute, that must be valid against
+	 * the type's schema descriptor.
+	 * <p>
+	 * Note this is the only way of modifying the internal structure of a
+	 * complex type.
+	 * </p>
+	 * 
+	 * @param attribute
+	 * @throws IllegalArgumentException
+	 */
+	void set(List<Attribute> newValue);
+
+	/**
+	 * List view of attribtue types, in a manner similar Map.keys().
+	 * <p>
+	 * The content available through types() an values() are considered a view
+	 * of attribtues(). Order is maintained, and removing content will result in
+	 * a modification to all three lists. in a manner simialr to Map.keysSet()
+	 * and Map.values().
+	 * <p>
+	 * Collections naming conventions are used to indicate this is a view into
+	 * our data model.
+	 */
+	List<AttributeType> types();
+
+	/**
+	 * Value view of attribtue types, in a manner similar Map.values().
+	 * <p>
+	 * The content avalable through types() an values() are considered a view of
+	 * attribtues(). Order is maintained, and removing content will result in a
+	 * modification to all three lists. in a manner simialr to Map.keysSet() and
+	 * Map.values().
+	 * <p>
+	 * Collections naming conventions are used to indicate this is a view into
+	 * our data model.
+	 */
+	List<Object> values();
+
+	/**
+	 * Access to value associated with name.
+	 * <p>
+	 * This method acts as a precanned search of getAttribtues() based on
+	 * AttributeType, where type is determined by getType().type( name )
+	 * <ul>
+	 * <li>AttributeType by Descriptor 1:1 - it will return an Object of the
+	 * bound java class indicated by AttributeType
+	 * <li>AttributeType by Descriptor 0:* - it will return a possibly empty
+	 * List of the bound java class indicated by AttributeType
+	 * </p>
+	 * <p>
+	 * This method is not considered useful for accessing content with
+	 * multiplicity
+	 * 
+	 * @param name
+	 * @return Object, or List based on type
+	 * 
+	 * Object get( String name );
+	 */
+	/**
+	 * Access to value associated with type.
+	 * 
+	 * @param type
+	 * @return Object, or List based on schema referencing AttributeType
+	 */
+	Object get(AttributeType type);
 }
