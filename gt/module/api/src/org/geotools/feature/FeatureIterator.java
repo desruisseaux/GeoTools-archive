388b20326aeefada8d084039632ec0d4a6e5e778
Cory Horner <>
2005-12-10 01:06:48 +0000

    fixed the build
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@17259 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/api/src/org/geotools/feature/FeatureIterator.java
+++ gt/module/api/src/org/geotools/feature/FeatureIterator.java
@@ -18,14 +18,29 @@ package org.geotools.feature;
 
 
 /**
- * A drop in replacement for Iterator that does not require casting for Java 1.4 code.
+ * A convenience class for dealing with FeatureCollection Iterators. DOES NOT
+ * implement Iterator.
  * <p>
  * We are sorry but this does not implement Iteartor<Feature>, although it should
  * be a drop in replacement when Geotools is able to upgrade to Java 5.
  * </p>
  * @author Ian Schneider
  */
-public interface FeatureIterator {
+public class FeatureIterator {
+    /** The iterator from the FeatureCollection to return features from. */
+     java.util.Iterator iterator;
+     FeatureCollection collection;
+    /**
+     * Create a new FeatureIterator using the Iterator from the given
+     * FeatureCollection.
+     *
+     * @param collection The FeatureCollection to perform the iteration on.
+     */
+    public FeatureIterator(FeatureCollection collection) {
+        this.collection = collection;
+        this.iterator = collection.iterator();
+    }
+
     /**
      * Does another Feature exist in this Iteration.
      * <p>
@@ -33,7 +48,9 @@ public interface FeatureIterator {
      * </p>
      * @return true if more Features exist, false otherwise.
      */
-    public boolean hasNext();
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
 
     /**
      * Get the next Feature in this iteration.
@@ -42,10 +59,17 @@ public interface FeatureIterator {
      *
      * @throws java.util.NoSuchElementException If no more Features exist.
      */
-    public Feature next() throws java.util.NoSuchElementException;
-    
+    public Feature next() throws java.util.NoSuchElementException {
+        return (Feature) iterator.next();
+    }
     /**
      * Required so FeatureCollection classes can implement close( FeatureIterator ).
      */
-    public void close();
+    public void close(){
+        if( iterator != null ){
+            collection.close( iterator );
+            iterator = null;
+            collection = null;
+        }
+    }
 }
