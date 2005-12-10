0f8fd43e399f3f17a367ac0541901d5acca48c9e
Jody Garnett <jody.garnett@gmail.com>
2005-12-10 00:20:25 +0000

    SortBy - from Filter 1.1
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@17254 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/module/api/src/org/geotools/filter/SortBy.java
@@ -0,0 +1,83 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002-2005, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.filter;
+
+/**
+ * Defines the sort order, based on a property and assending/desending.
+ * <p>
+ * Having SortBy at the Filter level is an interesting undertaking of Filter 1.1
+ * support. Why you ask? It is at the Same level as Filter, it is not *used* by
+ * Filter itself. The services that make use of Filter, such as WFS are starting
+ * to make use of SortBy in the same breath.
+ * </p>
+ * <p>
+ * Where is SortBy used:
+ * <ul>
+ * <li>WFS 1.1 Query
+ * <li>CSW 2.0.1 AbstractQuery
+ * </ul>
+ * There may be more ...
+ * </p>
+ * <p>
+ * What this means is that the GeoTools Query will make use of this
+ * construct. As for sorting the result of an expression (where an
+ * expression matches more then one element), we will splice it in to
+ * AttributeExpression as an optional parameter. Note function is defined
+ * to return a single value (so we don't need to worry there).
+ * </p> 
+ * @see http://schemas.opengis.net/filter/1.1.0/sort.xsd
+ * @see http://schemas.opengis.net/wfs/1.1.0/wfs.xsd
+ * 
+ * @since GeoTools 2.2, Filter 1.1
+ * @author Jody Garnett, Refractions Research, Inc.
+ */
+public interface SortBy {
+	
+	/**
+	 * Indicate property to sort by.
+	 * <p>
+	 * Not sure if this is allowed to be a xPath expression?
+	 * <ul>
+	 * <li>It would be consist with our use in GeoTools
+	 * <li>It would not seem to agree with the short hand notation
+	 *     used by WFS1.1 (ie. "year A, month A, day A" )
+	 * </ul>
+	 * </p>
+	 * @return Name of property to sort by.
+	 */
+	public String getPropertyName();
+	
+	/**
+	 * Indicate property.
+	 *   
+	 * @param name
+	 */
+	public void setPropertyName( String name );
+	
+	/**
+	 * The the sort order - one of ASC or DESC.
+	 * 
+	 * @return ASC or DESC.
+	 */
+	public SortOrder getSortOrderType();
+	
+	/**
+	 * Set the sort order - to ASC or DESC.
+	 *  
+	 * @param order ASC or DESC.
+	 */
+	public void setSortOrder( SortOrder order);	
+}
---
+++ gt/module/api/src/org/geotools/filter/SortOrder.java
@@ -0,0 +1,50 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002-2005, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opengis.util.CodeList;
+
+/**
+ * Captures the SortBy order, ASC or DESC.
+ * 
+ * @see http://schemas.opengis.net/filter/1.1.0/sort.xsd
+ * @author Jody Garnett, Refractions Research.
+ * @since GeoTools 2.2, Filter 1.1
+ */
+public final class SortOrder extends CodeList {
+	private static final long serialVersionUID = 7840334200112859571L;
+	private static final List all = new ArrayList();
+
+	/**
+	 * Represents acending order.
+	 */
+	public static final SortOrder ASC = new SortOrder("ASC");
+	/**
+	 * Represents descending order.
+	 */	
+	public static final SortOrder DESC= new SortOrder("DESC");
+	
+	private SortOrder( String name ){
+		super(name, all );
+	}
+			
+	public CodeList[] family() {
+		return (CodeList[]) all.toArray( new CodeList[ all.size()]);
+	}	
+}
