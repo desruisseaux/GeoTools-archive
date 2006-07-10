df883aa6e8ce15d5b61e3bcf643eafe8fa1522b0
Jody Garnett <jody.garnett@gmail.com>
2006-07-10 08:32:00 +0000

    IP check for arcgrid in review.txt
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@20429 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/arcgrid/test/org/geotools/gce/arcgrid/ArcGridHeaderTest.java
+++ gt/plugin/arcgrid/test/org/geotools/gce/arcgrid/ArcGridHeaderTest.java
@@ -1,7 +1,7 @@
 /*
- *    Geotools2 - OpenSource mapping toolkit
+ *    GeoTools - OpenSource mapping toolkit
  *    http://geotools.org
- *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *    (C) 2002-2006, GeoTools Project Managment Committee (PMC)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -12,13 +12,6 @@
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
- *
- */
-/*
- * ArcGridHeader.java
- * JUnit based test
- *
- * Created on 12 February 2002, 21:27
  */
 package org.geotools.gce.arcgrid;
 
