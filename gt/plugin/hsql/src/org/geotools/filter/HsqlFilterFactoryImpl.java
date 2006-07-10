7a9c28bc9c3ab3efe91ed0f411cf8f76e09a7c41
Jody Garnett <jody.garnett@gmail.com>
2006-07-10 11:26:11 +0000

    IP review.txt added, and headers udpated
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@20444 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/hsql/src/org/geotools/filter/HsqlFilterFactoryImpl.java
+++ gt/plugin/hsql/src/org/geotools/filter/HsqlFilterFactoryImpl.java
@@ -1,7 +1,7 @@
 /*
- *    Geotools2 - OpenSource mapping toolkit
+ *    GeoTools - OpenSource mapping toolkit
  *    http://geotools.org
- *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *    (C) 2005-2006, GeoTools Project Managment Committee (PMC)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -12,12 +12,6 @@
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
- *
- */
-/*
- * HsqlFilterFactoryImpl.java
- *
- * Created on 18 July 2005
  */
 package org.geotools.filter;
 
