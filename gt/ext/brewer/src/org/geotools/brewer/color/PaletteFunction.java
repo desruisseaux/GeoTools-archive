6cb0639914048f39e3a177e3672cb9649e143c61
Jody Garnett <jody.garnett@gmail.com>
2006-07-10 09:11:24 +0000

    IP review.txt added and headers updated
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@20440 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/ext/brewer/src/org/geotools/brewer/color/PaletteFunction.java
+++ gt/ext/brewer/src/org/geotools/brewer/color/PaletteFunction.java
@@ -1,9 +1,18 @@
 /*
- * PaletteFunction.java
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2004-2006, GeoTools Project Managment Committee (PMC)
  *
- * Created on October 27, 2004, 11:27 AM
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
-
 package org.geotools.brewer.color;
 
 //import edu.psu.geovista.colorbrewer.OriginalColor;
