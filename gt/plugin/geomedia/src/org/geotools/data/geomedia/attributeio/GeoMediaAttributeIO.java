77602b3156fc04916e8e643bdf9f6d169e87113a
Jody Garnett <jody.garnett@gmail.com>
2006-07-10 08:45:20 +0000

    IP review.txt added
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@20435 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/geomedia/src/org/geotools/data/geomedia/attributeio/GeoMediaAttributeIO.java
+++ gt/plugin/geomedia/src/org/geotools/data/geomedia/attributeio/GeoMediaAttributeIO.java
@@ -1,7 +1,7 @@
 /*
- *    Geotools2 - OpenSource mapping toolkit
+ *    GeoTools - OpenSource mapping toolkit
  *    http://geotools.org
- *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *    (C) 2003-2006, GeoTools Project Managment Committee (PMC)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -12,7 +12,6 @@
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
- *
  */
 package org.geotools.data.geomedia.attributeio;
 
