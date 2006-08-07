86264b7763c72d1a1750f184555438525e496341
Jody Garnett <jody.garnett@gmail.com>
2006-08-07 13:48:09 +0000

    review.txt added, please review
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@20883 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/ext/widgets-swing/src/org/geotools/gui/swing/image/ColorRamp.java
+++ gt/ext/widgets-swing/src/org/geotools/gui/swing/image/ColorRamp.java
@@ -1,8 +1,9 @@
 /*
- * Geotools 2 - OpenSource mapping toolkit
- * (C) 2003, Geotools Project Management Committee (PMC)
- * (C) 2000, Institut de Recherche pour le Développement
- * (C) 1999, Pêches et Océans Canada
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2003-2006, Geotools Project Managment Committee (PMC)
+ *    (C) 2000, Institut de Recherche pour le Développement
+ *    (C) 1999, Pêches et Océans Canada
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -13,10 +14,6 @@
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package org.geotools.gui.swing.image;
 
