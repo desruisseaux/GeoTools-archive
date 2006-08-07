13e0202fb3f4a44d2a50228c6f53b4bf57bfe732
Jody Garnett <jody.garnett@gmail.com>
2006-08-07 13:19:24 +0000

    ip review complete, see review.txt for details
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@20880 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/wfs/src/org/geotools/data/wfs/WFSFeatureType.java
+++ gt/plugin/wfs/src/org/geotools/data/wfs/WFSFeatureType.java
@@ -1,16 +1,17 @@
-/* uDig - User Friendly Desktop Internet GIS client
- * http://udig.refractions.net
- * (C) 2004, Refractions Research Inc.
+/*
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2004-2006, Geotools Project Managment Committee (PMC)
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation;
- * version 2.1 of the License.
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 package org.geotools.data.wfs;
 
