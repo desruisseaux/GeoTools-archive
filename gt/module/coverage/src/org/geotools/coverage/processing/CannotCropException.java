acfd9e850623f3f1349d241896e68986c185d0c9
Jody Garnett <jody.garnett@gmail.com>
2006-08-11 07:53:22 +0000

    IP review.txt updated, along with headers
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@20970 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/coverage/src/org/geotools/coverage/processing/CannotCropException.java
+++ gt/module/coverage/src/org/geotools/coverage/processing/CannotCropException.java
@@ -1,13 +1,28 @@
+/*
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2006, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package org.geotools.coverage.processing;
 
 /**
  * 
  * @author Simone Giannecchini
- * 
  */
 public class CannotCropException extends RuntimeException {
+    private static final long serialVersionUID = -4382377333378224973L;
 
-	public CannotCropException(String message) {
+    public CannotCropException(String message) {
 		super(message);
 	}
 
