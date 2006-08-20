96911bae5e70c46499db4ede9a2687a8d9a542a2
Simone Giannecchini <simboss1@gmail.com>
2006-08-20 20:01:09 +0000

    -minor fix in GeoTiffFormatFactorySpi.java and GTopo30FormatFactory.java
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@21103 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/gtopo30/src/org/geotools/gce/gtopo30/GTopo30FormatFactory.java
+++ gt/plugin/gtopo30/src/org/geotools/gce/gtopo30/GTopo30FormatFactory.java
@@ -22,55 +22,53 @@ import java.util.Map;
 import org.geotools.data.coverage.grid.GridFormatFactorySpi;
 import org.opengis.coverage.grid.Format;
 
-
 /**
  * The GTopo30FormatFactory will be discovered by the GridFormatFinder. Use the
  * standard Geotools method of discovering a factory in order to create a
  * format.
- *
+ * 
  * @author Simone Giannecchini
  * @author mkraemer
- * @source $URL$
+ * @source $URL:
+ *         http://svn.geotools.org/geotools/trunk/gt/plugin/gtopo30/src/org/geotools/gce/gtopo30/GTopo30FormatFactory.java $
  */
 public class GTopo30FormatFactory implements GridFormatFactorySpi {
-    /**
-     * Creates a new instance of GTopo30Format
-     *
-     * @return an instance of GTopo30Format
-     */
-    public Format createFormat() {
-        return new GTopo30Format();
-    }
+	/**
+	 * Creates a new instance of GTopo30Format
+	 * 
+	 * @return an instance of GTopo30Format
+	 */
+	public Format createFormat() {
+		return new GTopo30Format();
+	}
 
-    /**
-     * Checks for the JAI library which is needed by the GTopo30DataSource
-     *
-     * @return true if all libraries are available
-     */
-    public boolean isAvailable() {
-        boolean available = true;
+	/**
+	 * Checks for the JAI library which is needed by the GTopo30DataSource
+	 * 
+	 * @return true if all libraries are available
+	 */
+	public boolean isAvailable() {
+		boolean available = true;
 
-        // if these classes are here, then the runtine environment has 
-        // access to JAI and the JAI ImageI/O toolbox.
-        try {
-            Class.forName("javax.media.jai.ImageLayout");
-            Class.forName("javax.media.jai.JAI");
-            Class.forName("javax.media.jai.ParameterBlockJAI");
-            Class.forName("javax.media.jai.RenderedOp");
-            Class.forName("com.sun.media.imageio.stream.RawImageInputStream");
-        } catch (ClassNotFoundException cnf) {
-            available = false;
-        }
+		// if these classes are here, then the runtine environment has
+		// access to JAI and the JAI ImageI/O toolbox.
 
-        return available;
-    }
+		try {
+			Class.forName("javax.media.jai.JAI");
+			Class.forName("com.sun.media.jai.operator.ImageReadDescriptor");
+		} catch (ClassNotFoundException cnf) {
+			available = false;
+		}
 
-    /**
-     * Returns the implementation hints
-     *
-     * @return the implementation hints (an empty map, actually)
-     */
-    public Map getImplementationHints() {
-        return Collections.EMPTY_MAP;
-    }
+		return available;
+	}
+
+	/**
+	 * Returns the implementation hints
+	 * 
+	 * @return the implementation hints (an empty map, actually)
+	 */
+	public Map getImplementationHints() {
+		return Collections.EMPTY_MAP;
+	}
 }
