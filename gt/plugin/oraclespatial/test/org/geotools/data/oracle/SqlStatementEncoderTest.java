27bac1199d93a67c2bd1611399b46bdb7125b85b
Andrea Aime <andrea.aime@gmail.com>
2006-08-26 08:33:52 +0000

    The code changed and the unit test did not follow it apparently... fixed, hopefully it'll work too...
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@21218 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/plugin/oraclespatial/test/org/geotools/data/oracle/SqlStatementEncoderTest.java
+++ gt/plugin/oraclespatial/test/org/geotools/data/oracle/SqlStatementEncoderTest.java
@@ -19,43 +19,55 @@ import junit.framework.TestCase;
 
 import org.geotools.data.DataUtilities;
 import org.geotools.feature.FeatureType;
+import org.geotools.feature.SchemaException;
 import org.geotools.filter.SQLEncoderOracle;
 
 public class SqlStatementEncoderTest extends TestCase {
 	private SQLEncoderOracle encoder;
+
 	private SqlStatementEncoder sql;
 
 	protected void setUp() throws Exception {
-	    encoder = new SQLEncoderOracle( 4326 );
-		sql = new SqlStatementEncoder( encoder, "table", "fid" );
+		encoder = new SQLEncoderOracle(4326);
+		sql = new SqlStatementEncoder(encoder, "table", "fid");
 		super.setUp();
 	}
 
 	/*
-	 * Test method for 'org.geotools.data.oracle.SqlStatementEncoder.makeCreateTableSQL(FeatureType)'
+	 * Test method for
+	 * 'org.geotools.data.oracle.SqlStatementEncoder.makeCreateTableSQL(FeatureType)'
 	 */
 	public void testMakeCreateTableSQL() throws Exception {
-		FeatureType schema = DataUtilities.createType( "ignore", "name:String, line:MultiLineString, measure:Integer");
-		String create = sql.makeCreateTableSQL( schema );
-		String expected =
-			"CREATE TABLE table(fid NUMBER,name VARCHAR, line MDSYS.SDO_GEOMETRY, measure INTEGER);\n"+
-		    "CREATE UNIQUE INDEX table_index ON (fid );\n"+
-		    "CREATE INDEX table_sidx ON table( line) INDEXTYPE IS mdsys.spatial_index;";
-		assertEquals( expected, create );	
+		FeatureType schema = DataUtilities.createType("ignore",
+				"name:String, line:MultiLineString, measure:Integer");
+		String create = sql.makeCreateTableSQL(schema);
+		System.out.println(create);
+		String expected = "CREATE TABLE table(fid NUMBER,name VARCHAR, line MDSYS.SDO_GEOMETRY, measure INTEGER)";
+		assertEquals(expected, create);
 	}
 
 	/*
-	 * Test method for 'org.geotools.data.oracle.SqlStatementEncoder.makeType(Class)'
+	 * Test method for
+	 * 'org.geotools.data.oracle.SqlStatementEncoder.makeCreateTableSQL(FeatureType)'
 	 */
-	public void testMakeType() {
-
+	public void testMakeCreateFixIndex() throws Exception {
+		FeatureType schema = DataUtilities.createType("ignore",
+				"name:String, line:MultiLineString, measure:Integer");
+		String create = sql.makeCreateFidIndex();
+		String expected = "CREATE UNIQUE INDEX table_index ON (fid )";
+		assertEquals(expected, create);
 	}
 
 	/*
-	 * Test method for 'org.geotools.data.oracle.SqlStatementEncoder.makeCreateIndexSQL()'
+	 * Test method for
+	 * 'org.geotools.data.oracle.SqlStatementEncoder.makeCreateGeomIndex()'
 	 */
-	public void testMakeCreateIndexSQL() {
-
+	public void testMakeCreateIndexSQL() throws SchemaException {
+		FeatureType schema = DataUtilities.createType("ignore",
+				"name:String, line:MultiLineString, measure:Integer");
+		String create = sql.makeCreateGeomIndex(schema);
+		String expected = "CREATE INDEX table_sidx ON table( line) INDEXTYPE IS mdsys.spatial_index";
+		assertEquals(expected, create);
 	}
 
 }
