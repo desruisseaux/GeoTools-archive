0b59f5472ad7600d7ca190f0e3b9fe11a66e9ffe
Justin Deoliveira <jdeolive@gmail.com>
2006-08-29 04:24:12 +0000

    changed SortBy#getSortOrderType to getSortOrder
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@21247 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/feature/collection/SubFeatureList.java
+++ gt/module/main/src/org/geotools/feature/collection/SubFeatureList.java
@@ -123,7 +123,7 @@ public class SubFeatureList extends SubFeatureCollection implements FeatureList,
                         Comparable value1 = (Comparable) name.evaluate( feature1 );
                         Comparable value2 = (Comparable) name.evaluate( feature2 );
                         
-                        if( order.getSortOrderType() == SortOrder.ASCENDING ){
+                        if( order.getSortOrder() == SortOrder.ASCENDING ){
                             return value1.compareTo( value2 );
                         }
                         else return value2.compareTo( value1 );                        
