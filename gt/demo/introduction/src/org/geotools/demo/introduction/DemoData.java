ab01a0779333596cd511cef99ccf15118b0cdd80
Adrian Custer <adrian.custer@geomatys.fr>
2006-09-27 12:35:14 +0000

    Split DemoApp into Model-View-Controller
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@21656 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/demo/introduction/src/org/geotools/demo/introduction/DemoData.java
@@ -0,0 +1,122 @@
+package org.geotools.demo.introduction;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.geotools.catalog.Catalog;
+import org.geotools.catalog.GeoResource;
+import org.geotools.catalog.Service;
+import org.geotools.catalog.defaults.DefaultCatalog;
+import org.geotools.catalog.defaults.DefaultServiceFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.memory.MemoryDataStore;
+import org.geotools.data.postgis.PostgisDataStoreFactory;
+import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.wfs.WFSDataStoreFactory;
+import org.geotools.demo.mappane.MapViewer;
+import org.geotools.feature.AttributeType;
+import org.geotools.feature.AttributeTypeFactory;
+import org.geotools.feature.Feature;
+import org.geotools.feature.FeatureType;
+import org.geotools.feature.FeatureTypes;
+import org.geotools.feature.GeometryAttributeType;
+import org.geotools.feature.IllegalAttributeException;
+import org.geotools.feature.SchemaException;
+import org.geotools.styling.Graphic;
+import org.geotools.styling.Mark;
+import org.geotools.styling.SLDParser;
+import org.geotools.styling.Style;
+import org.geotools.styling.StyleBuilder;
+import org.geotools.styling.StyleFactory;
+import org.geotools.styling.StyleFactoryFinder;
+import org.geotools.styling.Symbolizer;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+
+/**
+ * A demo class illustrating some of the various parts of the geotools api.
+ * 
+ * @author Adrian Custer, gnuGIS
+ * 
+ * @version 0.03
+ * @since   2.3-M0
+ *
+ */
+public class DemoData {
+
+	
+    /**
+     * The List of Features used to store data outside the localCatalog.
+     */
+    List theFeatureList;
+//    List <Feature> theFeatureList;
+    
+    /**
+     * The List of FeatureCollections used to store data outside the localCatalog.
+     */
+    List  theFeatureCollectionList;
+//    List <FeatureCollection> theFeatureCollectionList;
+    
+	/**
+	 * The List of DataStores used to store data outside the localCatalog.
+	 */
+	List theDataStoreList;
+//    List <DataStore> theDataStoreList;
+	
+    /**
+     * The local Catalog used to store the services pointing to data in data stores.
+     */
+    Catalog localCatalog;
+    
+	/**
+	 * Creates the demo class and an underlying catalog for storing data.
+	 */
+	public DemoData() {
+        theFeatureList = new LinkedList();
+        theFeatureCollectionList = new LinkedList();
+        theDataStoreList = new LinkedList();
+		localCatalog = new DefaultCatalog();
+	}
+	
+	/**
+	 * @return The List used to store data as Features.
+	 */
+	public List getFeaturenList() {
+		return theFeatureList;
+	}
+	
+    /**
+     * @return The List used to store data in FeatureCollections.
+     */
+    public List getFeatureCollectionList() {
+        return theFeatureCollectionList;
+    }
+    
+    /**
+     * @return The List used to store data in DataStores.
+     */
+    public List getDataStoreList() {
+        return theDataStoreList;
+    }
+    
+    /**
+     * @return The catalog used to store data.
+     */
+    public Catalog getLocalCatalog() {
+        return localCatalog;
+    }
+
+
+    
+}
--- gt/demo/introduction/test/org/geotools/demo/introduction/DemoTest.java
+++ gt/demo/introduction/test/org/geotools/demo/introduction/DemoTest.java
@@ -12,50 +12,51 @@ import junit.framework.TestCase;
 
 public class DemoTest extends TestCase {
 
-	Demo demo;
-	
-	protected void setUp() throws Exception {
-		demo = new Demo();
-	}
-	
-	public void testCreateFeatureSourceFromScratch() throws Exception {
-		FeatureSource featureSource = demo.createFeatureSourceFromScratch();
-		assertNotNull( featureSource );
-		
-		assertFalse( featureSource.getFeatures().isEmpty() );
-	}
-	
-	public void testLoadShapefileIntoCatalog() throws IOException {
-		demo.loadShapefileIntoCatalog();
-		
-		Catalog catalog = demo.getCatalog();
-		assertEquals( 1, catalog.members( null ).size() );
-		
-		Service service = (Service) catalog.members( null ).get( 0 );
-		assertTrue( service.canResolve( ShapefileDataStore.class ) );
-	}
-	
-	public void testLoadShapefileFeatureSource() throws IOException {
-		demo.loadShapefileIntoCatalog();
-		
-		FeatureSource featureSource = demo.loadShapefileFeatureSource();
-		assertNotNull( featureSource );
-		
-		assertFalse( featureSource.getFeatures().isEmpty() );
-	}
-	
-	public void testCreateStyleFromScratch() throws Exception {
-		Style style = demo.createStyleFromScratch();
-		assertNotNull( style );
-	
-		assertEquals( 1, style.getFeatureTypeStyles().length );
-	}
-	
-	public void testCreateStyleFromFile() throws Exception {
-		Style style = demo.createStyleFromFile();
-		assertNotNull( style );
-		
-		assertEquals( 1, style.getFeatureTypeStyles().length ); 
-	}
+
+    DemoBase demoBase;
+    
+    protected void setUp() throws Exception {
+        demoBase = new DemoBase();
+    }
+    
+    public void testCreateFeatureSourceFromScratch() throws Exception {
+        FeatureSource featureSource = demoBase.createFeatureSourceFromScratchNext();
+        assertNotNull( featureSource );
+        
+        assertFalse( featureSource.getFeatures().isEmpty() );
+    }
+    
+    public void testLoadShapefileIntoCatalog() throws IOException {
+        demoBase.loadShapefileIntoCatalog();
+        
+        Catalog catalog = demoBase.demoData.getLocalCatalog();
+        assertEquals( 1, catalog.members( null ).size() );
+        
+        Service service = (Service) catalog.members( null ).get( 0 );
+        assertTrue( service.canResolve( ShapefileDataStore.class ) );
+    }
+    
+    public void testLoadShapefileFeatureSource() throws IOException {
+        demoBase.loadShapefileIntoCatalog();
+        
+        FeatureSource featureSource = demoBase.getFeatureSourceForShapefile();
+        assertNotNull( featureSource );
+        
+        assertFalse( featureSource.getFeatures().isEmpty() );
+    }
+    
+    public void testCreateStyleFromScratch() throws Exception {
+        Style style = demoBase.createStyleFromScratch();
+        assertNotNull( style );
+    
+        assertEquals( 1, style.getFeatureTypeStyles().length );
+    }
+    
+    public void testCreateStyleFromFile() throws Exception {
+        Style style = demoBase.createStyleFromFile();
+        assertNotNull( style );
+        
+        assertEquals( 1, style.getFeatureTypeStyles().length ); 
+    }
 	
 }
