e3fd6dca66b84459649ae2f1489476232cd07bca
Justin Deoliveira <jdeolive@gmail.com>
2006-10-06 18:05:25 +0000

    udpated to use new configuration class
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@21890 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/ext/xml-gml3/test/org/geotools/gml3/GML3TestSupport.java
+++ gt/ext/xml-gml3/test/org/geotools/gml3/GML3TestSupport.java
@@ -15,18 +15,8 @@
  */
 package org.geotools.gml3;
 
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.impl.CoordinateArraySequenceFactory;
-import org.geotools.gml3.bindings.GMLBindingConfiguration;
-import org.geotools.gml3.bindings.GMLSchemaLocationResolver;
-import org.geotools.gml3.bindings.smil.SMIL20BindingConfiguration;
-import org.geotools.gml3.bindings.smil.SMIL20SchemaLocationResolver;
-import org.geotools.xlink.bindings.XLINKBindingConfiguration;
-import org.geotools.xlink.bindings.XLINKSchemaLocationResolver;
 import org.geotools.xml.Configuration;
 import org.geotools.xml.test.XMLTestSupport;
-import org.geotools.xs.bindings.XSBindingConfiguration;
-import org.picocontainer.MutablePicoContainer;
 import org.w3c.dom.Element;
 
 
@@ -42,22 +32,6 @@ public abstract class GML3TestSupport extends XMLTestSupport {
     }
 
     protected Configuration createConfiguration() {
-        return new Configuration() {
-                public void configureBindings(MutablePicoContainer container) {
-                    new XSBindingConfiguration().configure(container);
-                    new XLINKBindingConfiguration().configure(container);
-                    new SMIL20BindingConfiguration().configure(container);
-                    new GMLBindingConfiguration().configure(container);
-                }
-
-                public void configureContext(MutablePicoContainer container) {
-                    container.registerComponentImplementation(XLINKSchemaLocationResolver.class);
-                    container.registerComponentImplementation(SMIL20SchemaLocationResolver.class);
-                    container.registerComponentImplementation(GMLSchemaLocationResolver.class);
-                    container.registerComponentInstance(CoordinateArraySequenceFactory
-                        .instance());
-                    container.registerComponentImplementation(GeometryFactory.class);
-                }
-            };
+        return new GMLConfiguration();
     }
 }
