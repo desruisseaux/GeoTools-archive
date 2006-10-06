3e064e1ef45a9f2eeca5e311cbb5570b3b9c6ea8
Simone Giannecchini <simboss1@gmail.com>
2006-10-06 12:42:31 +0000

    -minor improvements to AbstractGridCoverageWriter.java
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@21872 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/module/main/src/org/geotools/data/coverage/grid/AbstractGridCoverageWriter.java
+++ gt/module/main/src/org/geotools/data/coverage/grid/AbstractGridCoverageWriter.java
@@ -15,107 +15,30 @@
  */
 package org.geotools.data.coverage.grid;
 
-import java.io.IOException;
-
 import javax.imageio.stream.ImageOutputStream;
 
 import org.geotools.factory.Hints;
-import org.opengis.coverage.MetadataNameNotFoundException;
-import org.opengis.coverage.grid.Format;
-import org.opengis.coverage.grid.GridCoverage;
 import org.opengis.coverage.grid.GridCoverageWriter;
-import org.opengis.parameter.GeneralParameterValue;
 
 /**
+ * 
+ * 
  * @author Simone Giannecchini
+ * @since 2.3.x
  * 
  */
 public abstract class AbstractGridCoverageWriter implements GridCoverageWriter {
 
 	/** Hints to be used for the writing process. */
 	protected Hints hints = null;
-	
-	/**The destination {@link ImageOutputStream}.*/
-	protected ImageOutputStream outStream=null;
+
+	/** The destination {@link ImageOutputStream}. */
+	protected ImageOutputStream outStream = null;
 
 	/**
-	 * 
+	 * Default constructor for an {@link AbstractGridCoverageWriter}.
 	 */
 	public AbstractGridCoverageWriter() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opengis.coverage.grid.GridCoverageWriter#dispose()
-	 */
-	public void dispose() throws IOException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opengis.coverage.grid.GridCoverageWriter#getDestination()
-	 */
-	public Object getDestination() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opengis.coverage.grid.GridCoverageWriter#getFormat()
-	 */
-	public Format getFormat() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opengis.coverage.grid.GridCoverageWriter#getMetadataNames()
-	 */
-	public String[] getMetadataNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opengis.coverage.grid.GridCoverageWriter#setCurrentSubname(java.lang.String)
-	 */
-	public void setCurrentSubname(String arg0) throws IOException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opengis.coverage.grid.GridCoverageWriter#setMetadataValue(java.lang.String,
-	 *      java.lang.String)
-	 */
-	public void setMetadataValue(String arg0, String arg1) throws IOException,
-			MetadataNameNotFoundException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opengis.coverage.grid.GridCoverageWriter#write(org.opengis.coverage.grid.GridCoverage,
-	 *      org.opengis.parameter.GeneralParameterValue[])
-	 */
-	public void write(GridCoverage arg0, GeneralParameterValue[] arg1)
-			throws IllegalArgumentException, IOException {
-		// TODO Auto-generated method stub
 
 	}
 
