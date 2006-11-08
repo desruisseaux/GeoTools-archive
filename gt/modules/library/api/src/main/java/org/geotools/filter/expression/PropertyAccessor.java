2a31f741a524c1b2373b9205d70329bb55c0381e
Justin Deoliveira <jdeolive@gmail.com>
2006-11-08 18:05:31 +0000

    added property accessor factory interfaces
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@22650 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/modules/library/api/src/main/java/org/geotools/filter/expression/PropertyAccessor.java
@@ -0,0 +1,66 @@
+/*
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002-2006, GeoTools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.filter.expression;
+
+import org.geotools.feature.IllegalAttributeException;
+
+
+/**
+ * Used to get and set object properties based on an xpath expression.
+ *
+ * @author Justin Deoliveira, The Open Planning Project
+ *
+ */
+public interface PropertyAccessor {
+    /**
+     * Determines if the property accessor can handle the property denoted by <param>xpath</param>.
+     *
+     * @param object The target object.
+     * @param xpath An xpath expression denoting a property of the target object.
+     *
+     * @return <code>true</code> if the property can be accessed, otherwise <code>false</code>
+     */
+    boolean canHandle(Object object, String xpath);
+
+    /**
+     * Accesses a property of <param>object</param> via xpath expression.
+     * <p>
+     * {@link #canHandle(Object, String)} should be called before calling this method to ensure
+     * that the property can be safely accessed.
+     * </p>
+     * @param object The target object.
+     * @param xpath An xpath expression denoting a property of the target object.
+     *
+     * @return The property, which might be <code>null</code>
+     *
+     */
+    Object get(Object object, String xpath);
+
+    /**
+     * Sets a property of <param>object</param> via xpath expression.
+     * <p>
+     * {@link #canHandle(Object, String)} should be called before calling this method to ensure
+     * that the property can be safely accessed.
+     * </p>
+     * @param object The target object.
+     * @param xpath An xpath expression denoting a property of the target object.
+     * @param value The new value to set.
+     *
+     * @throws IllegalAttributeException If the value set is not legal for the target object.
+     */
+    void set(Object object, String xpath, Object value)
+        throws IllegalAttributeException;
+}
