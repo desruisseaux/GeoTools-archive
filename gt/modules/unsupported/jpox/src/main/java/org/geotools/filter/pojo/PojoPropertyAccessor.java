e1fa9dcd8920e998e4248e2ae76a188966b3c147
tmarti <>
2006-11-24 23:52:47 +0000

    Simple reflection implementation.
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@22988 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/jpox/src/main/java/org/geotools/filter/pojo/PojoPropertyAccessor.java
+++ gt/modules/unsupported/jpox/src/main/java/org/geotools/filter/pojo/PojoPropertyAccessor.java
@@ -1,6 +1,12 @@
 package org.geotools.filter.pojo;
 
-import java.util.Date;
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.geotools.feature.IllegalAttributeException;
 import org.geotools.filter.expression.PropertyAccessor;
@@ -8,11 +14,49 @@ import org.geotools.filter.expression.PropertyAccessor;
 public class PojoPropertyAccessor implements PropertyAccessor {
 
     public boolean canHandle( Object object, String xpath ) {
-        return ( object instanceof Date );
+    	// We can handle everything! ...yeah, right.. ;)
+        return true;
     }
 
     public Object get( Object object, String xpath ) {
-        // TODO Auto-generated method stub
+    	if ( object == null || xpath == null  ) return null;
+    	xpath = xpath.trim();
+    	if ( xpath.equals( "" ) ) return null;
+    	
+    	try {
+			BeanInfo info = Introspector.getBeanInfo( object.getClass() );
+			
+			PropertyDescriptor[] descriptors = info.getPropertyDescriptors();
+			for ( int i = 0; i < descriptors.length; i++ ) {
+				
+				if ( descriptors[i].getName().equalsIgnoreCase( xpath ) ) {
+					Method getter = descriptors[i].getReadMethod();
+					if ( getter != null ) {
+						getter.setAccessible( true );
+						Object value = getter.invoke( object, null );
+						return value;
+					} else {
+						Field field = object.getClass().getDeclaredField( xpath );
+						if ( field != null ) {
+							field.setAccessible( true );
+							return field.get( object );
+						}
+					}
+				}
+			}			
+		} catch ( IntrospectionException e ) {
+			e.printStackTrace();
+		} catch ( IllegalArgumentException e ) {
+			e.printStackTrace();
+		} catch ( IllegalAccessException e ) {
+			e.printStackTrace();
+		} catch ( InvocationTargetException e ) {
+			e.printStackTrace();
+		} catch ( SecurityException e ) {
+			e.printStackTrace();
+		} catch ( NoSuchFieldException e ) {
+			e.printStackTrace();
+		}
         return null;
     }
 
