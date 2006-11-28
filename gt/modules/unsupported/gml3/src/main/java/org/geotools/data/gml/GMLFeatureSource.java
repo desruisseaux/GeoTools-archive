769d3e5675f8d9df9c02606b49a2644f0076b61e
Justin Deoliveira <jdeolive@gmail.com>
2006-11-28 21:48:25 +0000

    reprojecting feature collections using curve transformer
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23094 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/gml3/src/main/java/org/geotools/data/gml/GMLFeatureSource.java
+++ gt/modules/unsupported/gml3/src/main/java/org/geotools/data/gml/GMLFeatureSource.java
@@ -3,7 +3,9 @@ package org.geotools.data.gml;
 import java.io.IOException;
 
 import org.geotools.data.store.AbstractFeatureSource2;
+import org.geotools.data.store.ReprojectingFeatureCollection;
 import org.geotools.feature.FeatureCollection;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 public class GMLFeatureSource extends AbstractFeatureSource2 {
 
@@ -15,7 +17,11 @@ public class GMLFeatureSource extends AbstractFeatureSource2 {
 		return new GMLFeatureCollection( (GMLTypeEntry) entry );
 	}
 
-	
-	
+	protected FeatureCollection reproject(FeatureCollection features, CoordinateReferenceSystem source, CoordinateReferenceSystem target) {
+		ReprojectingFeatureCollection reprojected = 
+			(ReprojectingFeatureCollection) super.reproject( features, source, target );
+		reprojected.setTransformer( new GMLGeometryCoordinateSequenceTransformer() );
+		return reprojected;
+	}
 	
 }
