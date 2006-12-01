ae41bce0d6e65ef94ed4b29a1c3f5bee0e2540bd
Justin Deoliveira <jdeolive@gmail.com>
2006-12-01 21:07:52 +0000

    case insensitive test
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23179 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/library/main/src/test/java/org/geotools/filter/IsEqualsToImplTest.java
+++ gt/modules/library/main/src/test/java/org/geotools/filter/IsEqualsToImplTest.java
@@ -20,9 +20,22 @@ public class IsEqualsToImplTest extends TestCase {
 	
 	public void testOperandsDifferentType() {
 		Expression e1 = filterFactory.literal( 1 );
-		Expression e2 = filterFactory.literal( "1" );
+		Expression e2 = filterFactory.literal( "1"
+				);
 		
 		PropertyIsEqualTo equal = filterFactory.equals( e1, e2 );
 		assertTrue( equal.evaluate( null ) );
+	}
+	
+	public void testCaseSensitivity() {
+		Expression e1 = filterFactory.literal( "foo" );
+		Expression e2 = filterFactory.literal( "FoO" );
+		
+		PropertyIsEqualTo caseSensitive = filterFactory.equal( e1, e2, true );
+		assertFalse( caseSensitive. evaluate( null ) );
+		
+		PropertyIsEqualTo caseInsensitive = filterFactory.equal( e1, e2, false );
+		assertTrue( caseInsensitive. evaluate( null ) );
+		
 	}
 }
