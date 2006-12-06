de7add8b803fde7774469300c5c5a246fb96f52a
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2006-12-06 20:28:31 +0000

    Enabled the 'testCenterTile()' test.
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23264 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/library/render/src/test/java/org/geotools/renderer/lite/RenderUtilitiesTest.java
+++ gt/modules/library/render/src/test/java/org/geotools/renderer/lite/RenderUtilitiesTest.java
@@ -6,6 +6,7 @@ import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.referencing.CRS;
 import org.geotools.referencing.crs.DefaultEngineeringCRS;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.geotools.resources.XMath;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import com.vividsolutions.jts.geom.Envelope;
@@ -51,10 +52,13 @@ public class RenderUtilitiesTest extends TestCase {
      * The following test is from the tile module where the behavior
      * of RenderUtilities changed between 2.2. and 2.4.
      */
-    public void XtestCenterTile() throws Exception {
+    public void testCenterTile() throws Exception {
         Envelope centerTile = new Envelope( 0, 36, -18, 18 );
         CoordinateReferenceSystem crs = DefaultGeographicCRS.WGS84;
         double scale = RendererUtilities.calculateScale(centerTile, crs, 512, 512, 72.0 );
-        assertEquals(1.0, scale, 0.00001); // no projection deformation here!
+        double groundDistance = XMath.hypot( 36,  36) * (1852   * 60);
+        double pixelDistance  = XMath.hypot(512, 512) * (0.0254 / 72);
+        double expected = groundDistance / pixelDistance;
+        assertEquals(expected, scale, expected * 0.05); // no projection deformation here!
     }
 }
