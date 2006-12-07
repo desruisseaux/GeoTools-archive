94cfe5328e60074d96babcb7a8fa2f4f068295b2
Justin Deoliveira <jdeolive@gmail.com>
2006-12-07 20:19:15 +0000

    removing special transformer for curves
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23321 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/gml3/src/main/java/org/geotools/data/gml/GMLFeatureSource.java
+++ gt/modules/unsupported/gml3/src/main/java/org/geotools/data/gml/GMLFeatureSource.java
@@ -3,9 +3,7 @@ package org.geotools.data.gml;
 import java.io.IOException;
 
 import org.geotools.data.store.AbstractFeatureSource2;
-import org.geotools.data.store.ReprojectingFeatureCollection;
 import org.geotools.feature.FeatureCollection;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 public class GMLFeatureSource extends AbstractFeatureSource2 {
 
@@ -16,12 +14,4 @@ public class GMLFeatureSource extends AbstractFeatureSource2 {
 	public FeatureCollection getFeatures() throws IOException {
 		return new GMLFeatureCollection( (GMLTypeEntry) entry );
 	}
-
-	protected FeatureCollection reproject(FeatureCollection features, CoordinateReferenceSystem source, CoordinateReferenceSystem target) {
-		ReprojectingFeatureCollection reprojected = 
-			(ReprojectingFeatureCollection) super.reproject( features, source, target );
-		reprojected.setTransformer( new GMLGeometryCoordinateSequenceTransformer() );
-		return reprojected;
-	}
-	
 }
