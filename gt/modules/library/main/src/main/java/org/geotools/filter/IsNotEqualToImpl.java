d1ddd310b20964cbdc6797ef67fb1738ab2e953d
Justin Deoliveira <jdeolive@gmail.com>
2006-12-07 08:44:58 +0000

    added case insensitivitey
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23297 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/library/main/src/main/java/org/geotools/filter/IsNotEqualToImpl.java
+++ gt/modules/library/main/src/main/java/org/geotools/filter/IsNotEqualToImpl.java
@@ -16,6 +16,7 @@
 package org.geotools.filter;
 
 import org.geotools.feature.Feature;
+import org.geotools.filter.expression.Value;
 import org.opengis.filter.FilterVisitor;
 import org.opengis.filter.PropertyIsNotEqualTo;
 import org.opengis.filter.expression.Expression;
@@ -28,7 +29,11 @@ public class IsNotEqualToImpl extends CompareFilterImpl
 	}
 	
 	protected IsNotEqualToImpl(FilterFactory factory, Expression e1, Expression e2) {
-		super(factory, e1, e2);
+		this( factory, e1, e2, true );
+	}
+	
+	protected IsNotEqualToImpl(FilterFactory factory, Expression expression1, Expression expression2, boolean matchCase ) {
+		super(factory, expression1, expression2, matchCase);
 		
 		//backwards compat with old type system
 		this.filterType = COMPARE_NOT_EQUALS;
@@ -40,9 +45,24 @@ public class IsNotEqualToImpl extends CompareFilterImpl
 		Object value1 = values[ 0 ];
 		Object value2 = values[ 1 ];
 		
-		return (value1 == null && value2 != null) ||
+		boolean notEqual = (value1 == null && value2 != null) ||
 			(value1 != null && value2 == null) ||
 		    (value1 != null && !value1.equals( value2 ));
+		
+		if ( !notEqual ) {
+			return false;
+		}
+		
+		//not equal, case insensitive?
+		if ( !isMatchingCase() && value1 != null && value2 != null ) {
+			//fall back to string and check the case insensitive flag
+			String s1 = (String) new Value( value1 ).value( String.class );
+			String s2 = (String) new Value( value2 ).value( String.class );
+			
+			return !s1.equalsIgnoreCase( s2 );
+		}
+		
+		return true;
 	}
 	
 	public Object accept(FilterVisitor visitor, Object extraData) {
---
+++ gt/modules/library/main/src/test/java/org/geotools/filter/IsNotEqualToImpltest.java
@@ -0,0 +1,42 @@
+package org.geotools.filter;
+
+import org.geotools.factory.CommonFactoryFinder;
+import org.opengis.filter.PropertyIsEqualTo;
+import org.opengis.filter.PropertyIsNotEqualTo;
+import org.opengis.filter.expression.Expression;
+
+import junit.framework.TestCase;
+
+public class IsNotEqualToImpltest extends TestCase {
+
+	org.opengis.filter.FilterFactory filterFactory = CommonFactoryFinder.getFilterFactory( null );
+	
+	public void testOperandsSameType() {
+		Expression e1 = filterFactory.literal( 1 );
+		Expression e2 = filterFactory.literal( 2 );
+		
+		PropertyIsNotEqualTo notEqual = filterFactory.notEqual( e1, e2, true );
+		assertTrue( notEqual.evaluate( null ) );
+	}
+	
+	public void testOperandsDifferentType() {
+		Expression e1 = filterFactory.literal( 1 );
+		Expression e2 = filterFactory.literal( "2"
+				);
+		
+		PropertyIsNotEqualTo notEqual = filterFactory.notEqual( e1, e2, true );
+		assertTrue( notEqual.evaluate( null ) );
+	}
+	
+	public void testCaseSensitivity() {
+		Expression e1 = filterFactory.literal( "foo" );
+		Expression e2 = filterFactory.literal( "FoO" );
+		
+		PropertyIsNotEqualTo caseSensitive = filterFactory.notEqual( e1, e2, true );
+		assertTrue( caseSensitive. evaluate( null ) );
+		
+		PropertyIsNotEqualTo caseInsensitive = filterFactory.notEqual( e1, e2, false );
+		assertFalse( caseInsensitive. evaluate( null ) );
+		
+	}
+}
