ce157cfdd4d85dbcfa684303454c296cc229ba66
Justin Deoliveira <jdeolive@gmail.com>
2006-12-18 23:08:24 +0000

    handling of sql date types
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23504 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/library/main/src/main/java/org/geotools/util/TemporalConverterFactory.java
+++ gt/modules/library/main/src/main/java/org/geotools/util/TemporalConverterFactory.java
@@ -1,5 +1,8 @@
 package org.geotools.util;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,17 +14,46 @@ import org.geotools.factory.Hints;
  * Supported converstions:
  * <ul>
  * 	<li>{@link java.util.Date} to {@link Calendar}
+ *  <li>{@link java.sql.Timestamp} to {@link java.util.Calendar} 
+ *  <li>{@link java.sql.Time} to {@link java.util.Calendar}
+ *  <li>{@link java.util.Date} to {@link java.sql.Timestamp}
+ *  <li>{@link java.util.Date} to {@link java.sql.Time}
  *  <li>{@link java.util.Calendar} to {@link java.util.Date}
- * 	
+ *  <li>{@link java.util.Calendar} to {@link java.sql.Timestamp}
+ *  <li>{@link java.util.Calendar} to {@link java.sql.Time}
  * </ul>
+ * </p>
+ * <p>
+ * The hint {@link #DATE_FORMAT} can be used to control the format of converting a temporal value
+ * to a String.
  * </p>
  * @author Justin Deoliveira, The Open Planning Project
  *
  */
 public class TemporalConverterFactory implements ConverterFactory {
 
+//	/**
+//	 * Key to specify the format when converting from a Date value to a String.
+//	 */
+//	public static Hints.Key DATE_FORMAT= new Hints.Key( DateFormat.class );
+//	/**
+//	 * Key to specify the format when converting from a Caledar value to a String.
+//	 */
+//	public static Hints.Key DATETIME_FORMAT= new Hints.Key( DateFormat.class );
+	
+	
 	public Converter createConverter(Class source, Class target, Hints hints) {
+		//look for date format hint
+//		DateFormat dateFormat = null;
+//		DateFormat dateTimeFormat = null;
+//		if ( hints != null ) {
+//			dateFormat = (DateFormat) hints.get( DATE_FORMAT );
+//			dateTimeFormat = (DateFormat) hints.get( DATETIME_FORMAT );
+//		}
+		
+		
 		if ( Date.class.isAssignableFrom( source ) ) {
+			// handle all of (java.util.Date,java.sql.Timestamp,and java.sql.Time) -> java.util.Calendar 
 			if ( Calendar.class.isAssignableFrom( target ) ) {
 				return new Converter() {
 					public Object convert(Object source, Class target) throws Exception {
@@ -32,17 +64,68 @@ public class TemporalConverterFactory implements ConverterFactory {
 					}
 				};
 			}
+			
+			//handle all of (java.util.Date) -> (java.sql.Timestamp,java.sql.Time)
+			if ( Timestamp.class.isAssignableFrom( target ) || Time.class.isAssignableFrom( target ) ) {
+				return new Converter() {
+
+					public Object convert(Object source, Class target) throws Exception {
+						Date date = (Date) source;
+						return target.getConstructor( new Class[] { Long.TYPE } )
+							.newInstance( new Object[]{ new Long( date.getTime() ) } );
+					}
+					
+				};
+			}
+//			if ( String.class.equals( target ) ) {
+//				final DateFormat fFormat;
+//				if ( dateFormat != null ) {
+//					fFormat = dateFormat;
+//				}
+//				else {
+//					//create a default
+//					fFormat = DateFormat.getDateInstance();
+//				}
+//				
+//				return new Converter() {
+//					public Object convert(Object source, Class target) throws Exception {
+//						return fFormat.format( (Date)source );
+//					}
+//				};
+//			}
 		}
 		
+		//this should handle java.util.Calendar to (java.util.Date,java.sql.Timestamp,java.util.Time}
 		if ( Calendar.class.isAssignableFrom( source ) ) {
 			if ( Date.class.isAssignableFrom( target ) ) {
+				final Class fTarget = target;
 				return new Converter() {
 					public Object convert(Object source, Class target) throws Exception {
 						Calendar calendar = (Calendar) source;
-						return calendar.getTime();
+						
+						return target.getConstructor( new Class[] { Long.TYPE } ).newInstance( 
+							new Object[]{ new Long( calendar.getTimeInMillis() ) }
+						);
 					}
 				};
 			}
+//			if ( String.class.equals( target ) ) {
+//				final DateFormat fFormat;
+//				if ( dateTimeFormat != null ) {
+//					fFormat = dateTimeFormat;
+//				}
+//				else {
+//					//create a default
+//					fFormat = DateFormat.getDateTimeInstance();
+//				}
+//				
+//				return new Converter() {
+//					public Object convert(Object source, Class target) throws Exception {
+//						Date date = ((Calendar)source).getTime();
+//						return fFormat.format( date );
+//					}
+//				};
+//			}
 		}
 		
 		return null;
--- gt/modules/library/main/src/test/java/org/geotools/util/TemporalConverterFactoryTest.java
+++ gt/modules/library/main/src/test/java/org/geotools/util/TemporalConverterFactoryTest.java
@@ -1,7 +1,14 @@
 package org.geotools.util;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
+
+import org.geotools.factory.Hints;
 
 import junit.framework.TestCase;
 
@@ -13,7 +20,7 @@ public class TemporalConverterFactoryTest extends TestCase {
 		factory = new TemporalConverterFactory();
 	}
 	
-	public void testToDate() throws Exception {
+	public void testCalendarToDate() throws Exception {
 		Calendar calendar = Calendar.getInstance();
 		assertNotNull( factory.createConverter( Calendar.class, Date.class, null ) );
 		
@@ -23,7 +30,27 @@ public class TemporalConverterFactoryTest extends TestCase {
 		assertEquals( calendar.getTime(), date );
 	}
 	
-	public void testToCalendar() throws Exception {
+	public void testCalendarToTime() throws Exception {
+		Calendar calendar = Calendar.getInstance();
+		assertNotNull( factory.createConverter( Calendar.class, Time.class, null ) );
+		
+		Time time = (Time) factory.createConverter( Calendar.class, Time.class, null )
+			.convert( calendar, Time.class );
+		assertNotNull( time );
+		assertEquals( new Time( calendar.getTime().getTime() ), time );
+	}
+	
+	public void testCalendarToTimestamp() throws Exception {
+		Calendar calendar = Calendar.getInstance();
+		assertNotNull( factory.createConverter( Calendar.class, Timestamp.class, null ) );
+		
+		Timestamp timeStamp = (Timestamp) factory.createConverter( Calendar.class, Timestamp.class, null )
+			.convert( calendar, Timestamp.class );
+		assertNotNull( timeStamp );
+		assertEquals( new Timestamp( calendar.getTime().getTime() ), timeStamp );
+	}
+	
+	public void testDateToCalendar() throws Exception {
 		Date date = new Date();
 		assertNotNull( factory.createConverter(  Date.class, Calendar.class, null ) );
 		
@@ -32,4 +59,47 @@ public class TemporalConverterFactoryTest extends TestCase {
 		assertNotNull( calendar );
 		assertEquals( date, calendar.getTime() );
 	}
+	
+	public void testDateToTime() throws Exception {
+		Date date = new Date();
+		assertNotNull( factory.createConverter(  Date.class, Time.class, null ) );
+		
+		Time time = (Time) factory.createConverter( Date.class, Time.class, null )
+			.convert( date, Time.class );
+		assertNotNull( time );
+		assertEquals( new Time( date.getTime() ), time );
+	}
+	
+	public void testDateToTimestamp() throws Exception {
+		Date date = new Date();
+		assertNotNull( factory.createConverter(  Date.class, Timestamp.class, null ) );
+		
+		Timestamp timeStamp = (Timestamp) factory.createConverter( Date.class, Timestamp.class, null )
+			.convert( date, Timestamp.class );
+		assertNotNull( timeStamp );
+		assertEquals( new Timestamp( date.getTime() ), timeStamp );
+	}
+	
+	public void testTimeToCalendar() throws Exception {
+		Time time = new Time(new Date().getTime());
+		assertNotNull( factory.createConverter(  Time.class, Calendar.class, null ) );
+		
+		Calendar calendar = (Calendar) factory.createConverter( Time.class, Calendar.class, null )
+			.convert( time, Calendar.class );
+		assertNotNull( calendar );
+		assertEquals( time, new Time( calendar.getTime().getTime() ) );
+	}
+	
+	public void testTimestampToCalendar() throws Exception {
+		Timestamp timeStamp = new Timestamp( new Date().getTime() );
+		assertNotNull( factory.createConverter(  Timestamp.class, Calendar.class, null ) );
+		
+		Calendar calendar = (Calendar) factory.createConverter( Timestamp.class, Calendar.class, null )
+			.convert( timeStamp, Calendar.class );
+		assertNotNull( calendar );
+		assertEquals( timeStamp, new Timestamp( calendar.getTime().getTime() ) );
+	}
+	
+	
+	
 }
