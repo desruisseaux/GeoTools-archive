414a0d78833ff4cb33b158b372d0650ed8bd807f
Justin Deoliveira <jdeolive@gmail.com>
2006-12-20 06:44:17 +0000

    moved from gml3 module
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23547 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/modules/unsupported/xml-gml3/src/main/java/org/geotools/gml3/ApplicationSchemaConfiguration.java
@@ -0,0 +1,61 @@
+/*
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002-2006, GeoTools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.gml3;
+
+import org.eclipse.xsd.util.XSDSchemaLocationResolver;
+import org.geotools.xml.BindingConfiguration;
+import org.geotools.xml.Configuration;
+import org.geotools.xml.SchemaLocationResolver;
+
+
+/**
+ * An xml configuration for application schemas.
+ * <p>
+ *
+ * </p>
+ * @author Justin Deoliveira, The Open Planning Project
+ *
+ */
+public class ApplicationSchemaConfiguration extends Configuration {
+    /** application schema namespace */
+    private String namespace;
+
+    /** location of the application schema itself */
+    private String schemaLocation;
+
+    public ApplicationSchemaConfiguration(String namespace, String schemaLocation) {
+        this.namespace = namespace;
+        this.schemaLocation = schemaLocation;
+
+        addDependency(new GMLConfiguration());
+    }
+
+    public String getNamespaceURI() {
+        return namespace;
+    }
+
+    public String getSchemaFileURL() {
+        return schemaLocation;
+    }
+
+    public BindingConfiguration getBindingConfiguration() {
+        return null;
+    }
+
+    public XSDSchemaLocationResolver getSchemaLocationResolver() {
+        return new SchemaLocationResolver(namespace, schemaLocation);
+    }
+}
