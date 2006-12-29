08e99eb7e0f6aa20f97e8b1fb02d6c27072da797
Andrea Aime <andrea.aime@gmail.com>
2006-12-29 20:04:13 +0000

    Chech the id set really contains Identifiers, not something else
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23631 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/library/main/src/main/java/org/geotools/filter/FidFilterImpl.java
+++ gt/modules/library/main/src/main/java/org/geotools/filter/FidFilterImpl.java
@@ -15,7 +15,6 @@
  */
 package org.geotools.filter;
 
-
 // Geotools dependencies
 import java.util.Collection;
 import java.util.HashSet;
@@ -27,21 +26,21 @@ import org.geotools.feature.Feature;
 import org.opengis.filter.FilterVisitor;
 import org.opengis.filter.identity.Identifier;
 
-
 /**
- * Defines a ID filter, which holds a list of IDs ( usually feature id;s ). This filter
- * stores a series of IDs, which are used to distinguish features uniquely.
+ * Defines a ID filter, which holds a list of IDs ( usually feature id;s ). This filter stores a
+ * series of IDs, which are used to distinguish features uniquely.
  * <p>
- * Please note that addAllFids( Collection ) may be a performance hog; uDig makes
- * use of its own implementation of FidFilter in order to reuse the internal set
- * of fids between uses.
+ * Please note that addAllFids( Collection ) may be a performance hog; uDig makes use of its own
+ * implementation of FidFilter in order to reuse the internal set of fids between uses.
  * </p>
+ * 
  * @author Rob Hranac, TOPP
  * @author Justin Deoliveira, TOPP
  * 
  * TODO: this class shoul be renamed to IdFilterImpl
  * 
- * @source $URL$
+ * @source $URL:
+ *         http://svn.geotools.org/geotools/trunk/gt/modules/library/main/src/main/java/org/geotools/filter/FidFilterImpl.java $
  * @version $Id$
  */
 public class FidFilterImpl extends AbstractFilterImpl implements FidFilter {
@@ -50,42 +49,50 @@ public class FidFilterImpl extends AbstractFilterImpl implements FidFilter {
 
     /** List of the Identifer. */
     private Set fids = new HashSet();
-    
+
     /**
      * Empty constructor.
+     * 
      * @deprecated use {@link #FidFilterImpl(Set)}
      */
     protected FidFilterImpl() {
-    	super(FilterFactoryFinder.createFilterFactory());
-    	filterType = AbstractFilter.FID;
+        super(FilterFactoryFinder.createFilterFactory());
+        filterType = AbstractFilter.FID;
     }
 
     /**
      * Constructor with first fid set
-     *
-     * @param initialFid The type of comparison.
+     * 
+     * @param initialFid
+     *            The type of comparison.
      * @deprecated use {@link #FidFilterImpl(Set)}
      */
     protected FidFilterImpl(String initialFid) {
-    	super(FilterFactoryFinder.createFilterFactory());
-    	filterType = AbstractFilter.FID;
+        super(FilterFactoryFinder.createFilterFactory());
+        filterType = AbstractFilter.FID;
         addFid(initialFid);
     }
 
     /**
-     * Constructor which takes {@link org.opengis.filter.identity.Identifier}, 
-     * not String.
-     *
+     * Constructor which takes {@link org.opengis.filter.identity.Identifier}, not String.
+     * 
      */
-    protected FidFilterImpl( Set/*<Identiger>*/ fids ) {
-    	super( FilterFactoryFinder.createFilterFactory() );
-    	filterType = AbstractFilter.FID;
-    	this.fids = fids;
+    protected FidFilterImpl(Set/* <Identiger> */fids) {
+        super(FilterFactoryFinder.createFilterFactory());
+        filterType = AbstractFilter.FID;
+        // check these are really identifiers
+        for (Iterator it = fids.iterator(); it.hasNext();) {
+            Object next = it.next();
+            if(!(next instanceof Identifier))
+                throw new ClassCastException("Fids must implement Identifier, " 
+                        + next.getClass() + " does not");
+        }
+        this.fids = fids;
     }
-    
+
     /**
      * Returns all the fids in this filter.
-     *
+     * 
      * @return An array of all the fids in this filter.
      * 
      * @deprecated use {@link #getIDs()}
@@ -98,114 +105,120 @@ public class FidFilterImpl extends AbstractFilterImpl implements FidFilter {
      * @see org.opengis.filter.Id#getIDs()
      */
     public Set getIDs() {
-    	return getFidsSet();
+        return getFidsSet();
     }
-    
+
     /**
      * @see org.opengis.filter.Id#getIdentifiers()
      */
     public Set getIdentifiers() {
-    	return fids;
+        return fids;
     }
-    
+
     /**
      * @see org.opengis.filter.identity.FeatureId#setIDs(Set)
      */
     public void setIDs(Set ids) {
-    	fids = new HashSet();
-    	addAllFids( ids );
+        fids = new HashSet();
+        addAllFids(ids);
     }
-   
+
     /**
      * Accessor method for fid set as Strings.
-     *
+     * 
      * @return the internally stored fids.
      */
     public Set getFidsSet() {
         return fids();
     }
-    
+
     /**
      * Helper method to pull out strings from featureId set.
+     * 
      * @return
      */
     private Set fids() {
-    	HashSet set = new HashSet();
-    	for ( Iterator i = fids.iterator(); i.hasNext(); ) {
-    		Identifier id = (Identifier) i.next();
-    		set.add( id.toString() );
-    	}
-    	
-    	return set;
+        HashSet set = new HashSet();
+        for (Iterator i = fids.iterator(); i.hasNext();) {
+            Identifier id = (Identifier) i.next();
+            set.add(id.toString());
+        }
+
+        return set;
     }
-    
+
     /**
      * Adds a feature ID to the filter.
-     *
-     * @param fid A single feature ID.
+     * 
+     * @param fid
+     *            A single feature ID.
      * @deprecated
      */
     public final void addFid(String fid) {
-    	LOGGER.finest("got fid: " + fid);
-    	fids.add( factory.featureId( fid ) );
+        LOGGER.finest("got fid: " + fid);
+        fids.add(factory.featureId(fid));
     }
 
-    /** 
-	 * Adds a collection of feature IDs to the filter. 
-	 * 
-	 * @param fidsToAdd A collection of feature IDs as strings.
-	 */ 
-	public void addAllFids(Collection fidsToAdd) {
-		if ( fidsToAdd == null ) 
-			return;
-		
-		for ( Iterator i = fidsToAdd.iterator(); i.hasNext();) {
-			String fid = (String) i.next();
-			addFid( fid );
-		}
-	} 
-	
-    /** 
-	 * Removes a feature ID from the filter. 
-	 * 
-	 * @param fid A single feature ID. 
-	 */ 
-	public final void removeFid(String fid) {
-		if ( fid == null ) {
-			return;
-		}
-		
-		for ( Iterator f = fids.iterator(); f.hasNext(); ) {
-			Identifier featureId = (Identifier) f.next();
-			if ( fid.equals( featureId.toString() ) ) {
-				f.remove();
-			}
-		}
-		 
-	} 
+    /**
+     * Adds a collection of feature IDs to the filter.
+     * 
+     * @param fidsToAdd
+     *            A collection of feature IDs as strings.
+     */
+    public void addAllFids(Collection fidsToAdd) {
+        if (fidsToAdd == null)
+            return;
 
-	/** 
-	 * Removes a collection of feature IDs from the filter. 
-	 * 
-	 * @param fidsToRemove A collection of feature IDs. 
-	 */ 
-	public void removeAllFids(Collection fidsToRemove) {
-		if ( fidsToRemove == null ) 
-			return;
-		
-		for ( Iterator f = fidsToRemove.iterator(); f.hasNext(); ) {
-			String fid = (String) f.next();
-			removeFid( fid );
-		}
-	} 
-	
-	/**
+        for (Iterator i = fidsToAdd.iterator(); i.hasNext();) {
+            String fid = (String) i.next();
+            addFid(fid);
+        }
+    }
+
+    /**
+     * Removes a feature ID from the filter.
+     * 
+     * @param fid
+     *            A single feature ID.
+     */
+    public final void removeFid(String fid) {
+        if (fid == null) {
+            return;
+        }
+
+        for (Iterator f = fids.iterator(); f.hasNext();) {
+            Identifier featureId = (Identifier) f.next();
+            if (fid.equals(featureId.toString())) {
+                f.remove();
+            }
+        }
+
+    }
+
+    /**
+     * Removes a collection of feature IDs from the filter.
+     * 
+     * @param fidsToRemove
+     *            A collection of feature IDs.
+     */
+    public void removeAllFids(Collection fidsToRemove) {
+        if (fidsToRemove == null)
+            return;
+
+        for (Iterator f = fidsToRemove.iterator(); f.hasNext();) {
+            String fid = (String) f.next();
+            removeFid(fid);
+        }
+    }
+
+    /**
      * Determines whether or not the given feature's ID matches this filter.
-     *
-     * @param feature Specified feature to examine.
-     *
-     * @return <tt>true</tt> if the feature's ID matches an fid held by this
-     * filter, <tt>false</tt> otherwise.
+     * 
+     * @param feature
+     *            Specified feature to examine.
+     * 
+     * @return <tt>true</tt> if the feature's ID matches an fid held by this filter,
+     *         <tt>false</tt> otherwise.
      */
     public boolean evaluate(Feature feature) {
         if (feature == null) {
@@ -215,9 +228,9 @@ public class FidFilterImpl extends AbstractFilterImpl implements FidFilter {
         return fids().contains(feature.getID());
     }
 
-   /**
+    /**
      * Returns a string representation of this filter.
-     *
+     * 
      * @return String representation of the compare filter.
      */
     public String toString() {
@@ -236,36 +249,26 @@ public class FidFilterImpl extends AbstractFilterImpl implements FidFilter {
         return "[ " + fidFilter.toString() + " ]";
     }
 
-   
-
-  
-    
-  
-   
-
-	
-
-	
-	 
-	/**
-     * Used by FilterVisitors to perform some action on this filter instance.
-     * Typicaly used by Filter decoders, but may also be used by any thing
-     * which needs infomration from filter structure. Implementations should
-     * always call: visitor.visit(this); It is importatant that this is not
-     * left to a parent class unless the parents API is identical.
-     *
-     * @param visitor The visitor which requires access to this filter, the
-     *        method must call visitor.visit(this);
+    /**
+     * Used by FilterVisitors to perform some action on this filter instance. Typicaly used by
+     * Filter decoders, but may also be used by any thing which needs infomration from filter
+     * structure. Implementations should always call: visitor.visit(this); It is importatant that
+     * this is not left to a parent class unless the parents API is identical.
+     * 
+     * @param visitor
+     *            The visitor which requires access to this filter, the method must call
+     *            visitor.visit(this);
      */
-	public Object accept(FilterVisitor visitor, Object extraData) {
-    	return visitor.visit(this,extraData);
+    public Object accept(FilterVisitor visitor, Object extraData) {
+        return visitor.visit(this, extraData);
     }
-	
-	 /**
+
+    /**
      * Returns a flag indicating object equality.
-     *
-     * @param filter the filter to test equality on.
-     *
+     * 
+     * @param filter
+     *            the filter to test equality on.
+     * 
      * @return String representation of the compare filter.
      */
     public boolean equals(Object filter) {
@@ -275,7 +278,7 @@ public class FidFilterImpl extends AbstractFilterImpl implements FidFilter {
             LOGGER.finest("condition: " + ((FidFilterImpl) filter).filterType);
 
             if (((FidFilterImpl) filter).filterType == AbstractFilter.FID) {
-            	return fids.equals(((FidFilterImpl) filter).fids);
+                return fids.equals(((FidFilterImpl) filter).fids);
             } else {
                 return false;
             }
@@ -286,7 +289,7 @@ public class FidFilterImpl extends AbstractFilterImpl implements FidFilter {
 
     /**
      * Override of hashCode method.
-     *
+     * 
      * @return a hash code value for this fid filter object.
      */
     public int hashCode() {
