cfd7e88322aa681399d04ba00c4086b96fbd06e2
Justin Deoliveira <jdeolive@gmail.com>
2007-01-11 02:47:54 +0000

    made check for any resource next to configuration
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23773 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/xml-xsd/src/main/java/org/geotools/xml/SchemaLocationResolver.java
+++ gt/modules/unsupported/xml-xsd/src/main/java/org/geotools/xml/SchemaLocationResolver.java
@@ -1,36 +1,80 @@
 package org.geotools.xml;
 
+import java.io.File;
+import java.net.URL;
+
 import org.eclipse.xsd.XSDSchema;
 import org.eclipse.xsd.util.XSDSchemaLocationResolver;
 
 /**
- * Schema location resolver which resolves a schema location from a namespace uri
- * and a physical schema location.
+ * Resolves a physical schema location from a namespace uri.
+ * <p>
+ * This class works from a {@link org.geotools.xml.Configuration} which defines information about
+ * the schema. 
+ * </p>
+ * <p>
+ * Example usage:
+ *  
+ * <code>
+ * 	<pre>
+ * 	Configuration myConfig = ...
+ * 	String namespaceURI = myConfig.getNamesapceURI();
  * 
+ * 	SchemaLocationResolver resolver = new SchemaLocationResolver( myConfig );
+ * 	String schemaLocation = locator.resolveSchemaLocation( null, namespaceURI, "mySchema.xsd" ); 
+ * 	</pre>
+ * </code>
+ * 
+ * </p>
  * @author Justin Deoliveira, The Open Planning Project
  *
  */
 public class SchemaLocationResolver implements XSDSchemaLocationResolver {
 
-	String uri, location;
+	/**
+	 * The Configuration
+	 */
+	Configuration configuration;
 	
 	/**
 	 * Creates the new schema location resolver.
 	 * 
-	 * @param uri The namesapce of the schema.
-	 * @param location The location of the schema.
+	 * @param configuration The schema configuration
 	 */
-	public SchemaLocationResolver( String uri, String location ) {
-		this.uri = uri;
-		this.location = location;
+	public SchemaLocationResolver( Configuration configuration ) {
+		this.configuration = configuration;
 	}
 	
+	/**
+	 * Resolves <param>location<param> to a physical location.
+	 * <p>
+	 * Resolution is performed by stripping the filename off of <param>location</param>
+	 * and looking up a resource located in the same package as the class of the configuration.
+	 * </p>
+	 */
 	public String resolveSchemaLocation( XSDSchema schema, String uri, String location ) {
-		if ( this.uri.equals( uri ) ) {
-			return this.location;
-		}
-		
-		return null;
+		 
+        if ( location == null ) {
+            return null;
+        }
+
+        //if no namespace given, assume default for the current schema
+        if ((( uri == null) || "".equals( uri)) && ( schema != null ) ) {
+            uri = schema.getTargetNamespace();
+        }
+        
+
+        //namespace match?
+        if ( configuration.getNamespaceURI().equals( uri )) {
+        	//strip off the filename and do a resource lookup
+           String fileName = new File( location ).getName();
+           URL xsd = configuration.getClass().getResource( fileName );
+           if ( xsd != null ) {
+        	   return xsd.toString();
+           }
+        }
+        
+        return null;
 	}
 
 }
