b9b7adcdaca93532f09ea9eee035ac677cdaf7fe
Justin Deoliveira <jdeolive@gmail.com>
2007-01-11 02:49:01 +0000

    factored out create schema method
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23775 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/xml-xsd/src/main/java/org/geotools/xml/SchemaLocator.java
+++ gt/modules/unsupported/xml-xsd/src/main/java/org/geotools/xml/SchemaLocator.java
@@ -34,14 +34,14 @@ import org.eclipse.xsd.util.XSDSchemaLocator;
  * @author Justin Deoliveira, The Open Planning Project
  *
  */
-public final class SchemaLocator implements XSDSchemaLocator {
+public class SchemaLocator implements XSDSchemaLocator {
 
 	static Logger logger = Logger.getLogger( "org.geotools.xml" );
 	
 	/**
 	 * the configuration
 	 */
-	Configuration configuration;
+	protected Configuration configuration;
 	
 	/**
 	 * cached schema
@@ -84,30 +84,45 @@ public final class SchemaLocator implements XSDSchemaLocator {
 					return this.schema;
                 }
 				
-				//add dependent location resolvers
-				List resolvers = new ArrayList();
-				for ( Iterator d = configuration.allDependencies().iterator(); d.hasNext(); ) {
-					Configuration dependency = (Configuration) d.next();
-					XSDSchemaLocationResolver resolver = dependency.getSchemaLocationResolver();
-					
-					if ( resolver != null) {
-						resolvers.add( resolver );
-					}
-				}
-				
 				try {
-					String location = configuration.getSchemaFileURL().toString();
-					logger.fine("parsing schema " + namespaceURI);
-                    this.schema =  Schemas.parse( location, null, resolvers );
-	            } 
-				catch (IOException e) {
-					String msg = "Failed to create schema: " + namespaceURI;
-	                logger.log( Level.WARNING, msg, e );
-	            }
+					this.schema = createSchema();
+				}
+				catch( Throwable t ) {
+					String msg = "Failed to create schema: " + configuration.getNamespaceURI();
+		            logger.log( Level.WARNING, msg, t );
+		        }
 			}
 		}
 		
 		return this.schema;
 	}
-	    	
+	    
+	/**
+	 * Template method for creating the schema instance from the xml configuration.
+	 * <p>
+	 * Subclasses may with to override or exten this method. The default implementation 
+	 * uses {@link Configuration#getSchemaFileURL()} and {@link Configuration#getSchemaLocationResolver()}
+	 * to parse the schema directly.
+	 * </p>
+	 * 
+	 * @return The created schema.
+	 * 
+	 * @throws Exception Any errors that occur while parsing the schema.
+	 */
+	protected XSDSchema createSchema() throws Exception {
+		//add dependent location resolvers
+		List resolvers = new ArrayList();
+		for ( Iterator d = configuration.allDependencies().iterator(); d.hasNext(); ) {
+			Configuration dependency = (Configuration) d.next();
+			XSDSchemaLocationResolver resolver = dependency.getSchemaLocationResolver();
+			
+			if ( resolver != null) {
+				resolvers.add( resolver );
+			}
+		}
+		
+		String location = configuration.getSchemaFileURL().toString();
+		logger.fine("parsing schema " + configuration.getNamespaceURI() );
+        return Schemas.parse( location, null, resolvers );
+   }
 }
