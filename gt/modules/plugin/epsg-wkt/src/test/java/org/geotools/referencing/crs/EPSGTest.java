cf2b34aea7a03b5b0ba3ec0d9a9dbb11b8bc864d
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2007-01-16 00:05:44 +0000

    Added artic CRS to epsg-wkt as requested by user on the mailing list. As a side effect, added some explicit references to the 'referencing' module in pom.xml: transitive dependency is supposed to work, but explicit references seems to help to avoid some unexpected behavior.
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@23880 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/plugin/epsg-wkt/src/test/java/org/geotools/referencing/crs/EPSGTest.java
+++ gt/modules/plugin/epsg-wkt/src/test/java/org/geotools/referencing/crs/EPSGTest.java
@@ -68,7 +68,7 @@ public class EPSGTest extends TestCase {
         Set codes = factory.getAuthorityCodes( CoordinateReferenceSystem.class );
         
         assertNotNull( codes );
-        assertEquals( 2707, codes.size() );                               
+        assertEquals( 2798, codes.size() );                               
     }
     
     /**
