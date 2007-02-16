9ceaf41117c3300d00556c5ea419803c26548001
Simone Giannecchini <simboss1@gmail.com>
2007-02-16 16:46:31 +0000

    -first bunch of changes for the alignment of trunk with 2.3.x
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@24448 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/plugin/postgis/src/main/java/org/geotools/data/postgis/attributeio/PgWKBAttributeIO.java
+++ gt/modules/plugin/postgis/src/main/java/org/geotools/data/postgis/attributeio/PgWKBAttributeIO.java
@@ -23,7 +23,7 @@ import java.sql.Types;
 
 import org.geotools.data.DataSourceException;
 import org.geotools.data.jdbc.attributeio.AttributeIO;
-import org.geotools.util.LiteCoordinateSequenceFactory;
+import org.geotools.geometry.jts.LiteCoordinateSequenceFactory;
 
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
