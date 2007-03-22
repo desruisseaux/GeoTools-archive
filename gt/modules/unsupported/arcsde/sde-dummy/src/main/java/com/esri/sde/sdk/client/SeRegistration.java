a879d927d5e37ceaa5fe83e71049b359f00749cf
Saul Farber <>
2007-03-22 17:28:46 +0000

    fixes to support incoming raster changes
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@24874 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/arcsde/sde-dummy/src/main/java/com/esri/sde/sdk/client/SeRegistration.java
+++ gt/modules/unsupported/arcsde/sde-dummy/src/main/java/com/esri/sde/sdk/client/SeRegistration.java
@@ -1,9 +1,15 @@
 package com.esri.sde.sdk.client;
 
 public class SeRegistration {
+    
+    public static int SE_REGISTRATION_ROW_ID_COLUMN_TYPE_SDE = 0;
 	
 	public SeRegistration(SeConnection c, String s) {}
 	
 	public String getRowIdColumnName() { return null; }
+    public String getTableName() { return null; }
+    public void setRowIdColumnName(String s) {}
+    public void setRowIdColumnType(int i) {}
+    public void alter() {}
 
 }
