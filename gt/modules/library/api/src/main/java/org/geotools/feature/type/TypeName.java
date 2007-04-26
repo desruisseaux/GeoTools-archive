d6b2a2222b153d905e13baf55fb69ee6de627d2e
Gabriel Roldan <gabriel.roldan@gmail.com>
2007-04-26 11:46:53 +0000

    GEOT-1252, TypeName and Name equals symetry
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@25348 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/library/api/src/main/java/org/geotools/feature/type/TypeName.java
+++ gt/modules/library/api/src/main/java/org/geotools/feature/type/TypeName.java
@@ -15,7 +15,7 @@
  */
 package org.geotools.feature.type;
 
-import org.geotools.feature.Name;
+import org.geotools.resources.Utilities;
 
 
 /**
@@ -24,20 +24,87 @@ import org.geotools.feature.Name;
  * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
  *
  */
-public class TypeName extends Name implements org.opengis.feature.type.TypeName {
-    public TypeName(String namespace, String local) {
-        super(namespace, local);
-    }
+public class TypeName implements org.opengis.feature.type.TypeName {
+    /** namespace / scope */
+    protected String namespace;
 
+    /** local part */
+    protected String local;
+
+    /**
+     * Constructs an instance with the local part set. Namespace / scope is
+     * set to null.
+     *
+     * @param local The local part of the name.
+     */
     public TypeName(String local) {
-        super(local);
+        this(null, local);
     }
 
+    /**
+     * Constructs an instance with the local part and namespace set.
+     *
+     * @param namespace The namespace or scope of the name.
+     * @param local The local part of the name.
+     *
+     */
+    public TypeName(String namespace, String local) {
+        this.namespace = namespace;
+        this.local = local;
+    }
+
+    public boolean isGlobal() {
+        return getNamespaceURI() == null;
+    }
+
+    public String getNamespaceURI() {
+        return namespace;
+    }
+
+    public String getLocalPart() {
+        return local;
+    }
+
+    public String getURI() {
+        if ((namespace == null) && (local == null)) {
+            return null;
+        }
+
+        if (namespace == null) {
+            return local;
+        }
+
+        if (local == null) {
+            return namespace;
+        }
+
+        return namespace + local;
+    }
+
+    /**
+     * value object with equality based on name and namespace.
+     */
+    public int hashCode() {
+        String uri = getURI();
+
+        return (uri != null) ? uri.hashCode() : 0;
+    }
+
+    /**
+     * value object with equality based on name and namespace.
+     */
     public boolean equals(Object obj) {
-        if (obj instanceof Name) {
-            return super.equals(obj);
+        if (obj instanceof TypeName) {
+            TypeName other = (TypeName) obj;
+
+            return Utilities.equals(getURI(), other.getURI());
         }
 
         return false;
+    }
+
+    /** name or namespace:name */
+    public String toString() {
+        return getURI();
     }
 }
