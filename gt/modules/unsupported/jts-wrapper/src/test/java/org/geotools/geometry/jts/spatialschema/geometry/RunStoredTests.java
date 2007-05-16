5a486236f2b73a00667269e78bcfc71935020813
Cory Horner <>
2007-05-16 21:10:42 +0000

    tweaked jts tests for jts-wrapper
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@25554 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/jts-wrapper/src/test/java/org/geotools/geometry/jts/spatialschema/geometry/RunStoredTests.java
+++ gt/modules/unsupported/jts-wrapper/src/test/java/org/geotools/geometry/jts/spatialschema/geometry/RunStoredTests.java
@@ -22,9 +22,9 @@ import org.apache.commons.logging.LogFactory;
 import org.xml.sax.InputSource;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
 
 /**
  * @author Jody Garnett
@@ -32,7 +32,7 @@ import java.io.FilenameFilter;
  */
 public class RunStoredTests extends TestCase {
     private static final Log LOG = LogFactory.getLog(RunStoredTests.class);
-    private static String TEST_DIRECTORY = "xmltests";
+    private static String TEST_DIRECTORY = "src/main/resources/org/geotools/test-data/xml/geometry";
 
     private FilenameFilter xmlFilter = new FilenameFilter() {
         public boolean accept(File dir, String name) {
@@ -42,21 +42,20 @@ public class RunStoredTests extends TestCase {
 
     /**
      * Load and run all test files.
-     * @throws FileNotFoundException
+     * @throws IOException 
      */
-    public void testStoredTests() throws FileNotFoundException {
+    public void testGeometriesFromXML() throws IOException {
         GeometryTestParser parser = new GeometryTestParser();
-
         File dir = new File(TEST_DIRECTORY);
+        File[] listFiles = dir.listFiles(xmlFilter);
         if (dir.isDirectory()) {
-            File listFiles[] = dir.listFiles(xmlFilter);
-            for (int i=0; i<listFiles.length;i++) {
+            for (int i=0; i<listFiles.length; i++) {
                 File testFile  = (File) listFiles[i];
-            	LOG.info("Loading test description file:" + testFile);
+                LOG.info("Loading test description file:" + testFile);
                 FileInputStream inputStream = new FileInputStream(testFile);
                 InputSource inputSource = new InputSource(inputStream);
                 GeometryTestContainer tests = parser.parseTestDefinition(inputSource);
-                assertTrue("Failed test: " + testFile.getName(), tests.runAllTestCases());
+                assertTrue("Failed test(s) in: " + testFile.getName(), tests.runAllTestCases());
             }
         }
     }
