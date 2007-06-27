5e4b2ee6191de7e30f750b516e9cce7c93256601
Justin Deoliveira <jdeolive@gmail.com>
2007-06-27 19:24:31 +0000

    application schema configuration test
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@26056 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/xml-gml3/src/test/java/org/geotools/gml3/GML3EncodingTest.java
+++ gt/modules/unsupported/xml-gml3/src/test/java/org/geotools/gml3/GML3EncodingTest.java
@@ -24,7 +24,8 @@ import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.util.ArrayList;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.gml3.bindings.TEST;
 import org.geotools.gml3.bindings.TestConfiguration;
@@ -34,7 +35,7 @@ import org.geotools.xml.SchemaLocator;
 
 
 public class GML3EncodingTest extends TestCase {
-    public void test() throws Exception {
+    public void testWithConfiguration() throws Exception {
         TestConfiguration configuration = new TestConfiguration();
 
         //first parse in test data
@@ -62,19 +63,79 @@ public class GML3EncodingTest extends TestCase {
         saxParser.setProperty("http://apache.org/xml/properties/schema/external-schemaLocation",
             TEST.NAMESPACE + " " + configuration.getSchemaFileURL());
 
+        final ArrayList errors = new ArrayList();
         DefaultHandler handler = new DefaultHandler() {
                 public void error(SAXParseException e)
                     throws SAXException {
                     System.out.println(e.getMessage());
+                    errors.add(e);
                 }
 
                 public void fatalError(SAXParseException e)
                     throws SAXException {
                     System.out.println(e.getMessage());
+                    errors.add(e);
                 }
             };
 
         saxParser.setErrorHandler(handler);
         saxParser.parse(new InputSource(new ByteArrayInputStream(output.toByteArray())));
+
+        assertTrue(errors.isEmpty());
+    }
+
+    public void testWithApplicationSchemaConfiguration()
+        throws Exception {
+        String schemaLocation = new File(TestConfiguration.class.getResource("test.xsd").getFile())
+            .getAbsolutePath();
+
+        ApplicationSchemaConfiguration configuration = new ApplicationSchemaConfiguration(TEST.NAMESPACE,
+                schemaLocation);
+
+        //first parse in test data
+        Parser parser = new Parser(configuration);
+        FeatureCollection fc = (FeatureCollection) parser.parse(TestConfiguration.class
+                .getResourceAsStream("test.xml"));
+        assertNotNull(fc);
+
+        SchemaLocator locator = new SchemaLocator(configuration);
+        XSDSchema schema = locator.locateSchema(null, TEST.NAMESPACE, null, null);
+        assertNotNull(schema);
+
+        Encoder encoder = new Encoder(configuration, schema);
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        encoder.write(fc, TEST.TestFeatureCollection, output);
+
+        SAXParser saxParser = new SAXParser();
+        saxParser.setFeature("http://xml.org/sax/features/validation", true);
+        saxParser.setFeature("http://apache.org/xml/features/validation/schema", true);
+        saxParser.setFeature("http://apache.org/xml/features/validation/schema-full-checking", true);
+
+        saxParser.setProperty("http://apache.org/xml/properties/schema/external-noNamespaceSchemaLocation",
+            TEST.NAMESPACE);
+
+        saxParser.setProperty("http://apache.org/xml/properties/schema/external-schemaLocation",
+            TEST.NAMESPACE + " " + configuration.getSchemaFileURL());
+
+        final ArrayList errors = new ArrayList();
+        DefaultHandler handler = new DefaultHandler() {
+                public void error(SAXParseException e)
+                    throws SAXException {
+                    System.out.println(e.getMessage());
+                    errors.add(e);
+                }
+
+                public void fatalError(SAXParseException e)
+                    throws SAXException {
+                    System.out.println(e.getMessage());
+                    errors.add(e);
+                }
+            };
+
+        saxParser.setErrorHandler(handler);
+        saxParser.parse(new InputSource(new ByteArrayInputStream(output.toByteArray())));
+
+        assertTrue(errors.isEmpty());
     }
 }
