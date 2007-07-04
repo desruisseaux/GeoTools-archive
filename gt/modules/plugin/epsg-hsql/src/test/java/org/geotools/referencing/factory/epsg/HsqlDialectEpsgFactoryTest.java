8f4a1447c98f606e4f85ce5d5bccfc4b5d8ae44f
Cory Horner <>
2007-07-04 22:59:18 +0000

    added epsg hsql/oracle authority codes test
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@26155 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/plugin/epsg-hsql/src/test/java/org/geotools/referencing/factory/epsg/HsqlDialectEpsgFactoryTest.java
+++ gt/modules/plugin/epsg-hsql/src/test/java/org/geotools/referencing/factory/epsg/HsqlDialectEpsgFactoryTest.java
@@ -1,9 +1,9 @@
 package org.geotools.referencing.factory.epsg;
 
 import java.sql.Connection;
+import java.util.Set;
 
 import org.geotools.factory.Hints;
-import org.hsqldb.jdbc.jdbcDataSource;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import javax.sql.DataSource;
 import junit.framework.TestCase;
@@ -20,13 +20,19 @@ public class HsqlDialectEpsgFactoryTest extends TestCase {
 	}
 	
 	public void testCreation() throws Exception {
-		assertNotNull( factory );
+		assertNotNull(factory);
 		CoordinateReferenceSystem epsg4326 = factory.createCoordinateReferenceSystem("EPSG:4326");
 		CoordinateReferenceSystem code4326 = factory.createCoordinateReferenceSystem("4326");
 		
-		assertEquals( "4326 equals EPSG:4326", code4326, epsg4326);
-		assertSame( "4326 == EPSG:4326", code4326, epsg4326);		
+		assertEquals("4326 equals EPSG:4326", code4326, epsg4326);
+        assertSame("4326 == EPSG:4326", code4326, epsg4326);		
 	}
-	
-	
+    
+    public void testAuthorityCodes() throws Exception {
+        Set authorityCodes = factory.getAuthorityCodes(CoordinateReferenceSystem.class);
+
+        assertNotNull(authorityCodes);
+        assertTrue(authorityCodes.size() > 3000);
+    }
+
 }
