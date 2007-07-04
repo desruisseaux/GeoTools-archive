31e45cf9f171b78478a109032acdb7d49eb2f067
Cory Horner <>
2007-07-04 21:36:52 +0000

    made OracleDataStoreFactoryOnlineTest a proper online test
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@26153 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/oracle-spatial/src/test/java/org/geotools/data/oracle/OracleDataStoreFactoryOnlineTest.java
+++ gt/modules/unsupported/oracle-spatial/src/test/java/org/geotools/data/oracle/OracleDataStoreFactoryOnlineTest.java
@@ -7,22 +7,19 @@ package org.geotools.data.oracle;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-
-import junit.framework.TestCase;
 
 import org.geotools.data.DataStoreFactorySpi;
 import org.geotools.data.jdbc.ConnectionPoolManager;
+import org.geotools.test.OnlineTestCase;
 
 /**
  * Test the datastore factories
  * 
  * @author Andrea Aime
  */
-public class OracleDataStoreFactoryOnlineTest extends TestCase {
+public class OracleDataStoreFactoryOnlineTest extends OnlineTestCase {
     /** The Oracle driver class name */
     private static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    private Properties properties;
     boolean GO = true;
     /**
      * Creates a new OracleDataStoreFactoryTest Test.
@@ -41,45 +38,15 @@ public class OracleDataStoreFactoryOnlineTest extends TestCase {
     }
 
     /**
-     * Creates a new OracleDataStoreFactoryTest Test.
-     * 
-     * @param arg0 name of the test
-     * @throws ClassNotFoundException If the Oracle Driver cannot be found
-     */
-    public OracleDataStoreFactoryOnlineTest( String arg0 ) throws ClassNotFoundException {
-        super(arg0);
-        try {
-        	Class.forName(JDBC_DRIVER);
-        }
-        catch( Throwable t ){
-            GO = false;
-        	// must be running off dummy jar!
-        }
-    }
-
-    /**
-     * Loads the properties
-     * 
-     * @throws Exception
-     * @see junit.framework.TestCase#setUp()
-     */
-    protected void setUp() throws Exception {
-        super.setUp();
-        properties = new Properties();
-        properties.load( this.getClass().getResourceAsStream("test.properties") );
-    }
-
-    /**
      * Removes the properties
      * 
      * @throws Exception
      * @see junit.framework.TestCase#tearDown()
      */
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    protected void disconnect() throws Exception {
+        super.disconnect();
         ConnectionPoolManager manager = ConnectionPoolManager.getInstance();
         manager.closeAll();
-        properties = null;
     }
     
     public void testDataStoreFactory() throws Exception {
@@ -96,25 +63,29 @@ public class OracleDataStoreFactoryOnlineTest extends TestCase {
 
     public void checkFactoryNamespace(DataStoreFactorySpi factory) throws Exception {
     	Map map = new HashMap();
-        map.put("host", properties.getProperty("host"));
-        map.put("port", properties.getProperty("port"));
-        map.put("instance", properties.getProperty("instance"));
-        map.put("user", properties.getProperty("user"));
-        map.put("passwd", properties.getProperty("passwd"));
+        map.put("host", fixture.getProperty("host"));
+        map.put("port", fixture.getProperty("port"));
+        map.put("instance", fixture.getProperty("instance"));
+        map.put("user", fixture.getProperty("user"));
+        map.put("passwd", fixture.getProperty("passwd"));
         map.put("dbtype", "oracle");
-        map.put("alias", properties.getProperty("instance"));
+        map.put("alias", fixture.getProperty("instance"));
         map.put("namespace", null);
         
         assertTrue(factory.canProcess(map));
         OracleDataStore store = (OracleDataStore) factory.createDataStore(map); 
         assertNull(store.getNameSpace());
         
-        map.put("schema", properties.getProperty("user").toUpperCase());
+        map.put("schema", fixture.getProperty("user").toUpperCase());
         store = (OracleDataStore) factory.createDataStore(map); 
         assertNull(store.getNameSpace());
         
         map.put("namespace", "topp");
         store = (OracleDataStore) factory.createDataStore(map); 
         assertEquals(new URI("topp"), store.getNameSpace());
+    }
+
+    protected String getFixtureId() {
+        return "oracle.test";
     }
 }
