4e936dd23a321363b30ad627f41d65a706326ed0
Jody Garnett <jody.garnett@gmail.com>
2007-07-05 00:52:30 +0000

    Added WeakObjectCache implemended based on martin's email
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@26156 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/modules/library/metadata/src/main/java/org/geotools/util/WeakObjectCache.java
@@ -0,0 +1,171 @@
+/*
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2007, GeoTools Project Managment Committee (PMC)
+ *   
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.util;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import EDU.oswego.cs.dl.util.concurrent.ReentrantLock;
+
+
+/**
+ * Caching implementation for {@link ObjectCache}. This instance is used when
+ * caching is desired, and memory use is an issue.
+ * <p>
+ * Values are held in a WeakReference, the garbage collector may reclaim them
+ * at any time.
+ * 
+ * @since 2.4
+ * @version $Id$
+ * @source $URL$
+ * @author Cory Horner (Refractions Research)
+ */
+final class WeakObjectCache implements ObjectCache {
+    
+    /**
+     * The cached values for each key.
+     */
+    private final Map/*<Object,WeakReference>*/ cache;
+
+    /**
+     * The locks for keys under construction.
+     */
+    private final Map/*<K,ReentrantLock>*/ locks;
+    
+    /**
+     * Creates a new cache.
+     */
+    public WeakObjectCache() {
+        this( 50 );
+    }
+    
+    /**
+     * Creates a new cache using the indicated initialSize.
+     */
+    public WeakObjectCache(final int initialSize) {
+        cache = Collections.synchronizedMap( new HashMap(initialSize) );
+        locks = new HashMap();
+    }
+
+    /**
+     * Removes all entries from this map.
+     */
+    public void clear() {
+        cache.clear();        
+    }
+
+    /**
+     * Check if an entry exists in the cache.
+     * 
+     * @param key
+     * @return boolean
+     */
+    public boolean containsKey(final Object key) {
+        if( cache.containsKey(key) ){
+            Object stored = cache.get(key);
+            if(stored instanceof Reference){
+                Reference reference = (Reference) stored;
+                return reference.get() != null;
+            }
+            return stored != null;
+        }
+        return false;
+    }
+    
+    /**
+     * Returns the indicated object from the cache, or null if not found.
+     * 
+     * @param key
+     *            The authority code.
+     */
+    public Object get(final Object key) {
+        Object stored = cache.get(key);
+        if(stored instanceof Reference){
+            Reference reference = (Reference) stored;
+            Object value = reference.get();
+            if( value == null ){
+                cache.remove( key );
+            }
+            return value;
+        }
+        return stored;
+    }
+
+    public Object peek(final Object key) {
+        Object stored = cache.get(key);
+        if(stored instanceof Reference){
+            Reference reference = (Reference) stored;
+            return reference.get();
+        }
+        return stored;
+    }
+
+    public void writeLock(final Object key) {
+        ReentrantLock lock;
+        synchronized (locks) {
+            lock = (ReentrantLock) locks.get(key);
+            if (lock == null) {
+                lock = new ReentrantLock();
+                locks.put(key, lock);
+            }
+        }
+        try {
+            // Must be outside the above synchronized section, since this call may block.
+            lock.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void writeUnLock(final Object key) {
+        synchronized (locks) {
+            final ReentrantLock lock = (ReentrantLock) locks.get(key);
+            if (lock == null) {
+                throw new IllegalMonitorStateException("Cannot unlock prior to locking");
+            }
+            if( lock.holds() == 0 ){
+                throw new IllegalMonitorStateException("Cannot unlock prior to locking");
+            }
+            lock.release();
+            if (lock.holds() == 0) {
+                locks.remove(key);
+            }
+        }
+    }
+
+    boolean holdsLock(final Object key) {
+        synchronized (locks) {
+            final ReentrantLock lock = (ReentrantLock) locks.get(key);
+            if (lock != null) {
+                return lock.holds() != 0;
+            }
+        }
+        return false;
+    } 
+    /**
+     * Stores a value
+     */
+    public void put(final Object key, final Object object) {        
+        writeLock(key);
+        WeakReference reference = new WeakReference( object );
+        cache.put(key, reference);
+        writeUnLock(key);
+    }
+
+}
