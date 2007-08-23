9015fdf51e2783f572d9ad0d6b44a9b404832826
Justin Deoliveira <jdeolive@gmail.com>
2007-08-23 04:03:32 +0000

    renamed SimpleTypeBuilder to SimpleFeatureTypeBuilder
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@26691 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/xml-gml2/src/test/java/org/geotools/gml2/bindings/GML2MockData.java
+++ gt/modules/unsupported/xml-gml2/src/test/java/org/geotools/gml2/bindings/GML2MockData.java
@@ -37,7 +37,7 @@ import org.geotools.feature.Feature;
 import org.geotools.feature.FeatureType;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureFactoryImpl;
-import org.geotools.feature.simple.SimpleTypeBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.feature.simple.SimpleTypeFactoryImpl;
 import org.geotools.feature.type.DefaultFeatureTypeBuilder;
 import org.geotools.gml2.TEST;
