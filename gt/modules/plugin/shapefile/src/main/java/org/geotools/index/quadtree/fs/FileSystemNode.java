5e119c71279cddb5c5b9f7bc4b9a8cc1d1cceb4b
Andrea Aime <andrea.aime@gmail.com>
2007-09-13 09:17:01 +0000

    GEOT-1490 Access to spatial index is very inefficient
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@26948 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/plugin/shapefile/src/main/java/org/geotools/index/quadtree/fs/FileSystemNode.java
+++ gt/modules/plugin/shapefile/src/main/java/org/geotools/index/quadtree/fs/FileSystemNode.java
@@ -34,7 +34,7 @@ import com.vividsolutions.jts.geom.Envelope;
  * @source $URL$
  */
 public class FileSystemNode extends Node {
-    private FileChannel channel;
+    private ScrollingBuffer buffer;
     private ByteOrder order;
     private int subNodeStartByte;
     private int subNodesLength;
@@ -49,17 +49,15 @@ public class FileSystemNode extends Node {
      * @param startByte DOCUMENT ME!
      * @param subNodesLength DOCUMENT ME!
      */
-    public FileSystemNode(Envelope bounds, int id, Node parent, FileChannel channel,
-        ByteOrder order, int startByte, int subNodesLength) {
+    FileSystemNode(Envelope bounds, int id, Node parent, ScrollingBuffer buffer, int startByte, int subNodesLength) {
         super(bounds, id, parent);
-        this.channel = channel;
-        this.order = order;
+        this.buffer = buffer;
         this.subNodeStartByte = startByte;
         this.subNodesLength = subNodesLength;
     }
 
     public Node copy() throws IOException {
-    	FileSystemNode copy = new FileSystemNode(getBounds(), id, getParent(), channel, order, subNodeStartByte, subNodesLength );
+    	FileSystemNode copy = new FileSystemNode(getBounds(), id, getParent(), buffer, subNodeStartByte, subNodesLength );
     	copy.numShapesId=numShapesId;
     	copy.shapesId=new int [numShapesId];
     	System.arraycopy(shapesId, 0, copy.shapesId, 0, numShapesId);
@@ -122,16 +120,9 @@ public class FileSystemNode extends Node {
                     + subNode.getSubNodesLength();
             }
 
-            if (this.channel.position() != offset) {
-                /*
-                   LOGGER.finest("Actual position: " + this.channel.position() +
-                                 " moving to " + offset);
-                 */
-                this.channel.position(offset);
-            }
-
+            buffer.goTo(offset);
             for (int i = 0, ii = subNodes.size(); i < ((pos + 1) - ii); i++) {
-                subNode = readNode(pos, this, this.channel, this.order);
+                subNode = readNode(pos, this, buffer);
                 this.addSubNode(subNode);
             }
         } catch (IOException e) {
@@ -140,7 +131,7 @@ public class FileSystemNode extends Node {
 
         return super.getSubNode(pos);
     }
-
+    
     /**
      * DOCUMENT ME!
      *
@@ -152,43 +143,135 @@ public class FileSystemNode extends Node {
      */
     public static FileSystemNode readNode(int id, Node parent, FileChannel channel, ByteOrder order)
         throws IOException {
-        ByteBuffer buf = ByteBuffer.allocate(4 + 32 + 4);
-        buf.order(order);
-        channel.read(buf);
-        buf.flip();
+        ScrollingBuffer buffer = new ScrollingBuffer(channel, order);
+        return readNode(id, parent, buffer);
+    }
 
+    static FileSystemNode readNode(int id, Node parent, ScrollingBuffer buf)
+        throws IOException {
+        // offset 
         int offset = buf.getInt();
         double x1;
         double y1;
         double x2;
         double y2;
+        
+        // envelope
         x1 = buf.getDouble();
         y1 = buf.getDouble();
         x2 = buf.getDouble();
         y2 = buf.getDouble();
-
         Envelope env = new Envelope(x1, x2, y1, y2);
 
+        // shapes in this node
         int numShapesId = buf.getInt();
-
-        // The buffer is completed....
-        buf = ByteBuffer.allocate((4 * numShapesId) + 4);
-        buf.order(order);
-        channel.read(buf);
-        buf.flip();
-
         int[] ids = new int[numShapesId];
-        IntBuffer intBuf = buf.asIntBuffer();
-        intBuf.get(ids);
+        buf.getIntArray(ids);
+        int numSubNodes = buf.getInt();
 
-        int numSubNodes = intBuf.get();
-
-        // The buffer is completed....
-        FileSystemNode node = new FileSystemNode(env, id, parent, channel, order,
-                (int) channel.position(), offset);
+        // let's create the new node
+        FileSystemNode node = new FileSystemNode(env, id, parent, buf, (int) buf.getPosition(), offset);
         node.setShapesId(ids);
         node.setNumSubNodes(numSubNodes);
 
         return node;
+    }
+    
+    /**
+     * A utility class to access file contents by using a single scrolling buffer reading
+     * file contents with a minimum of 8kb per access
+     */
+    private static class ScrollingBuffer {
+        FileChannel channel;
+        ByteOrder order;
+        ByteBuffer buffer;
+        /** the initial position of the buffer in the channel */
+        long bufferStart;
+        
+        
+        public ScrollingBuffer(FileChannel channel, ByteOrder order) throws IOException {
+           this.channel = channel;
+           this.order = order;
+           this.bufferStart = channel.position();
+           
+           // start with an 8kb buffer
+           this.buffer = ByteBuffer.allocateDirect(8 * 1024);
+           this.buffer.order(order);
+           channel.read(buffer);
+           buffer.flip();
+        }
+        
+        public int getInt()  throws IOException {
+            if(buffer.remaining() < 4)
+                refillBuffer(4);
+            return buffer.getInt();
+        }
+        
+        public double getDouble()  throws IOException {
+            if(buffer.remaining() < 8)
+                refillBuffer(8);
+            return buffer.getDouble();
+        }
+        
+        public void getIntArray(int[] array)  throws IOException {
+            int size = array.length * 4;
+            if(buffer.remaining() < size)
+                refillBuffer(size);
+            // read the array using a view
+            IntBuffer intView = buffer.asIntBuffer();
+            intView.limit(array.length);
+            intView.get(array);
+            // don't forget to update the original buffer position, since the view is independent
+            buffer.position(buffer.position() + size);
+        }
+        
+        /**
+         * 
+         * @param requiredSize
+         * @throws IOException
+         */
+        void refillBuffer(int requiredSize) throws IOException {
+            // compute the actual position up to we have read something
+            long currentPosition = bufferStart + buffer.position();
+            // if the buffer is not big enough enlarge it
+            if(buffer.capacity() < requiredSize) {
+                int size = buffer.capacity();
+                while(size < requiredSize) size *= 2;
+                buffer = ByteBuffer.allocateDirect(size);
+                buffer.order(order);
+            }
+            readBuffer(currentPosition);
+        }
+
+        private void readBuffer(long currentPosition) throws IOException {
+            channel.position(currentPosition);
+            buffer.clear();
+            channel.read(buffer);
+            buffer.flip();
+            bufferStart = currentPosition;
+        }
+
+        /**
+         * Jumps the buffer to the specified position in the file
+         * @param newPosition
+         * @throws IOException
+         */
+        public void goTo(long newPosition) throws IOException {
+            // if the new position is already in the buffer, just move the buffer position
+            // otherwise we have to reload it
+            if(newPosition >= bufferStart && newPosition <= bufferStart + buffer.limit()) {
+                buffer.position((int) (newPosition - bufferStart));
+            } else {
+                readBuffer(newPosition);
+            }
+        }
+        
+        /**
+         * Returns the absolute position of the next byte that will be read
+         * @return
+         */
+        public long getPosition() {
+            return bufferStart + buffer.position();
+        }
     }
 }
