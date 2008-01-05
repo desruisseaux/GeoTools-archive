a1006e420786674cc81e1ca4fa1fac9566e34e2b
Andrea Aime <andrea.aime@gmail.com>
2008-01-05 10:58:50 +0000

    Fixing overview resolution setup
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@28613 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/plugin/image/src/main/java/org/geotools/gce/image/WorldImageReader.java
+++ gt/modules/plugin/image/src/main/java/org/geotools/gce/image/WorldImageReader.java
@@ -278,7 +278,7 @@ public final class WorldImageReader extends AbstractGridCoverage2DReader
 		// get the dimension of the hr image and build the model as well as
 		// computing the resolution
 		// //
-		numOverviews = wmsRequest ? 0 : reader.getNumImages(true);
+		numOverviews = wmsRequest ? 0 : reader.getNumImages(true) - 1;
 		int hrWidth = reader.getWidth(0);
 		int hrHeight = reader.getHeight(0);
 		final Rectangle actualDim = new Rectangle(0, 0, hrWidth, hrHeight);
@@ -325,7 +325,7 @@ public final class WorldImageReader extends AbstractGridCoverage2DReader
 			double res[];
 			for (int i = 0; i < numOverviews; i++) {
 				res = getResolution(originalEnvelope, new Rectangle(0, 0,
-						reader.getWidth(i), reader.getHeight(i)), crs);
+						reader.getWidth(i+1), reader.getHeight(i+1)), crs);
 				overViewResolutions[i][0] = res[0];
 				overViewResolutions[i][1] = res[1];
 			}
