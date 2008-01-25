88d404d635694e5ef0e54afe053124ce527cb5b5
Gabriel Roldan <gabriel.roldan@gmail.com>
2008-01-25 14:49:16 +0000

    GEOT-1692, crop positionLists with dimensions greater than 2 so it still parses as a two-dimensional geometry
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@28941 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/extension/xsd/gml3/src/main/java/org/geotools/gml3/bindings/DirectPositionListTypeBinding.java
+++ gt/modules/extension/xsd/gml3/src/main/java/org/geotools/gml3/bindings/DirectPositionListTypeBinding.java
@@ -26,38 +26,40 @@ import org.geotools.geometry.DirectPosition2D;
 import org.geotools.gml3.GML;
 import org.geotools.xml.*;
 
-
 /**
- * Binding object for the type http://www.opengis.net/gml:DirectPositionListType.
- *
+ * Binding object for the type
+ * http://www.opengis.net/gml:DirectPositionListType.
+ * 
  * <p>
- *        <pre>
+ * 
+ * <pre>
  *         <code>
- *  &lt;complexType name="DirectPositionListType"&gt;
+ *  &lt;complexType name=&quot;DirectPositionListType&quot;&gt;
  *      &lt;annotation&gt;
  *          &lt;documentation&gt;DirectPositionList instances hold the coordinates for a sequence of direct positions within the same coordinate
  *                          reference system (CRS).&lt;/documentation&gt;
  *      &lt;/annotation&gt;
  *      &lt;simpleContent&gt;
- *          &lt;extension base="gml:doubleList"&gt;
- *              &lt;attributeGroup ref="gml:SRSReferenceGroup"/&gt;
- *              &lt;attribute name="count" type="positiveInteger" use="optional"&gt;
+ *          &lt;extension base=&quot;gml:doubleList&quot;&gt;
+ *              &lt;attributeGroup ref=&quot;gml:SRSReferenceGroup&quot;/&gt;
+ *              &lt;attribute name=&quot;count&quot; type=&quot;positiveInteger&quot; use=&quot;optional&quot;&gt;
  *                  &lt;annotation&gt;
- *                      &lt;documentation&gt;"count" allows to specify the number of direct positions in the list. If the attribute count is present then
+ *                      &lt;documentation&gt;&quot;count&quot; allows to specify the number of direct positions in the list. If the attribute count is present then
  *                                                  the attribute srsDimension shall be present, too.&lt;/documentation&gt;
  *                  &lt;/annotation&gt;
  *              &lt;/attribute&gt;
  *          &lt;/extension&gt;
  *      &lt;/simpleContent&gt;
  *  &lt;/complexType&gt;
- *
- *          </code>
+ * </code>
  *         </pre>
+ * 
  * </p>
- *
+ * 
  * @generated
  */
 public class DirectPositionListTypeBinding extends AbstractComplexBinding {
+
     /**
      * @generated
      */
@@ -70,9 +72,8 @@ public class DirectPositionListTypeBinding extends AbstractComplexBinding {
     }
 
     /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     *
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated modifiable
      */
     public Class getType() {
@@ -80,57 +81,62 @@ public class DirectPositionListTypeBinding extends AbstractComplexBinding {
     }
 
     /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     *
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated modifiable
      */
-    public Object parse(ElementInstance instance, Node node, Object value)
-        throws Exception {
+    public Object parse(ElementInstance instance, Node node, Object value) throws Exception {
+        int crsDimension = 2;
+        Node dimensions = (Node) node.getAttribute("srsDimension");
+        if (dimensions != null) {
+            crsDimension = ((Number) dimensions.getValue()).intValue();
+        }
         CoordinateReferenceSystem crs = GML3ParsingUtils.crs(node);
 
-        //double[] values = (double[]) value;
+        // double[] values = (double[]) value;
         Double[] values = (Double[]) value;
-        BigInteger count = (BigInteger) node.getAttributeValue("count");
+        BigInteger coordinatesCount = (BigInteger) node.getAttributeValue("count");
 
-        if (count == null) {
-            //assume 2 dimensional
-            count = BigInteger.valueOf(values.length / 2);
+        if (coordinatesCount == null) {
+            coordinatesCount = BigInteger.valueOf(values.length / crsDimension);
         }
 
-        if (count.intValue() == 0) {
-            return new DirectPosition[] {  };
+        final int coordCount = coordinatesCount.intValue();
+        if (coordCount == 0) {
+            return new DirectPosition[] {};
         }
 
-        int dim = values.length / count.intValue();
+        int dim = values.length / coordCount;
 
-        if ((dim < 1) || (dim > 2)) {
-            throw new IllegalArgumentException("dimension must be 1 or 2");
+        //if ((dim < 1) || (dim > 2)) {
+        if (dim < 1) {
+            throw new IllegalArgumentException("dimension must be greater or equal to 1");
         }
 
-        DirectPosition[] dps = new DirectPosition[count.intValue()];
+        DirectPosition[] dps = new DirectPosition[coordCount];
 
         if (dim == 1) {
-            for (int i = 0; i < count.intValue(); i++) {
+            for (int i = 0; i < coordCount; i++) {
                 dps[i] = new DirectPosition1D(crs);
                 dps[i].setOrdinate(0, values[i].doubleValue());
             }
         } else {
-            int j = 0;
-
-            for (int i = 0; i < count.intValue(); i++) {
-                dps[i] = new DirectPosition2D(crs);
-                dps[i].setOrdinate(0, values[j++].doubleValue());
-                dps[i].setOrdinate(1, values[j++].doubleValue());
+            int ordinateIdx = 0;
+            // HACK: not sure if its correct to assign ordinates 0 to 0 and 1 to
+            // 1 or it should be inferred from the crs
+            for (int coordIndex = 0; coordIndex < coordCount; coordIndex++) {
+                dps[coordIndex] = new DirectPosition2D(crs);
+                dps[coordIndex].setOrdinate(0, values[ordinateIdx].doubleValue());
+                dps[coordIndex].setOrdinate(1, values[ordinateIdx + 1].doubleValue());
+                ordinateIdx += crsDimension;
             }
         }
 
         return dps;
     }
 
-    public Element encode(Object object, Document document, Element value)
-        throws Exception {
-        //TODO: remove this when the parser can do lists
+    public Element encode(Object object, Document document, Element value) throws Exception {
+        // TODO: remove this when the parser can do lists
         DirectPosition[] dps = (DirectPosition[]) object;
         StringBuffer sb = new StringBuffer();
 
--- gt/modules/extension/xsd/gml3/src/test/java/org/geotools/gml3/bindings/DirectPositionListTypeBindingTest.java
+++ gt/modules/extension/xsd/gml3/src/test/java/org/geotools/gml3/bindings/DirectPositionListTypeBindingTest.java
@@ -15,18 +15,17 @@
  */
 package org.geotools.gml3.bindings;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.opengis.geometry.DirectPosition;
 import org.geotools.geometry.DirectPosition1D;
 import org.geotools.geometry.DirectPosition2D;
 import org.geotools.gml3.GML;
 import org.geotools.gml3.GML3TestSupport;
+import org.opengis.geometry.DirectPosition;
 
 
 public class DirectPositionListTypeBindingTest extends GML3TestSupport {
     public void test1D() throws Exception {
         GML3MockData.element(GML.posList, document, document);
+        document.getDocumentElement().setAttribute("srsDimension", "1");
         document.getDocumentElement().setAttribute("count", "2");
         document.getDocumentElement().appendChild(document.createTextNode("1.0 2.0 "));
 
@@ -43,6 +42,7 @@ public class DirectPositionListTypeBindingTest extends GML3TestSupport {
 
     public void test2D() throws Exception {
         GML3MockData.element(GML.posList, document, document);
+        document.getDocumentElement().setAttribute("srsDimension", "2");
         document.getDocumentElement().setAttribute("count", "1");
         document.getDocumentElement().appendChild(document.createTextNode("1.0 2.0 "));
 
@@ -54,5 +54,24 @@ public class DirectPositionListTypeBindingTest extends GML3TestSupport {
 
         assertEquals(1d, dps[0].getOrdinate(0), 0d);
         assertEquals(2d, dps[0].getOrdinate(1), 0d);
+    }
+    
+    public void test3D() throws Exception {
+        GML3MockData.element(GML.posList, document, document);
+        document.getDocumentElement().setAttribute("srsDimension", "3");
+
+        document.getDocumentElement().appendChild(document.createTextNode("1.0 2.0 1.0 3 4 5"));
+
+        DirectPosition[] dps = (DirectPosition[]) parse();
+        assertNotNull(dps);
+
+        assertEquals(2, dps.length);
+        assertTrue(dps[0] instanceof DirectPosition2D);
+
+        assertEquals(1d, dps[0].getOrdinate(0), 0d);
+        assertEquals(2d, dps[0].getOrdinate(1), 0d);
+
+        assertEquals(3d, dps[1].getOrdinate(0), 0d);
+        assertEquals(4d, dps[1].getOrdinate(1), 0d);
     }
 }
