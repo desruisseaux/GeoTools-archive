51a175e74eb8758df25e39c557d627b0055b61fe
Andrea Aime <andrea.aime@gmail.com>
2008-02-07 10:39:47 +0000

    Adding mock tests for feature results implementations
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@29127 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/modules/library/main/src/test/java/org/geotools/data/DefaultFeatureResultsTest.java
@@ -0,0 +1,52 @@
+package org.geotools.data;
+
+import static org.easymock.EasyMock.*;
+import junit.framework.TestCase;
+
+import org.opengis.feature.simple.SimpleFeatureType;
+
+public class DefaultFeatureResultsTest extends TestCase {
+
+    public void testMaxFeatureOptimized() throws Exception {
+        DefaultQuery q = new DefaultQuery("roads");
+        q.setMaxFeatures(10);
+
+        // mock up the feature source so that it'll return a count of 20
+        SimpleFeatureType type = DataUtilities.createType("roads",
+                "_=the_geom:Point,FID:String,NAME:String");
+        FeatureSource fs = createMock(FeatureSource.class);
+        expect(fs.getSchema()).andReturn(type).anyTimes();
+        expect(fs.getCount(q)).andReturn(20);
+        replay(fs);
+
+        DefaultFeatureResults results = new DefaultFeatureResults(fs, q);
+        assertEquals(10, results.size());
+    }
+
+    public void testMaxfeaturesHandCount() throws Exception {
+        DefaultQuery q = new DefaultQuery("roads");
+        q.setMaxFeatures(1);
+
+        // mock up the feature source so that it'll return a count of -1 (too
+        // expensive)
+        // and then will return a reader
+        FeatureReader fr = createNiceMock(FeatureReader.class);
+        expect(fr.hasNext()).andReturn(true).times(2).andReturn(false);
+        replay(fr);
+
+        DataStore ds = createMock(DataStore.class);
+        expect(ds.getFeatureReader(q, Transaction.AUTO_COMMIT)).andReturn(fr);
+        replay(ds);
+
+        SimpleFeatureType type = DataUtilities.createType("roads",
+                "_=the_geom:Point,FID:String,NAME:String");
+        FeatureSource fs = createMock(FeatureSource.class);
+        expect(fs.getSchema()).andReturn(type).anyTimes();
+        expect(fs.getCount(q)).andReturn(-1);
+        expect(fs.getDataStore()).andReturn(ds);
+        replay(fs);
+
+        DefaultFeatureResults results = new DefaultFeatureResults(fs, q);
+        assertEquals(1, results.size());
+    }
+}
