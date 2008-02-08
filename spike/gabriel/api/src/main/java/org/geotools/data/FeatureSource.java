d4fb11c9f8d0cfae404b078acd9c0780c8c2df73
Gabriel Roldan <gabriel.roldan@gmail.com>
2008-02-08 17:04:59 +0000

    spike for explicit subclass approach
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@29156 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ spike/gabriel/api/src/main/java/org/geotools/data/FeatureSource.java
@@ -0,0 +1,272 @@
+/*
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002-2006, GeoTools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.data;
+
+import java.awt.RenderingHints;
+import java.io.IOException;
+import java.util.Set;
+import com.vividsolutions.jts.geom.Envelope;
+
+import org.opengis.feature.Feature;
+import org.opengis.feature.FeatureVisitor;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.filter.Filter;
+import org.opengis.util.ProgressListener;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+
+
+/**
+ * Highlevel API for Features from a specific location.
+ *
+ * <p>
+ * Individual Shapefiles, databases tables , etc. are referenced through this
+ * interface. Compare and constrast with DataStore.
+ * </p>
+ *
+ * <p>
+ * Differences from DataStore:
+ * </p>
+ *
+ * <ul>
+ * <li>
+ * This is a prototype DataSource replacement, the Transaction methods have
+ * been moved to an external object, and the locking api has been intergrated.
+ * </li>
+ * <li>
+ * FeatureCollection has been replaced with FeatureResult as we do not wish to
+ * indicate that results can be stored in memory.
+ * </li>
+ * <li>
+ * FeatureSource has been split into three interfaces, the intention is to use
+ * the instanceof opperator to check capabilities rather than the previous
+ * DataSourceMetaData.
+ * </li>
+ * </ul>
+ *
+ *
+ * @author Jody Garnett
+ * @author Ray Gallagher
+ * @author Rob Hranac, TOPP
+ * @author Chris Holmes, TOPP
+ * @source $URL$
+ * @version $Id$
+ */
+public interface FeatureSource<T extends FeatureType, F extends Feature> {
+    
+    /**
+     * Information describing the contents of this resoruce.
+     * <p>
+     * Please note that for FeatureContent:
+     * <ul>
+     * <li>name - unqiue with in the context of a Service
+     * <li>schema - used to identify the type of resource; usually gml schema; although it may be more specific
+     * <ul>
+     */
+    ResourceInfo getInfo();
+    
+    /**
+     * Access to the DataStore implementing this FeatureStore.
+     *
+     * @return DataStore implementing this FeatureStore
+     */
+    DataStore getDataStore();
+
+    /**
+     * Adds a listener to the list that's notified each time a change to the
+     * FeatureStore occurs.
+     *
+     * @param listener FeatureListener
+     */
+    void addFeatureListener(FeatureListener listener);
+
+    /**
+     * Removes a listener from the list that's notified each time a change to
+     * the FeatureStore occurs.
+     *
+     * @param listener FeatureListener
+     */
+    void removeFeatureListener(FeatureListener listener);
+
+    /**
+     * Loads features from the datasource into the returned FeatureResults,
+     * based on the passed query.
+     *
+     * @param query a datasource query object.  It encapsulates requested
+     *        information, such as typeName, maxFeatures and filter.
+     *
+     * @return Collection The collection to put the features into.
+     *
+     * @throws IOException For all data source errors.
+     *
+     * @see Query
+     */
+    FeatureCollection<T,F> getFeatures(Query query) throws IOException;
+
+    /**
+     * Loads features from the datasource into the returned FeatureResults,
+     * based on the passed filter.
+     *
+     * @param filter An OpenGIS filter; specifies which features to retrieve.
+     *        <tt>null</tt> is not allowed, use Filter.INCLUDE instead.
+     *
+     * @return Collection The collection to put the features into.
+     *
+     * @throws IOException For all data source errors.
+     */
+    FeatureCollection<T,F> getFeatures(Filter filter) throws IOException;
+
+    /**
+     * Loads all features from the datasource into the return FeatureResults.
+     *
+     * <p>
+     * Filter.INCLUDE can also be used to get all features.  Calling this function
+     * is equivalent to using {@link Query#ALL}
+     * </p>
+     *
+     * @return Collection The collection to put the features into.
+     *
+     * @throws IOException For all data source errors.
+     */
+    FeatureCollection<T,F> getFeatures() throws IOException;
+
+    /**
+     * Retrieves the featureType that features extracted from this datasource
+     * will be created with.
+     *
+     * <p>
+     * The schema returned is the LCD supported by all available Features. In
+     * the common case of shapfiles and database table this schema will match
+     * that of every feature available. In the degenerate GML case this will
+     * simply reflect the gml:AbstractFeatureType.
+     * </p>
+     *
+     * @return the schema of features created by this datasource.
+     *
+     * @task REVISIT: Our current FeatureType model is not yet advanced enough
+     *       to handle multiple featureTypes.  Should getSchema take a
+     *       typeName now that a query takes a typeName, and thus DataSources
+     *       can now support multiple types? Or just wait until we can
+     *       programmatically make powerful enough schemas?
+     * @task REVISIT: we could also just use DataStore to capture multi
+     *       FeatureTypes?
+     */
+
+    //FeatureType getSchema();
+    //SimpleFeatureType getSchema();
+    T getSchema();
+
+    /**
+     * Gets the bounding box of this datasource.
+     *
+     * <p>
+     * With getBounds(Query) this becomes a convenience method for
+     * getBounds(Query.ALL), that is the bounds for all features contained
+     * here.
+     * </p>
+     *
+     * <p>
+     * If getBounds() returns <code>null</code> due to expense consider using
+     * <code>getFeatures().getBounds()</code> as a an alternative.
+     * </p>
+     *
+     * @return The bounding box of the datasource or null if unknown and too
+     *         expensive for the method to calculate.
+     *
+     * @throws IOException if there are errors getting the bounding box.
+     *
+     * @task REVISIT: Do we need this or can we use getBounds( Query.ALL )?
+     */
+    ReferencedEnvelope getBounds() throws IOException;
+
+    /**
+     * Gets the bounding box of the features that would be returned by this
+     * query.
+     *
+     * <p>
+     * To retrieve the bounds of the DataSource please use <code>getBounds(
+     * Query.ALL )</code>.
+     * </p>
+     *
+     * <p>
+     * This method is needed if we are to stream features to a gml out, since a
+     * FeatureCollection must have a boundedBy element.
+     * </p>
+     *
+     * <p>
+     * If getBounds(Query) returns <code>null</code> due to expense consider
+     * using <code>getFeatures(Query).getBounds()</code> as a an alternative.
+     * </p>
+     *
+     * @param query Contains the Filter and MaxFeatures to find the bounds for.
+     *
+     * @return The bounding box of the datasource or null if unknown and too
+     *         expensive for the method to calculate or any errors occur.
+     *
+     * @throws IOException DOCUMENT ME!
+     */
+    ReferencedEnvelope getBounds(Query query) throws IOException;
+
+    /**
+     * Gets the number of the features that would be returned by this query.
+     *
+     * <p></p>
+     *
+     * <p>
+     * If getBounds(Query) returns <code>-1</code> due to expense consider
+     * using <code>getFeatures(Query).getCount()</code> as a an alternative.
+     * </p>
+     *
+     * @param query Contains the Filter and MaxFeatures to find the bounds for.
+     *
+     * @return The number of Features provided by the Query or <code>-1</code>
+     *         if count is too expensive to calculate or any errors or occur.
+     *
+     * @throws IOException if there are errors getting the count
+     */
+    int getCount(Query query) throws IOException;
+
+    /**
+     * Returns the set of hints this {@link FeatureSource} is able to support.<p>
+     * Hints are to be specified in the {@link Query}, for each data access where they
+     * may be required.<br>
+     * Depending on the actual value provide by the user, the {@link FeatureSource}
+     * may decide not to honor the hint.
+     * @return a set of {@link RenderingHints#Key} objects (eventually empty, never null).
+     */
+    public Set /*<RenderingHints.Key>*/ getSupportedHints();
+    
+    // FeatureReader getFeatureReader( Query qury ); // ask justin for proposal
+
+    /**
+     * Accepts a visitor which is iterated over the entire set of features.
+     * 
+     * @param visitor The visitor.
+     * @param listener The listener to track progress of iteration.
+     */
+    void accept( FeatureVisitor visitor, ProgressListener listener ) throws IOException;
+
+    /**
+     * Accepts a visitor which is iterated over a filtered set of features.
+     * 
+     * @param filter Filter specifying the set of features to iterate over.
+     * @param visitor The visitor.
+     * @param listener Listener to track progress of iteration.
+     */
+    void accept( Filter filter, FeatureVisitor visitor, ProgressListener listener ) 
+        throws IOException;
+}
---
+++ spike/gabriel/api/src/main/java/org/geotools/feature/CollectionEvent.java
@@ -0,0 +1,121 @@
+/*
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002-2006, GeoTools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.feature;
+
+import java.util.EventObject;
+import org.opengis.feature.simple.SimpleFeature;
+import org.geotools.data.FeatureEvent;
+
+
+/**
+ * A simple event object to represent all events triggered by FeatureCollection
+ * instances (typically change events).
+ * @source $URL$
+ */
+public class CollectionEvent extends EventObject {
+    private static final long serialVersionUID = -1864190177730929948L;
+
+    /*
+     * Design Notes:
+     *  - Must look at other classes for hints on how to implement nicely.
+     */
+
+    /** event type constant denoting the adding of a feature */
+    public static final int FEATURES_ADDED = 0;
+
+    /** event type constant denoting the removal of a feature */
+    public static final int FEATURES_REMOVED = 1;
+
+    /**
+     * event type constant denoting that features in the collection has been
+     * modified
+     */
+    public static final int FEATURES_CHANGED = 2;
+
+    /** Indicates one of FEATURES_ADDED, FEATURES_REMOVED, FEATURES_CHANGED */
+    private int type;
+
+    /** Holds value of property features. */
+    private SimpleFeature[] features;
+
+    public CollectionEvent(FeatureCollection collection, FeatureEvent event) {
+        super(collection);
+
+        switch (event.getEventType()) {
+        case FeatureEvent.FEATURES_ADDED:
+            this.type = CollectionEvent.FEATURES_ADDED;
+
+            break;
+
+        case FeatureEvent.FEATURES_CHANGED:
+            this.type = CollectionEvent.FEATURES_CHANGED;
+
+            break;
+
+        case FeatureEvent.FEATURES_REMOVED:
+            this.type = CollectionEvent.FEATURES_REMOVED;
+
+            break;
+
+        default:
+            this.type = CollectionEvent.FEATURES_REMOVED;
+        }
+
+        this.features = null;
+    }
+
+    /**
+     * Constructs a new CollectionEvent.
+     *
+     * @param source the collection which triggered the event
+     * @param involvedFeatures DOCUMENT ME!
+     * @param type DOCUMENT ME!
+     */
+    public CollectionEvent(FeatureCollection source, SimpleFeature[] involvedFeatures, int type) {
+        super(source);
+        this.type = type;
+        this.features = involvedFeatures;
+    }
+
+    /**
+     * provides access to the featurecollection which fired the event
+     *
+     * @return The FeatureCollection which was the event's source.
+     */
+    public FeatureCollection getCollection() {
+        return (FeatureCollection) source;
+    }
+
+    /**
+     * Provides information on the type of change that has occured. Possible
+     * types are: add, remove, change
+     *
+     * @return an int which must be one of FEATURES_ADDED, FEATURES_REMOVED,
+     *         FEATURES_CHANGED
+     */
+    public int getEventType() {
+        return type;
+    }
+
+    /**
+     * Getter for property features.
+     *
+     * @return Value of property features.
+     */
+    public SimpleFeature[] getFeatures() {
+        return this.features;
+    }
+}
