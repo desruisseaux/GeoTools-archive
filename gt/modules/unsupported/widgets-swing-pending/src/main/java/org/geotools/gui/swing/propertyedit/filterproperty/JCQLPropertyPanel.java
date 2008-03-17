05232af02168b83a87b12b6d9904d33cf4c92017
Johann Sorel <johann.sorel@geomatys.com>
2008-03-17 17:07:18 +0000

    Improving Style edition , added new classes, handle partly Text Symbolizer.
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@29650 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/modules/unsupported/widgets-swing-pending/src/main/java/org/geotools/gui/swing/propertyedit/filterproperty/JCQLPropertyPanel.java
@@ -0,0 +1,63 @@
+/*
+ *    GeoTools - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002-2007, GeoTools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotools.gui.swing.propertyedit.filterproperty;
+
+import java.awt.Component;
+import org.geotools.data.DefaultQuery;
+import org.geotools.gui.swing.filter.JCQLFilterPanel;
+import org.geotools.gui.swing.propertyedit.PropertyPanel;
+import org.geotools.map.MapLayer;
+
+/**
+ *
+ * @author johann sorel
+ */
+public class JCQLPropertyPanel extends JCQLFilterPanel implements PropertyPanel{
+
+    
+    private MapLayer layer;
+        
+    
+    private void parse(){
+        
+        setLayer(layer);
+        setFilter(layer.getQuery().getFilter());
+    }
+    
+    public void setTarget(Object target) {
+        if (target instanceof MapLayer) {
+            layer = (MapLayer) target;
+            parse();
+        }
+    }
+
+    public void apply() {  
+        if(layer !=null){
+            layer.setQuery(new DefaultQuery("cqlfilter", getFilter()));
+        }
+    }
+
+    public void reset() {
+        parse();
+    }
+
+    
+    public Component getPanel() {
+        return getComponent();
+    }
+
+}
