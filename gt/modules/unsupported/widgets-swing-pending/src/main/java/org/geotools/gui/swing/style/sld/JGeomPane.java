d847221f07cf04ff91716d47bc1b6f93f1ec2ff5
Johann Sorel <johann.sorel@geomatys.com>
2008-03-19 14:19:40 +0000

    fixed jgeompane, switch grouplayout 1.6 to 1.5 (dependency jar)
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@29666 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/widgets-swing-pending/src/main/java/org/geotools/gui/swing/style/sld/JGeomPane.java
+++ gt/modules/unsupported/widgets-swing-pending/src/main/java/org/geotools/gui/swing/style/sld/JGeomPane.java
@@ -68,20 +68,20 @@ public class JGeomPane extends javax.swing.JPanel {
         lbl.setText(bundle.getString("geometry")); // NOI18N
         lbl.setEnabled(false);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(lbl)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(guiBox, javax.swing.GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE))
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(layout.createSequentialGroup()
+                .add(lbl)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(guiBox, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                .addComponent(lbl)
-                .addComponent(guiBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                .add(lbl)
+                .add(guiBox, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
         );
     }// </editor-fold>//GEN-END:initComponents
     
