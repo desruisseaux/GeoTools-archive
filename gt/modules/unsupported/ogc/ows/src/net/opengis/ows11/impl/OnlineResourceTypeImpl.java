85862ee5635a1706c9b9fde47a1511fe158add07
Justin Deoliveira <jdeolive@gmail.com>
2008-04-09 04:42:44 +0000

    added emf modules for ogc web service schemas
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@29859 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ gt/modules/unsupported/ogc/ows/src/net/opengis/ows11/impl/OnlineResourceTypeImpl.java
@@ -0,0 +1,44 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package net.opengis.ows11.impl;
+
+import net.opengis.ows11.OnlineResourceType;
+import net.opengis.ows11.Ows11Package;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Online Resource Type</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class OnlineResourceTypeImpl extends EObjectImpl implements OnlineResourceType {
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected OnlineResourceTypeImpl() {
+        super();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected EClass eStaticClass() {
+        return Ows11Package.Literals.ONLINE_RESOURCE_TYPE;
+    }
+
+} //OnlineResourceTypeImpl
