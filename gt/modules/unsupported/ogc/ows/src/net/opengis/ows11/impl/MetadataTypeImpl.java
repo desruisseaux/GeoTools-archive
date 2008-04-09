d5b37a66832f74e67d33c4d1df245de0919bb544
Justin Deoliveira <jdeolive@gmail.com>
2008-04-09 16:58:34 +0000

    instrumented 1.1 model, added href to OnlineResource and title to Metadata
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@29876 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/unsupported/ogc/ows/src/net/opengis/ows11/impl/MetadataTypeImpl.java
+++ gt/modules/unsupported/ogc/ows/src/net/opengis/ows11/impl/MetadataTypeImpl.java
@@ -33,6 +33,7 @@ import org.eclipse.emf.ecore.util.InternalEList;
  *   <li>{@link net.opengis.ows11.impl.MetadataTypeImpl#getAbstractMetaDataGroup <em>Abstract Meta Data Group</em>}</li>
  *   <li>{@link net.opengis.ows11.impl.MetadataTypeImpl#getAbstractMetaData <em>Abstract Meta Data</em>}</li>
  *   <li>{@link net.opengis.ows11.impl.MetadataTypeImpl#getAbout <em>About</em>}</li>
+ *   <li>{@link net.opengis.ows11.impl.MetadataTypeImpl#getTitle <em>Title</em>}</li>
  * </ul>
  * </p>
  *
@@ -40,203 +41,256 @@ import org.eclipse.emf.ecore.util.InternalEList;
  */
 public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
     /**
-     * The cached value of the '{@link #getAbstractMetaDataGroup() <em>Abstract Meta Data Group</em>}' attribute list.
-     * <!-- begin-user-doc -->
+	 * The cached value of the '{@link #getAbstractMetaDataGroup() <em>Abstract Meta Data Group</em>}' attribute list.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getAbstractMetaDataGroup()
-     * @generated
-     * @ordered
-     */
+	 * @see #getAbstractMetaDataGroup()
+	 * @generated
+	 * @ordered
+	 */
     protected FeatureMap abstractMetaDataGroup;
 
     /**
-     * The default value of the '{@link #getAbout() <em>About</em>}' attribute.
-     * <!-- begin-user-doc -->
+	 * The default value of the '{@link #getAbout() <em>About</em>}' attribute.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getAbout()
-     * @generated
-     * @ordered
-     */
+	 * @see #getAbout()
+	 * @generated
+	 * @ordered
+	 */
     protected static final String ABOUT_EDEFAULT = null;
 
     /**
-     * The cached value of the '{@link #getAbout() <em>About</em>}' attribute.
-     * <!-- begin-user-doc -->
+	 * The cached value of the '{@link #getAbout() <em>About</em>}' attribute.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getAbout()
-     * @generated
-     * @ordered
-     */
+	 * @see #getAbout()
+	 * @generated
+	 * @ordered
+	 */
     protected String about = ABOUT_EDEFAULT;
 
     /**
-     * <!-- begin-user-doc -->
+	 * The default value of the '{@link #getTitle() <em>Title</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getTitle()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String TITLE_EDEFAULT = null;
+
+				/**
+	 * The cached value of the '{@link #getTitle() <em>Title</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getTitle()
+	 * @generated
+	 * @ordered
+	 */
+	protected String title = TITLE_EDEFAULT;
+
+				/**
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     protected MetadataTypeImpl() {
-        super();
-    }
+		super();
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     protected EClass eStaticClass() {
-        return Ows11Package.Literals.METADATA_TYPE;
-    }
+		return Ows11Package.Literals.METADATA_TYPE;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public FeatureMap getAbstractMetaDataGroup() {
-        if (abstractMetaDataGroup == null) {
-            abstractMetaDataGroup = new BasicFeatureMap(this, Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP);
-        }
-        return abstractMetaDataGroup;
-    }
+		if (abstractMetaDataGroup == null) {
+			abstractMetaDataGroup = new BasicFeatureMap(this, Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP);
+		}
+		return abstractMetaDataGroup;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public EObject getAbstractMetaData() {
-        return (EObject)getAbstractMetaDataGroup().get(Ows11Package.Literals.METADATA_TYPE__ABSTRACT_META_DATA, true);
-    }
+		return (EObject)getAbstractMetaDataGroup().get(Ows11Package.Literals.METADATA_TYPE__ABSTRACT_META_DATA, true);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public NotificationChain basicSetAbstractMetaData(EObject newAbstractMetaData, NotificationChain msgs) {
-        return ((FeatureMap.Internal)getAbstractMetaDataGroup()).basicAdd(Ows11Package.Literals.METADATA_TYPE__ABSTRACT_META_DATA, newAbstractMetaData, msgs);
-    }
+		return ((FeatureMap.Internal)getAbstractMetaDataGroup()).basicAdd(Ows11Package.Literals.METADATA_TYPE__ABSTRACT_META_DATA, newAbstractMetaData, msgs);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public String getAbout() {
-        return about;
-    }
+		return about;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public void setAbout(String newAbout) {
-        String oldAbout = about;
-        about = newAbout;
-        if (eNotificationRequired())
-            eNotify(new ENotificationImpl(this, Notification.SET, Ows11Package.METADATA_TYPE__ABOUT, oldAbout, about));
-    }
+		String oldAbout = about;
+		about = newAbout;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, Ows11Package.METADATA_TYPE__ABOUT, oldAbout, about));
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+				/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setTitle(String newTitle) {
+		String oldTitle = title;
+		title = newTitle;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, Ows11Package.METADATA_TYPE__TITLE, oldTitle, title));
+	}
+
+				/**
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
-        switch (featureID) {
-            case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
-                return ((InternalEList)getAbstractMetaDataGroup()).basicRemove(otherEnd, msgs);
-            case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA:
-                return basicSetAbstractMetaData(null, msgs);
-        }
-        return super.eInverseRemove(otherEnd, featureID, msgs);
-    }
+		switch (featureID) {
+			case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
+				return ((InternalEList)getAbstractMetaDataGroup()).basicRemove(otherEnd, msgs);
+			case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA:
+				return basicSetAbstractMetaData(null, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public Object eGet(int featureID, boolean resolve, boolean coreType) {
-        switch (featureID) {
-            case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
-                if (coreType) return getAbstractMetaDataGroup();
-                return ((FeatureMap.Internal)getAbstractMetaDataGroup()).getWrapper();
-            case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA:
-                return getAbstractMetaData();
-            case Ows11Package.METADATA_TYPE__ABOUT:
-                return getAbout();
-        }
-        return super.eGet(featureID, resolve, coreType);
-    }
+		switch (featureID) {
+			case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
+				if (coreType) return getAbstractMetaDataGroup();
+				return ((FeatureMap.Internal)getAbstractMetaDataGroup()).getWrapper();
+			case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA:
+				return getAbstractMetaData();
+			case Ows11Package.METADATA_TYPE__ABOUT:
+				return getAbout();
+			case Ows11Package.METADATA_TYPE__TITLE:
+				return getTitle();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public void eSet(int featureID, Object newValue) {
-        switch (featureID) {
-            case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
-                ((FeatureMap.Internal)getAbstractMetaDataGroup()).set(newValue);
-                return;
-            case Ows11Package.METADATA_TYPE__ABOUT:
-                setAbout((String)newValue);
-                return;
-        }
-        super.eSet(featureID, newValue);
-    }
+		switch (featureID) {
+			case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
+				((FeatureMap.Internal)getAbstractMetaDataGroup()).set(newValue);
+				return;
+			case Ows11Package.METADATA_TYPE__ABOUT:
+				setAbout((String)newValue);
+				return;
+			case Ows11Package.METADATA_TYPE__TITLE:
+				setTitle((String)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public void eUnset(int featureID) {
-        switch (featureID) {
-            case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
-                getAbstractMetaDataGroup().clear();
-                return;
-            case Ows11Package.METADATA_TYPE__ABOUT:
-                setAbout(ABOUT_EDEFAULT);
-                return;
-        }
-        super.eUnset(featureID);
-    }
+		switch (featureID) {
+			case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
+				getAbstractMetaDataGroup().clear();
+				return;
+			case Ows11Package.METADATA_TYPE__ABOUT:
+				setAbout(ABOUT_EDEFAULT);
+				return;
+			case Ows11Package.METADATA_TYPE__TITLE:
+				setTitle(TITLE_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public boolean eIsSet(int featureID) {
-        switch (featureID) {
-            case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
-                return abstractMetaDataGroup != null && !abstractMetaDataGroup.isEmpty();
-            case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA:
-                return getAbstractMetaData() != null;
-            case Ows11Package.METADATA_TYPE__ABOUT:
-                return ABOUT_EDEFAULT == null ? about != null : !ABOUT_EDEFAULT.equals(about);
-        }
-        return super.eIsSet(featureID);
-    }
+		switch (featureID) {
+			case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA_GROUP:
+				return abstractMetaDataGroup != null && !abstractMetaDataGroup.isEmpty();
+			case Ows11Package.METADATA_TYPE__ABSTRACT_META_DATA:
+				return getAbstractMetaData() != null;
+			case Ows11Package.METADATA_TYPE__ABOUT:
+				return ABOUT_EDEFAULT == null ? about != null : !ABOUT_EDEFAULT.equals(about);
+			case Ows11Package.METADATA_TYPE__TITLE:
+				return TITLE_EDEFAULT == null ? title != null : !TITLE_EDEFAULT.equals(title);
+		}
+		return super.eIsSet(featureID);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public String toString() {
-        if (eIsProxy()) return super.toString();
+		if (eIsProxy()) return super.toString();
 
-        StringBuffer result = new StringBuffer(super.toString());
-        result.append(" (abstractMetaDataGroup: ");
-        result.append(abstractMetaDataGroup);
-        result.append(", about: ");
-        result.append(about);
-        result.append(')');
-        return result.toString();
-    }
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (abstractMetaDataGroup: ");
+		result.append(abstractMetaDataGroup);
+		result.append(", about: ");
+		result.append(about);
+		result.append(", title: ");
+		result.append(title);
+		result.append(')');
+		return result.toString();
+	}
 
 } //MetadataTypeImpl
