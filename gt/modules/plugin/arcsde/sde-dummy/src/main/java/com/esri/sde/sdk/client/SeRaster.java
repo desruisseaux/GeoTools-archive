a1578c8ed6cf43ee8b1eb62869ba2e2ca27f1de3
Saul Farber <>
2008-04-18 21:16:49 +0000

    dummy api fixes to support recent changes
    git-svn-id: https://svn.osgeo.org/geotools/geotools/trunk@29991 e5c1c795-43da-0310-a71f-fac65c449510

--- gt/modules/plugin/arcsde/sde-dummy/src/main/java/com/esri/sde/sdk/client/SeRaster.java
+++ gt/modules/plugin/arcsde/sde-dummy/src/main/java/com/esri/sde/sdk/client/SeRaster.java
@@ -6,8 +6,9 @@ public class SeRaster {
     public static int SE_PIXEL_TYPE_8BIT_U = 1;
     public static int SE_PIXEL_TYPE_8BIT_S = 2;
     public static int SE_PIXEL_TYPE_1BIT = 3;
+    public static int SE_PIXEL_TYPE_32BIT_REAL = 5;
     
-    public static int SE_COMPRESSION_NONE = 4;
+    public static int SE_COMPRESSION_NONE = 100;
     
     
 }
